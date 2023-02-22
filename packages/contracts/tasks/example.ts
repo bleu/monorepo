5c62493d5ebb8f1be6d4d0b098c1333d71c894b5
Marcos Tullyo <marcostullyo@ufersa.edu.br>
2023-02-22 19:12:48 -0300

    Squashed commit of the following:
    commit 96d7120a1d1df3d5eed4a5acaacd8eeebc7a6e1e
    Merge: 1396584 e547cbb
    Author: Marcos Tullyo <marcostullyo@ufersa.edu.br>
    Date:   Wed Feb 22 17:09:48 2023 -0300
    
        Merge pull request #2 from bleu-llc/jose/bal-60-set-up-repository-for-frontend
    
        set up frontend package
    
    commit e547cbbe895c2bae84e8139d034ee6eefc0022cc
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 22:59:42 2023 -0300
    
        fix lint/formatting issues
    
    commit 8d5ae0b17bf38c4d9b8f3a7419166124793dc52f
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 22:37:06 2023 -0300
    
        create config packages
    
    commit 45ec51dfad4cb26e5d51e904209cfc7493a656c3
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 21:46:27 2023 -0300
    
        move wagmi script location
    
    commit 45428599f968495bc745f68c1c0cc964811453c7
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 21:28:46 2023 -0300
    
        add next project
    
    commit 0aab6e7087db4372bced8e68cc4d5966f6d1ef95
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 21:28:40 2023 -0300
    
        add dev script to root package.json
    
    commit 9ea6d4c2096612487ef3bfd205aeb23fe72b07b5
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 20:57:18 2023 -0300
    
        update packages dependendencies
    
    commit 9986c175738c4fb0575171bc3595019a2ea90d74
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 20:57:10 2023 -0300
    
        update foundry config
    
    commit 8f3d178acc657902e42148a8a6e3bc3bfbf5174c
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Tue Feb 21 20:57:00 2023 -0300
    
        ignore **/lib/** workspaces in gitmodules

--- packages/contracts/tasks/example.ts
+++ packages/contracts/tasks/example.ts
@@ -1,13 +1,14 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
 
 export default async function example(
-  params: any,
+  _params: unknown,
   hre: HardhatRuntimeEnvironment
 ): Promise<void> {
   const ethers = hre.ethers;
 
   const [account] = await ethers.getSigners();
 
+  // eslint-disable-next-line no-console
   console.log(
     `Balance for 1st account ${await account.getAddress()}: ${await account.getBalance()}`
   );

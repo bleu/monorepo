64a8f847d961a7d1d5d600b95a8b094a5c90d76e
Marcos Tullyo <marcostullyo@ufersa.edu.br>
2023-02-25 19:45:46 -0300

    isPoolRegistered function on PoolMetadataRegistry (#3)
    * isPoolRegistered function on PoolMetadataRegistry
    
    * forge install: balancer-v2-monorepo
    
    * balancer install (forge)
    
    * forge install: balancer-v2-monorepo
    
    * forge install: balancer-v2-monorepo
    
    * update remappings
    
    * set up base testing structure
    
    * test cleanup
    
    * add remapping for balancer contracts
    
    * set .npmrc node-linker=hoisted
    
    * add test when pool is not registered
    
    * fuzz test
    
    * add .prettierignore
    
    * last adjustments
    
    * fuzzy test removed
    
    * Squashed commit of the following:
    
    commit 92dfec6a8c99a6809269bf2a5d15d334bb26894c
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Sat Feb 25 12:49:58 2023 -0300
    
        add example github actions to fix eslint/test contracts
    
    commit e234390b92b4ce385e5b46bb0c7de2a20d7e6426
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Sat Feb 25 12:49:31 2023 -0300
    
        substitute hardhat tests
    
    commit 613265790ff7345b366e1c48a1cb49ca0083ff88
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Sat Feb 25 12:49:17 2023 -0300
    
        use submodule balancer packages instead of npms
    
    commit 197f1b07b57348cdfe7de50493aec0f990dda93f
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Sat Feb 25 12:47:29 2023 -0300
    
        update .npmrc
    
    commit a33a2b772376a31052e4aff74464abb047256025
    Author: José Ribeiro <me@joseribeiro.dev>
    Date:   Sat Feb 25 12:47:16 2023 -0300
    
        add balancer dependencies to root of repo
    
    * refactor actions
    
    * refactor format scripts
    
    * add lint/format scripts
    
    * attempt at hardhat tests
    
    * remove hardhat test for poolMetadataRegistry
    
    ---------
    
    Co-authored-by: José Ribeiro <me@joseribeiro.dev>

---
+++ packages/contracts/test/PoolMetadataRegistry.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "../src/PoolMetadataRegistry.sol";
+
+import "balancer-v2-monorepo/pkg/vault/contracts/test/MockBasicAuthorizer.sol";
+import "balancer-v2-monorepo/pkg/vault/contracts/test/MockPool.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v2-vault/contracts/Vault.sol";
+import "@balancer-labs/v2-pool-utils/contracts/lib/PoolRegistrationLib.sol";
+
+import {Test} from "forge-std/Test.sol";
+
+contract PoolMetadataRegistryTest is Test {
+    PoolMetadataRegistry poolMetadataRegistry;
+    IVault private _vault;
+
+    bytes32 poolId;
+
+    function setUp() external {
+        _vault = new Vault(IAuthorizer(0), IWETH(0), 0, 0);
+
+        IERC20[] memory tokens = new IERC20[](10);
+        for (uint256 i = 0; i < tokens.length; i++) {
+            tokens[i] = IERC20(i + 1);
+        }
+        address[] memory assetManagers = new address[](10);
+
+        poolId =
+            PoolRegistrationLib.registerComposablePool(_vault, IVault.PoolSpecialization.GENERAL, tokens, assetManagers);
+
+        poolMetadataRegistry = new PoolMetadataRegistry(_vault);
+    }
+
+    function testIsPoolRegistered() public {
+        emit log_named_bytes32("poolId = ", poolId);
+        bool isPool = poolMetadataRegistry.isPoolRegistered(poolId);
+        emit log_named_string("is a Pool?", isPool ? "Yes" : "No");
+
+        assertTrue(isPool);
+    }
+
+    function testIsPoolNotRegistered() public {
+        emit log_named_bytes32("poolId = ", 0);
+        bool isPool = poolMetadataRegistry.isPoolRegistered(0);
+        emit log_named_string("is a Pool?", isPool ? "Yes" : "No");
+
+        assertFalse(isPool);
+    }
+}

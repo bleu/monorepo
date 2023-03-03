043efb28d75b1c0691bb4311b939025501501998
Marcos Tullyo <marcostullyo@ufersa.edu.br>
2023-03-03 15:28:48 -0300

    subgraph(fix): fix codegen

--- packages/subgraph/scripts/generate-manifests.ts
+++ packages/subgraph/scripts/generate-manifests.ts
@@ -6,12 +6,12 @@ import path = require('path');
 
 const generateManifests = async (): Promise<void> => {
   const networksFilePath = path.resolve(__dirname, '../networks.yaml');
-  const networks: Record<string, Record<string, unknown>> = yaml.load(
+  const networks: Record<string, Record<string, unknown>> | unknown = yaml.load(
     await fs.readFile(networksFilePath, { encoding: 'utf-8' })
   );
 
   const template = fs.readFileSync('manifest.template.yaml').toString();
-  Object.entries(networks).forEach(([network, config]) => {
+  Object.entries(networks ? networks : []).forEach(([network, config]) => {
     fs.writeFileSync(
       `subgraph${network === 'mainnet' ? '' : `.${network}`}.yaml`,
       Handlebars.compile(template)(config)

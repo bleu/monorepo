7c60f3ee2f7cd3eea0ca31652e64dda0dcede526
Marcos Tullyo <marcostullyo@ufersa.edu.br>
2023-03-02 23:49:28 -0300

    update mapping file

--- packages/subgraph/src/mappings/pool-metadata-registry.ts
+++ packages/subgraph/src/mappings/pool-metadata-registry.ts
@@ -1,18 +1,18 @@
-import { PoolMetadataUpdated as PoolMetadataUpdatedEvent } from "../../generated/PoolMetadataRegistry/PoolMetadataRegistry"
-import { PoolMetadataUpdated } from "../../generated/schema"
+import { PoolMetadataUpdated as PoolMetadataUpdatedEvent } from "../types/PoolMetadataRegistry/PoolMetadataRegistry"
 
 export function handlePoolMetadataUpdated(
   event: PoolMetadataUpdatedEvent
 ): void {
-  let entity = new PoolMetadataUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.poolId = event.params.poolId
-  entity.metadataCID = event.params.metadataCID
+  // TODO: https://linear.app/bleu-llc/issue/BAL-36/create-mapping-for-handling-metadata-updates-events
+  // const entity = new PoolMetadataUpdated(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32())
+  // )
+  // entity.poolId = event.params.poolId
+  // entity.metadataCID = event.params.metadataCID
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
 
-  entity.save()
+  // entity.save()
 }

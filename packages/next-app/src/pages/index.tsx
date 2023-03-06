9199eb411fd403245be7eb1cbe26427e2cce09f8
Joao Victor Assis da Silveira <joaovictorassisdasilveira@Joaos-MacBook-Air.local>
2023-03-06 17:09:29 -0300

    next-app(chore): set gql client endpoint when user select network

--- packages/next-app/src/pages/index.tsx
+++ packages/next-app/src/pages/index.tsx
@@ -1,15 +1,34 @@
 import { Stack } from "@chakra-ui/react";
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import { useAccount, useNetwork } from "wagmi";
 
-import { Header, Sidebar } from "../components";
+import { Header, Hero, Sidebar } from "../components";
 
 function Page() {
-  return (
+  const { chain } = useNetwork();
+  const { isConnected } = useAccount();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("networkId", chain?.id ? String(chain.id) : "1");
+    if (mounted && chain) {
+      Router.reload();
+    }
+  }, [chain]);
+
+  useEffect(() => setMounted(true), []);
+
+  return isConnected ? (
     <>
       <Header />
       <Stack h="100%" w="100%" background="gray.900">
         <Sidebar />
       </Stack>
     </>
+  ) : (
+    <Hero />
   );
 }
 

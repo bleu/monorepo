3c11ab6dce430c9f1963192448d0238af8a57ff0
Joao Victor Assis da Silveira <joaovictorassisdasilveira@Joaos-MacBook-Air.local>
2023-03-06 18:22:09 -0300

    next-app(linting): format lib/gql file

--- packages/next-app/src/lib/gql.ts
+++ packages/next-app/src/lib/gql.ts
@@ -1,21 +1,27 @@
 import { GraphQLClient } from "graphql-request";
-import { ENDPOINTS, Network } from "@balancer-pool-metadata/balancer-gql/codegen";
+import {
+  ENDPOINTS,
+  Network,
+} from "@balancer-pool-metadata/balancer-gql/codegen";
 import { getSdkWithHooks as mainnetSdk } from "@balancer-pool-metadata/balancer-gql/src/gql/__generated__/mainnet";
 import { getSdkWithHooks as goerliSdk } from "@balancer-pool-metadata/balancer-gql/src/gql/__generated__/goerli";
 import { getSdkWithHooks as polygonSdk } from "@balancer-pool-metadata/balancer-gql/src/gql/__generated__/polygon";
 import { getSdkWithHooks as arbitrumSdk } from "@balancer-pool-metadata/balancer-gql/src/gql/__generated__/arbitrum";
 
-const currentNetwork = typeof localStorage !== "undefined" ? localStorage.getItem('networkId') ?? "1" : "1"
+const currentNetwork =
+  typeof localStorage !== "undefined"
+    ? localStorage.getItem("networkId") ?? "1"
+    : "1";
 
 function networkFor(key: string | number): Network {
   switch (key.toString()) {
-    case '1':
+    case "1":
       return Network.mainnet;
-    case '5':
+    case "5":
       return Network.goerli;
-    case '137':
+    case "137":
       return Network.polygon;
-    case '42161':
+    case "42161":
       return Network.arbitrum;
     default:
       return Network.mainnet;
@@ -29,10 +35,7 @@ const networkSdks = {
   [Network.goerli]: goerliSdk,
 };
 
-
-const client = new GraphQLClient(
-  ENDPOINTS[networkFor(currentNetwork)]
-);
+const client = new GraphQLClient(ENDPOINTS[networkFor(currentNetwork)]);
 
 const gql = networkSdks[networkFor(currentNetwork)](client);
 

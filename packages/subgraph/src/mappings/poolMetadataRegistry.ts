4bded16eaa6234db9391d72e0cca0f7d7c119e17
Marcos Tullyo <marcostullyo@ufersa.edu.br>
2023-03-06 23:57:09 -0300

    subgraph: implement mapping handler

--- packages/subgraph/src/mappings/poolMetadataRegistry.ts
+++ packages/subgraph/src/mappings/poolMetadataRegistry.ts
@@ -1,16 +1,16 @@
 import { PoolMetadataUpdated as PoolMetadataUpdatedEvent } from "../types/PoolMetadataRegistry/PoolMetadataRegistry";
+import { PoolMetadataUpdate } from "../types/schema";
 
 export function handlePoolMetadataUpdated(
   event: PoolMetadataUpdatedEvent
 ): void {
-  // TODO: https://linear.app/bleu-llc/issue/BAL-36/create-mapping-for-handling-metadata-updates-events
-  // const entity = new PoolMetadataUpdated(
-  //   event.transaction.hash.concatI32(event.logIndex.toI32())
-  // )
-  // entity.poolId = event.params.poolId
-  // entity.metadataCID = event.params.metadataCID
-  // entity.blockNumber = event.block.number
-  // entity.blockTimestamp = event.block.timestamp
-  // entity.transactionHash = event.transaction.hash
-  // entity.save()
+  const entity = new PoolMetadataUpdate(event.transaction.hash.toHexString());
+  entity.poolId = event.params.poolId.toHexString();
+  entity.metadataCID = event.params.metadataCID;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }

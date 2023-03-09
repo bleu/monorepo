475ff7abdd35cb4476be7cefa53d25def79f1ea8
Marcos Tullyo <mtullyoc@gmail.com>
2023-03-08 23:49:05 -0300

    add pool-metadata-gql to next-app

--- packages/next-app/next.config.js
+++ packages/next-app/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  transpilePackages: ["@balancer-pool-metadata/balancer-gql"],
+  transpilePackages: [
+    "@balancer-pool-metadata/balancer-gql",
+    "@balancer-pool-metadata/pool-metadata-gql",
+  ],
   experimental: {
     appDir: true,
     typedRoutes: true,

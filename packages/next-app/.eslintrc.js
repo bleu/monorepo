04dfdd02dfcfa1502451c595bf8af85b0206cf2c
José Ribeiro <me@joseribeiro.dev>
2023-03-12 20:53:41 -0300

    next-app(chore): remove custom tw class eslint warning

--- packages/next-app/.eslintrc.js
+++ packages/next-app/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "tailwindcss/enforces-shorthand": "warn",
     "tailwindcss/migration-from-tailwind-2": "warn",
     "tailwindcss/no-arbitrary-value": "off",
-    "tailwindcss/no-custom-classname": "error",
+    "tailwindcss/no-custom-classname": "off",
     "tailwindcss/no-contradicting-classname": "error",
   },
 };

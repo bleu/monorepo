fbc2967c8bd186fb9ff8dfe2f2b513f0d6a93666
devjoaov <joaovictorass95@gmail.com>
2023-03-13 19:34:00 -0300

    next-app(fix): change poolId type

--- packages/next-app/src/app/pool/[poolId]/page.tsx
+++ packages/next-app/src/app/pool/[poolId]/page.tsx
@@ -3,7 +3,7 @@ import { MetadataAttributesTable } from "./(components)/MetadataAttributesTable"
 export default function Page({ params }: { params: { poolId: string } }) {
   return (
     <div className="h-full flex-1 py-5 text-white">
-      <MetadataAttributesTable poolId={params.poolId} />
+      <MetadataAttributesTable poolId={params.poolId as `0x${string}`} />
     </div>
   );
 }

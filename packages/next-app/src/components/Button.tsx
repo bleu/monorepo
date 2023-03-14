f99b1fc4fc75a3139e7eb493d8acea0539c74ad2
Marcos Tullyo <mtullyoc@gmail.com>
2023-03-13 22:41:45 -0300

    Function components cannot be given refs. Attempts to access this ref will fail warning

--- packages/next-app/src/components/Button.tsx
+++ packages/next-app/src/components/Button.tsx
@@ -1,12 +1,25 @@
 import cn from "classnames";
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  forwardRef,
+  LegacyRef,
+} from "react";
 
-export function Button({
-  children,
-  className,
-  ...rest
-}: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
+export const Button = forwardRef(function (
+  {
+    children,
+    className,
+    ...rest
+  }: DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >,
+  ref: LegacyRef<HTMLButtonElement>
+) {
   return (
     <button
+      ref={ref}
       {...rest}
       className={cn(
         "rounded-md py-2 px-3 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
@@ -16,4 +29,4 @@ export function Button({
       {children}
     </button>
   );
-}
+});

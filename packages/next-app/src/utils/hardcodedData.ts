2b85e05fe9a58c2875d1a17dbca0b4a7bf482440
luizakp <luiza.puccinelli06@gmail.com>
2023-03-20 18:40:13 +0100

    add hardcoded DAO ops

---
+++ packages/next-app/src/utils/hardcodedData.ts
@@ -0,0 +1,74 @@
+export const hardcodedData = {
+    actions: [
+      {
+        id: 1,
+        name: "Add Reward Tokens",
+        operationResponsible: "Maxis",
+        contractAddress: "0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B",
+        contractUrl:
+          "https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d",
+        fields: [
+          {
+            name: "Name",
+            type: "string",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Set Pool Amp Factor",
+        operationResponsible: "Labs",
+        contractAddress: "0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B",
+        contractUrl:
+          "https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d",
+        fields: [
+          {
+            name: "Name",
+            type: "string",
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Allowlist Pool Gauge",
+        operationResponsible: "Maxis",
+        contractAddress: "0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B",
+        contractUrl:
+          "https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d",
+        fields: [
+          {
+            name: "Name",
+            type: "string",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Set Pool Swap Fee",
+        operationResponsible: "Maxis",
+        contractAddress: "0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B",
+        contractUrl:
+          "https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d",
+        fields: [
+          {
+            name: "Name",
+            type: "string",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Kill Unkill Pool Gauge",
+        operationResponsible: "Maxis",
+        contractAddress: "0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B",
+        contractUrl:
+          "https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d",
+        fields: [
+          {
+            name: "Name",
+            type: "string",
+          },
+        ],
+      },
+    ],
+  };

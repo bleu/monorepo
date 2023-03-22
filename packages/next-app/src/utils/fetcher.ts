eb89624fafb602a97e06e223c1b291d0f181b2b7
devjoaov <53192759+devjoaov@users.noreply.github.com>
2023-03-22 12:03:35 -0300

    [Pool Metadata]: Do not persist pools data when switching networks (#44)
    * next-app(bugfix): do not persist data when switching networks
    
    * next-app(refactor): remove unused getpool function

--- packages/next-app/src/utils/fetcher.ts
+++ packages/next-app/src/utils/fetcher.ts
@@ -1,7 +1,17 @@
+import gql from "#/lib/gql";
+
 export async function fetcher<JSON = unknown>(
   input: RequestInfo,
   init?: RequestInit
 ): Promise<JSON> {
   const res = await fetch(input, init);
   return res.json();
+}
+
+export async function fetchOwnedPools(address: string, chainId: string) {
+  if (!address) return { pools: [] };
+  const data = await gql(chainId).Pool({
+    owner: address,
+  });
+  return data;
 }

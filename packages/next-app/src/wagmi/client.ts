2744936003dbe67832d2b16227fd1fe467be43df
José Ribeiro <me@joseribeiro.dev>
2023-03-22 13:51:30 -0300

    patch add safe connector

--- packages/next-app/src/wagmi/client.ts
+++ packages/next-app/src/wagmi/client.ts
@@ -1,10 +1,10 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient } from "wagmi";
-import { arbitrum, goerli, mainnet, polygon } from "wagmi/chains";
+import { arbitrum, gnosis, goerli, mainnet, polygon } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, polygon, arbitrum, goerli],
+  [mainnet, polygon, arbitrum, goerli, gnosis],
   [publicProvider()]
 );
 

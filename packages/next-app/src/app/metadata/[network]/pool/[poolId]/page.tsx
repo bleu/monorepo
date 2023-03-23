1d73f417396cf1deb48d2772202e192107516198
devjoaov <53192759+devjoaov@users.noreply.github.com>
2023-03-23 11:56:03 -0300

    [Pool Metadata] Allow accessing a pool page with no wallet connected (#46)
    * next-app(refactor): remove wagmi autoconnect
    
    * next-app(refactor): refact layout and page to allow visualize metadata without connecting wallet
    
    * next-app(chore): get network id from network name
    
    * next-app(chore): move /pool to /[network]/pool to specify network in the url
    
    * next-app(refactor): remove metadata fetching from TransactionDialog

---
+++ packages/next-app/src/app/metadata/[network]/pool/[poolId]/page.tsx
@@ -0,0 +1,18 @@
+import { Network } from "@balancer-pool-metadata/balancer-gql/codegen";
+
+import { MetadataAttributesTable } from "./(components)/MetadataAttributesTable";
+
+export default function Page({
+  params,
+}: {
+  params: { poolId: `0x${string}`; network: Network };
+}) {
+  return (
+    <div className="h-screen flex-1 py-5 text-white">
+      <MetadataAttributesTable
+        poolId={params.poolId}
+        network={params.network}
+      />
+    </div>
+  );
+}

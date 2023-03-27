89909582195b792d34f66d53cef15579b470175c
devjoaov <joaovictorass95@gmail.com>
2023-03-27 11:02:29 -0300

    next-app(chore): create toast nofification component

--- packages/next-app/tailwind.config.js
+++ packages/next-app/tailwind.config.js
@@ -35,6 +35,15 @@ module.exports = {
         slideRightAndFade: {
           from: { opacity: 0, transform: "translateX(2px)" },
           to: { opacity: 1, transform: "translateX(0)" },
+        slideIn: {
+          from: {
+            transform: "translateX(calc(100% + var(--viewport-padding)))",
+          },
+          to: { transform: "translateX(0))" },
+        },
+        swipeOut: {
+          from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+          to: { transform: "translateX(calc(100% + var(--viewport-padding)))" },
         },
       },
       animation: {
@@ -44,6 +53,9 @@ module.exports = {
         slideLeftAndFade: "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
         slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
         slideRightAndFade: "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        hide: "hide 100ms ease-in",
+        slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        swipeOut: "swipeOut 100ms ease-out",
       },
     },
   },

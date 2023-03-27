0dddfc6aee3c17f5da0a9c449a1dbeaade52e47a
devjoaov <joaovictorass95@gmail.com>
2023-03-27 16:43:57 -0300

    contracts(chore): implement access control in PoolMetadataRegistry using PoolMetadataAuthentication

--- packages/contracts/src/PoolMetadataAuthorization.sol
+++ packages/contracts/src/PoolMetadataAuthorization.sol
@@ -2,20 +2,20 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "balancer-v2-monorepo/pkg/interfaces/contracts/vault/IAuthorizer.sol";
-import "balancer-v2-monorepo/pkg/solidity-utils/contracts/helpers/Authentication.sol";
+import "./PoolMetadataAuthentication.sol";
 import {BasePoolAuthorization} from "balancer-v2-monorepo/pkg/pool-utils/contracts/BasePoolAuthorization.sol";
 
-abstract contract PoolMetadataAuthorization is Authentication {
+abstract contract PoolMetadataAuthorization is PoolMetadataAuthentication {
     address internal constant _DELEGATE_OWNER = 0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B;
 
     /// @notice Determines whether an action can be performed by a given account
     /// @param actionId The ID of the action to be performed
     /// @param account The account to check for authorization
     /// @return bool Returns true if the action can be performed, otherwise false
-    function _canPerform(bytes32 actionId, address account) internal view override returns (bool) {
-        if (_getOwner() != _DELEGATE_OWNER) {
+    function _canPerform(bytes32 actionId, bytes32 poolId, address account) internal view override returns (bool) {
+        if (_getOwner(poolId) != _DELEGATE_OWNER) {
             // Only the owner can perform "owner only" actions, unless the owner is delegated.
-            return msg.sender == _getOwner();
+            return msg.sender == _getOwner(poolId);
         } else {
             // Non-owner actions are always processed via the Authorizer, as "owner only" ones are when delegated.
             return _getAuthorizer().canPerform(actionId, account, address(this));
@@ -24,7 +24,7 @@ abstract contract PoolMetadataAuthorization is Authentication {
 
     /// @notice Returns the owner of the pool that's currently being operated on
     /// @return The owner of the pool
-    function _getOwner() internal view virtual returns (address);
+    function _getOwner(bytes32 poolId) internal view virtual returns (address);
 
     /// @notice Returns the authorizer contract for managing access control
     /// @return The authorizer contract
--- packages/contracts/src/PoolMetadataRegistry.sol
+++ packages/contracts/src/PoolMetadataRegistry.sol
@@ -2,10 +2,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "balancer-v2-monorepo/pkg/interfaces/contracts/vault/IVault.sol";
-import "balancer-v2-monorepo/pkg/solidity-utils/contracts/helpers/Authentication.sol";
+import "./PoolMetadataAuthentication.sol";
 import {BasePoolAuthorization} from "balancer-v2-monorepo/pkg/pool-utils/contracts/BasePoolAuthorization.sol";
 import "balancer-v2-monorepo/pkg/interfaces/contracts/vault/IAuthorizer.sol";
-
 import "./PoolMetadataAuthorization.sol";
 
 interface IPoolMetadataRegistry {
@@ -18,23 +17,14 @@ interface IPoolMetadataRegistry {
 /// @notice This contract provides a registry for Balancer's Pools metadata
 contract PoolMetadataRegistry is IPoolMetadataRegistry, PoolMetadataAuthorization {
     IVault private immutable _vault;
-    bytes32 private _poolId;
     mapping(bytes32 => string) public poolIdMetadataCIDMap;
 
-    constructor(IVault vault) Authentication(bytes32(uint256(address(this)))) PoolMetadataAuthorization() {
+    constructor(IVault vault) PoolMetadataAuthentication(bytes32(uint256(address(this)))) PoolMetadataAuthorization() {
         _vault = vault;
     }
 
     modifier onlyRegisteredPool(bytes32 poolId) {
         require(_isPoolRegistered(poolId), "Pool not registered");
-        _;
-    }
-
-    /// @notice setPoolId modifier is used to set the pool ID before the authenticate modifier is called.
-    /// The authenticate modifier then calls the canPerform function,
-    /// which in turn calls the _getOwner function that requires the pool ID.
-    modifier setPoolId(bytes32 poolId) {
-        _poolId = poolId;
         _;
     }
 
@@ -57,8 +47,8 @@ contract PoolMetadataRegistry is IPoolMetadataRegistry, PoolMetadataAuthorizatio
 
     /// @notice Returns the owner of the pool that's currently being operated on
     /// @return The owner of the pool
-    function _getOwner() internal view override returns (address) {
-        (address pool,) = _vault.getPool(_poolId);
+    function _getOwner(bytes32 poolId) internal view override returns (address) {
+        (address pool,) = _vault.getPool(poolId);
         return BasePoolAuthorization(pool).getOwner();
     }
 
@@ -68,8 +58,7 @@ contract PoolMetadataRegistry is IPoolMetadataRegistry, PoolMetadataAuthorizatio
     function setPoolMetadata(bytes32 poolId, string memory metadataCID)
         public
         onlyRegisteredPool(poolId)
-        setPoolId(poolId)
-        authenticate
+        authenticate(poolId)
     {
         poolIdMetadataCIDMap[poolId] = metadataCID;
         emit PoolMetadataUpdated(poolId, metadataCID);

fb4045c59436dca1bf2dc282c88963e27d11a367
devjoaov <joaovictorass95@gmail.com>
2023-03-29 09:43:24 -0300

    next-app(chore): create and add footer component to pool metadata layout

---
+++ packages/next-app/src/components/Footer.tsx
@@ -0,0 +1,47 @@
+import { DiscordLogoIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export function Footer({
+  githubLink,
+  discordLink,
+}: {
+  githubLink: string;
+  discordLink: string;
+}) {
+  return (
+    <footer className="mt-auto flex border-t border-gray-700 bg-gray-800 px-6 py-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-300 sm:text-center">
+        Proudly powered by{" "}
+        <a
+          target="_blank"
+          href="https://github.com/bleu-studio"
+          className="font-bold text-blue-500"
+        >
+          Bleu Studio
+        </a>{" "}
+        through Balancer Grants
+      </span>
+      <div className="mt-4 flex space-x-6 sm:justify-center md:mt-0">
+        {githubLink && (
+          <a
+            target="_blank"
+            href={githubLink}
+            className="text-gray-300 hover:text-white"
+          >
+            <GitHubLogoIcon width={30} height={30} />
+            <span className="sr-only">GitHub account</span>
+          </a>
+        )}
+        {discordLink && (
+          <a
+            target="_blank"
+            href={discordLink}
+            className="text-gray-300 hover:text-white"
+          >
+            <DiscordLogoIcon width={30} height={30} />
+            <span className="sr-only">GitHub account</span>
+          </a>
+        )}
+      </div>
+    </footer>
+  );
+}

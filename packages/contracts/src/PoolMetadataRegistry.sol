511799893e151faca2b5c4cfb61fe64c03deef35
devjoaov <53192759+devjoaov@users.noreply.github.com>
2023-03-28 21:46:57 -0300

    [Pool Metadata] update access control flow removing unnecessary inheritance (#50)
    * contracts(chore): implement authentication functionality using PoolMetadataAuthentication contract
    
    * contracts(chore): implement access control in PoolMetadataRegistry using PoolMetadataAuthentication
    
    * contracts(refactor): refactor PoolMetadataRegistry contract to remove unnecessary inheritance and simplify access control logic
    
    * contracts(refactor): use the pool address in _canPerform

--- packages/contracts/src/PoolMetadataRegistry.sol
+++ packages/contracts/src/PoolMetadataRegistry.sol
@@ -2,11 +2,8 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "balancer-v2-monorepo/pkg/interfaces/contracts/vault/IVault.sol";
-import "balancer-v2-monorepo/pkg/solidity-utils/contracts/helpers/Authentication.sol";
 import {BasePoolAuthorization} from "balancer-v2-monorepo/pkg/pool-utils/contracts/BasePoolAuthorization.sol";
 import "balancer-v2-monorepo/pkg/interfaces/contracts/vault/IAuthorizer.sol";
-
-import "./PoolMetadataAuthorization.sol";
 
 interface IPoolMetadataRegistry {
     event PoolMetadataUpdated(bytes32 indexed poolId, string metadataCID);
@@ -16,25 +13,27 @@ interface IPoolMetadataRegistry {
 /// @title A Pool Metadata dApp
 /// @author Bleu LLC
 /// @notice This contract provides a registry for Balancer's Pools metadata
-contract PoolMetadataRegistry is IPoolMetadataRegistry, PoolMetadataAuthorization {
+contract PoolMetadataRegistry is IPoolMetadataRegistry {
     IVault private immutable _vault;
-    bytes32 private _poolId;
+    address internal constant _DELEGATE_OWNER = 0xBA1BA1ba1BA1bA1bA1Ba1BA1ba1BA1bA1ba1ba1B;
     mapping(bytes32 => string) public poolIdMetadataCIDMap;
 
-    constructor(IVault vault) Authentication(bytes32(uint256(address(this)))) PoolMetadataAuthorization() {
+    constructor(IVault vault) {
         _vault = vault;
     }
 
+    /// @notice Reverts unless the pool with the given ID is registered on Balancer.
+    /// @param poolId The ID of the pool to check registration status for.
     modifier onlyRegisteredPool(bytes32 poolId) {
         require(_isPoolRegistered(poolId), "Pool not registered");
         _;
     }
 
-    /// @notice setPoolId modifier is used to set the pool ID before the authenticate modifier is called.
-    /// The authenticate modifier then calls the canPerform function,
-    /// which in turn calls the _getOwner function that requires the pool ID.
-    modifier setPoolId(bytes32 poolId) {
-        _poolId = poolId;
+    /// @notice Reverts unless the caller is allowed to call the function.
+    /// @param poolId The ID of the pool that the caller wants to access.
+    modifier authenticate(bytes32 poolId) {
+        bytes32 actionId = getActionId(msg.sig);
+        require(_canPerform(actionId, poolId, msg.sender), "sender not allowed");
         _;
     }
 
@@ -49,29 +48,39 @@ contract PoolMetadataRegistry is IPoolMetadataRegistry, PoolMetadataAuthorizatio
         }
     }
 
-    /// @notice Returns the authorizer contract for managing access control
-    /// @return The authorizer contract
-    function _getAuthorizer() internal view override returns (IAuthorizer) {
-        return _vault.getAuthorizer();
-    }
-
-    /// @notice Returns the owner of the pool that's currently being operated on
-    /// @return The owner of the pool
-    function _getOwner() internal view override returns (address) {
-        (address pool,) = _vault.getPool(_poolId);
-        return BasePoolAuthorization(pool).getOwner();
-    }
-
     /// @notice Updates the pool metadata CID
     /// @param poolId The pool ID to update the metadata
     /// @param metadataCID The metadataCID related to the new pool metadata
     function setPoolMetadata(bytes32 poolId, string memory metadataCID)
         public
         onlyRegisteredPool(poolId)
-        setPoolId(poolId)
-        authenticate
+        authenticate(poolId)
     {
         poolIdMetadataCIDMap[poolId] = metadataCID;
         emit PoolMetadataUpdated(poolId, metadataCID);
+    }
+
+    /// @notice Computes the action ID for a given function selector.
+    /// @param selector The function selector to compute the action ID for.
+    /// @return The computed action ID.
+    function getActionId(bytes4 selector) internal view returns (bytes32) {
+        return keccak256(abi.encodePacked(bytes32(uint256(address(this))), selector));
+    }
+
+    /// @notice Determines whether an action can be performed by a given account
+    /// @param actionId The ID of the action to be performed
+    /// @param account The account to check for authorization
+    /// @return bool Returns true if the action can be performed, otherwise false
+    function _canPerform(bytes32 actionId, bytes32 poolId, address account) internal view returns (bool) {
+        (address pool,) = _vault.getPool(poolId);
+        address poolOwner = BasePoolAuthorization(pool).getOwner();
+
+        if (poolOwner != _DELEGATE_OWNER) {
+            // Only the owner can perform "owner only" actions, unless the owner is delegated.
+            return msg.sender == poolOwner;
+        } else {
+            // Non-owner actions are always processed via the Authorizer, as "owner only" ones are when delegated.
+            return _vault.getAuthorizer().canPerform(actionId, account, pool);
+        }
     }
 }

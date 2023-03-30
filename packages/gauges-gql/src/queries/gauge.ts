a8394bdaeb0eae0d935dc637442c9fbc3404858f
luizakp <luiza.puccinelli06@gmail.com>
2023-03-30 17:30:47 -0300

    minor fix

--- packages/gauges-gql/src/queries/gauge.ts
+++ packages/gauges-gql/src/queries/gauge.ts
@@ -1,10 +1,9 @@
 import { gql } from "graphql-tag";
 
 export const gaugeSymbol = gql`
-  query Gauge($gaugeId: ID!){
+  query Gauge($gaugeId: ID!) {
     liquidityGauge(id: $gaugeId) { 
       symbol 
     }
   }
 `
-

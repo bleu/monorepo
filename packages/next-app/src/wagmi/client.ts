26afc8dc1979f7c364a3d3808385540e12f419ec
José Ribeiro <me@joseribeiro.dev>
2023-03-31 14:50:06 -0300

    remove unsupported chains

--- packages/next-app/src/wagmi/client.ts
+++ packages/next-app/src/wagmi/client.ts
@@ -1,10 +1,10 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient } from "wagmi";
-import { arbitrum, gnosis, goerli, mainnet, polygon } from "wagmi/chains";
+import { goerli, polygon } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, polygon, arbitrum, goerli, gnosis],
+  [polygon, goerli],
   [publicProvider()]
 );
 

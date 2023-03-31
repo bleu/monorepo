9c452b151389fb019f11fa3f020df0901ec4e6b6
José Ribeiro <me@joseribeiro.dev>
2023-03-31 08:31:46 -0300

    add deploy to polygon

--- packages/next-app/src/wagmi/setPoolMetadata.ts
+++ packages/next-app/src/wagmi/setPoolMetadata.ts
@@ -2,12 +2,18 @@ import { prepareWriteContract, writeContract } from "@wagmi/core";
 
 import { poolMetadataRegistryABI } from "./generated";
 
+const METADATA_CONTRACT_NETWORK_MAP = {
+  1: "0xebfadf723e077c80f6058dc9c9202bb613de07cf",
+  137: "0x68fd16B6D2D1D4AA042009872b08f3756Cc76261",
+} as const;
+
 export async function writeSetPoolMetadata(
   poolId: `0x${string}`,
-  metadataCID: string
+  metadataCID: string,
+  networkId?: keyof typeof METADATA_CONTRACT_NETWORK_MAP
 ) {
   const config = await prepareWriteContract({
-    address: "0xebfadf723e077c80f6058dc9c9202bb613de07cf",
+    address: METADATA_CONTRACT_NETWORK_MAP[networkId || 1],
     abi: poolMetadataRegistryABI,
     functionName: "setPoolMetadata",
     args: [poolId, metadataCID],

b84f39c90f6c42ce5d5eb1c6f7ee716cc01f23d3
devjoaov <joaovictorass95@gmail.com>
2023-04-03 10:20:26 -0300

    next-app(chore): add search pool to header

--- packages/next-app/src/components/Header.tsx
+++ packages/next-app/src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { ReactNode } from "react";
 import { UrlObject } from "url";
 
 import { CustomConnectButton } from "./CustomConnectButton";
@@ -10,17 +11,19 @@ interface IHeader {
   linkUrl: UrlObject | __next_route_internal_types__.RouteImpl<string>;
   imageSrc?: string;
   title: string;
+  children?: ReactNode;
 }
 
-export function Header({ linkUrl, imageSrc, title }: IHeader) {
+export function Header({ linkUrl, imageSrc, title, children }: IHeader) {
   return (
     <div className="flex h-20 flex-wrap items-center justify-between border-b border-gray-700 bg-gray-800 p-4 text-white">
       <Link href={linkUrl} className="mr-5 flex items-center gap-3">
         {imageSrc && <Image src={imageSrc} height={50} width={50} alt="" />}
-        <h1 className="flex gap-2 text-4xl font-thin not-italic leading-8 text-gray-200">
+        <h1 className="flex gap-2 text-xl font-thin not-italic leading-8 text-gray-200">
           Balancer <p className="font-medium">{title}</p>
         </h1>
       </Link>
+      {children && children}
       <CustomConnectButton />
     </div>
   );

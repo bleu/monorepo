18a0aa6f4f227b3f963b16d148776b5b8338beab
luizakp <luiza.puccinelli06@gmail.com>
2023-04-03 11:46:04 -0300

    add input error to Input compoennt

--- packages/next-app/src/components/Input.tsx
+++ packages/next-app/src/components/Input.tsx
@@ -2,18 +2,22 @@
 
 import React, { HTMLProps } from "react";
 
-export const Input = React.forwardRef<
-  HTMLInputElement,
-  HTMLProps<HTMLInputElement>
->(({ label, ...rest }: React.HTMLProps<HTMLInputElement>, ref) => {
-  return (
-    <div className="mb-4">
-      <label className="mb-2 block text-sm text-gray-400">{label}</label>
-      <input
-        ref={ref}
-        {...rest}
-        className="selection:color-white box-border inline-flex h-[35px] w-full appearance-none items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] shadow-blackA9 outline-none selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] disabled:bg-blackA9"
-      />
-    </div>
-  );
-});
+interface IInput extends HTMLProps<HTMLInputElement> {
+  errorMessage?: string;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, IInput>(
+  ({ label, errorMessage, ...rest }: IInput, ref) => {
+    return (
+      <div className="mb-4">
+        <label className="mb-2 block text-sm text-gray-400">{label}</label>
+        <input
+          ref={ref}
+          {...rest}
+          className="selection:color-white box-border inline-flex h-[35px] w-full appearance-none items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] shadow-blackA9 outline-none selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] disabled:bg-blackA9"
+        />
+        {errorMessage !== undefined && <span>{errorMessage}</span>}
+      </div>
+    );
+  }
+);

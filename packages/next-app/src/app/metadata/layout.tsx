7c40a78dc0541380cd92d7c8f57274bc54890f7c
devjoaov <53192759+devjoaov@users.noreply.github.com>
2023-04-04 16:20:21 -0300

    [Pool Metadata] creates search pool button and form (#61)
    * next-app(chore): add pool address query
    
    * next-app(chore): create search pool form
    
    * next-app(chore): add search pool to header
    
    * next-app(refactor): rename search pool input

--- packages/next-app/src/app/metadata/layout.tsx
+++ packages/next-app/src/app/metadata/layout.tsx
@@ -1,11 +1,14 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import type { Metadata } from "next";
 import * as React from "react";
 
 import balancerSymbol from "#/assets/balancer-symbol.svg";
+import { Dialog } from "#/components/Dialog";
 import { Footer } from "#/components/Footer";
 import { Header } from "#/components/Header";
 
 import { MetadataProvider } from "./(components)/MetadataProvider";
+import SearchPoolForm from "./(components)/SearchPoolForm";
 
 export const metadata: Metadata = {
   title: "Balancer Pool Metadata",
@@ -19,7 +22,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         linkUrl={"/metadata"}
         title={"Pool Metadata"}
         imageSrc={balancerSymbol}
-      />
+      >
+        <Dialog title={"Search pool"} content={<SearchPoolForm />}>
+          <div className="pointer-events-auto relative rounded bg-white">
+            <button
+              type="button"
+              className="flex w-full items-center rounded-md py-1.5 pl-2 pr-3 text-sm leading-6 text-slate-400 shadow-sm ring-1 ring-slate-900/10 hover:ring-slate-300 lg:flex"
+            >
+              <MagnifyingGlassIcon
+                width="21"
+                height="21"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1}
+                className="mr-3 flex-none"
+              />
+              Search pool...
+            </button>
+          </div>
+        </Dialog>
+      </Header>
       <MetadataProvider>{children}</MetadataProvider>
       <Footer
         githubLink="https://github.com/bleu-studio/balancer-pool-metadata"

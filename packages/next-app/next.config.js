4dc904be7a0a5e30fe8d9bebe3e9fe0bee6c4efa
José Ribeiro <me@joseribeiro.dev>
2023-04-05 13:59:02 -0300

    remove unused gql packages from next-app

--- packages/next-app/next.config.js
+++ packages/next-app/next.config.js
@@ -2,9 +2,7 @@
 
 module.exports = {
   transpilePackages: [
-    "@balancer-pool-metadata/balancer-gql",
     "@balancer-pool-metadata/gql",
-    "@balancer-pool-metadata/gauges-gql",
     "@balancer-pool-metadata/schema",
   ],
   experimental: {

0feafb17796a207605438c023b17e828e8301d73
José Ribeiro <me@joseribeiro.dev>
2023-04-05 13:15:54 -0300

    start consolidating gql packages into standalone package

--- packages/next-app/next.config.js
+++ packages/next-app/next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   transpilePackages: [
     "@balancer-pool-metadata/balancer-gql",
-    "@balancer-pool-metadata/pool-metadata-gql",
+    "@balancer-pool-metadata/gql",
     "@balancer-pool-metadata/gauges-gql",
     "@balancer-pool-metadata/schema",
   ],

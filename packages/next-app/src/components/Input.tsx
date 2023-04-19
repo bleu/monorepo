887b18cb5a0fe007c38615b265d8d9e4801c7654
luizakp <luiza.puccinelli06@gmail.com>
2023-04-19 18:23:39 -0300

    remove input custom width

--- packages/next-app/src/components/Input.tsx
+++ packages/next-app/src/components/Input.tsx
@@ -5,11 +5,10 @@ import React, { HTMLProps } from "react";
 
 interface IInput extends HTMLProps<HTMLInputElement> {
   errorMessage?: string;
-  customWidth?: string;
 }
 
 export const Input = React.forwardRef<HTMLInputElement, IInput>(
-  ({ label, errorMessage, customWidth, ...rest }: IInput, ref) => {
+  ({ label, errorMessage, ...rest }: IInput, ref) => {
     return (
       <div className="mb-4 flex flex-col">
         <label className="mb-2 block text-sm text-slate12">{label}</label>
@@ -17,8 +16,7 @@ export const Input = React.forwardRef<HTMLInputElement, IInput>(
           ref={ref}
           {...rest}
           className={cn(
-            "selection:color-white box-border inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] bg-blue4 px-[10px] text-[15px] leading-none text-slate12 shadow-[0_0_0_1px] shadow-blue6 outline-none selection:bg-blue9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] disabled:bg-blue1",
-            customWidth ? customWidth : "w-full"
+            "w-full selection:color-white box-border inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] bg-blue4 px-[10px] text-[15px] leading-none text-slate12 shadow-[0_0_0_1px] shadow-blue6 outline-none selection:bg-blue9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] disabled:bg-blue1"
           )}
         />
         <div className="h-6 mt-1 text-red-800">

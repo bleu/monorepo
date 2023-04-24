f608c3ccad8d7c555988b373caef436de6cb491e
luizakp <luiza.puccinelli06@gmail.com>
2023-04-24 11:40:43 -0300

    add internal manager wallet not connected

--- packages/next-app/src/app/internalmanager/[network]/[operationKind]/[tokenAddress]/page.tsx
+++ packages/next-app/src/app/internalmanager/[network]/[operationKind]/[tokenAddress]/page.tsx
@@ -106,7 +106,7 @@ export default function Page({
   });
 
   if (!isConnected && !isReconnecting && !isConnecting) {
-    return <WalletNotConnected isInternal />;
+    return <WalletNotConnected isInternalManager />;
   }
 
   if (isConnecting || isReconnecting || !token.tokenInfo) {
--- packages/next-app/src/components/WalletNotConnected.tsx
+++ packages/next-app/src/components/WalletNotConnected.tsx
@@ -5,20 +5,25 @@ import ConnectWalletImage from "#/assets/connect-wallet.svg";
 
 import { Dialog } from "./Dialog";
 
-export default function WalletNotConnected() {
+export default function WalletNotConnected({
+  isInternalManager = false,
+}: {
+  isInternalManager?: boolean;
+}) {
   return (
     <div className="w-full rounded-3xl items-center py-16 px-12 md:py-20 flex flex-col">
       <div className="text-center text-amber9 text-3xl">
         Please connect your wallet
       </div>
-      <div className="text-center text-slate11 text-lg">
-        {/* TODO: */}
-        Alternatively, open a pool directly clicking&nbsp;
-        <Dialog title="Go to pool" content={<SearchPoolForm />}>
-          <span className="cursor-pointer text-slate12"> here</span>
-        </Dialog>
-      </div>
-
+      {!isInternalManager && (
+        <div className="text-center text-slate11 text-lg">
+          {/* TODO: */}
+          Alternatively, open a pool directly clicking&nbsp;
+          <Dialog title="Go to pool" content={<SearchPoolForm />}>
+            <span className="cursor-pointer text-slate12"> here</span>
+          </Dialog>
+        </div>
+      )}
       <Image src={ConnectWalletImage} height={500} width={500} alt="" />
     </div>
   );

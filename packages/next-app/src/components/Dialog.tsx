7ff89c1ac4089474c54fdd7c8fb5f02918dd1198
José Ribeiro <me@joseribeiro.dev>
2023-05-03 20:35:05 -0300

    add overflow-y-auto to Dialog content

--- packages/next-app/src/components/Dialog.tsx
+++ packages/next-app/src/components/Dialog.tsx
@@ -29,7 +29,7 @@ export function Dialog({
         />
         <DialogPrimitive.Content
           className={cn(
-            "data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-blue3 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none",
+            "data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-blue3 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none  overflow-y-auto",
             customWidth ? customWidth : "w-[90vw] max-w-[450px]"
           )}
         >

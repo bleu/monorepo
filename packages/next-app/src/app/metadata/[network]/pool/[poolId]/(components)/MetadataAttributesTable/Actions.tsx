ee3489953a2ee172c4f7be38a9b2505b2b753497
José Ribeiro <me@joseribeiro.dev>
2023-05-03 20:25:45 -0300

    determine permission through prepared write instead of heuristic

--- packages/next-app/src/app/metadata/[network]/pool/[poolId]/(components)/MetadataAttributesTable/Actions.tsx
+++ packages/next-app/src/app/metadata/[network]/pool/[poolId]/(components)/MetadataAttributesTable/Actions.tsx
@@ -15,12 +15,12 @@ import { TransactionModal } from "./TransactionModal";
 
 export function Actions({
   poolId,
-  isOwner,
+  canEditMetadata,
   metadataUpdated,
   isMetadataValid,
 }: {
   poolId: `0x${string}`;
-  isOwner: boolean;
+  canEditMetadata: boolean;
   metadataUpdated: boolean;
   isMetadataValid: boolean;
 }) {
@@ -39,23 +39,27 @@ export function Actions({
   return (
     <div className="mt-5 w-full justify-between sm:flex sm:items-center">
       <div className="flex gap-4">
-        <Tooltip content="You are not the pool owner" disableTooltip={isOwner}>
+        <Tooltip
+          content="You are not the pool owner"
+          disableTooltip={canEditMetadata}
+        >
           <span tabIndex={0}>
             <Dialog title="Add attribute" content={<PoolMetadataItemForm />}>
-              <Button shade="light" disabled={!isOwner}>
-                Add attribute
-              </Button>
+              <Button shade="light">Add attribute</Button>
             </Dialog>
           </span>
         </Tooltip>
 
-        <Tooltip content="You are not the pool owner" disableTooltip={isOwner}>
+        <Tooltip
+          content="You are not the pool owner"
+          disableTooltip={canEditMetadata}
+        >
           <span tabIndex={0}>
             <Dialog
               title="Add predefined attributes"
               content={<PredefinedMetadataModal />}
             >
-              <Button shade="light" variant="outline" disabled={!isOwner}>
+              <Button shade="light" variant="outline">
                 Add predefined attributes
               </Button>
             </Dialog>
@@ -65,11 +69,11 @@ export function Actions({
 
       <Tooltip
         content={
-          !isOwner
-            ? "You are not the pool owner"
+          !canEditMetadata
+            ? "You are not allowed to update this pool's metadata"
             : "You need to make changes to the metadata first"
         }
-        disableTooltip={isOwner && metadataUpdated && isMetadataValid}
+        disableTooltip={canEditMetadata && metadataUpdated && isMetadataValid}
       >
         <span tabIndex={0}>
           <Dialog
@@ -78,7 +82,9 @@ export function Actions({
           >
             <Button
               color="cyan"
-              disabled={!metadataUpdated || !isOwner || !isMetadataValid}
+              disabled={
+                !metadataUpdated || !canEditMetadata || !isMetadataValid
+              }
             >
               Update metadata
             </Button>

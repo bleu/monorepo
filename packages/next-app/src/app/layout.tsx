89d7e1db5c112732b234546130a2a878b5afbe6b
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-05-04 18:41:48 -0300

    change body backround to match footer and header

--- packages/next-app/src/app/layout.tsx
+++ packages/next-app/src/app/layout.tsx
@@ -9,7 +9,7 @@ import { RootLayout } from "#/components/RootLayout";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
-      <body className="h-full flex flex-col">
+      <body className="h-full flex flex-col bg-blue2">
         <RootLayout>{children}</RootLayout>
         <Footer
           githubLink="https://github.com/bleu-studio/balancer-pool-metadata"

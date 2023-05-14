3856ffa0e8e749f20206058d99db392936a24749
luizakp <luiza.puccinelli06@gmail.com>
2023-05-14 13:36:24 -0300

    fix: add new chains on impersonateWhetherDAO function

--- packages/next-app/src/lib/gql/server.ts
+++ packages/next-app/src/lib/gql/server.ts
@@ -19,6 +19,9 @@ export function impersonateWhetherDAO(
   if (
     network !== Network.Goerli &&
     network !== Network.Sepolia &&
+    network !== Network.Gnosis &&
+    network !== Network.Optimism &&
+    network !== Network.Arbitrum &&
     networkMultisigs[network] === address
   ) {
     return DELEGATE_OWNER;

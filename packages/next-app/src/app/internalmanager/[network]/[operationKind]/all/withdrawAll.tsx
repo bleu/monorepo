ce997839d6bc3ed8227fd69b1f635ee2d9ad6ace
luizakp <luiza.puccinelli06@gmail.com>
2023-05-18 13:35:43 -0300

    fix: Query import

--- packages/next-app/src/app/internalmanager/[network]/[operationKind]/all/withdrawAll.tsx
+++ packages/next-app/src/app/internalmanager/[network]/[operationKind]/all/withdrawAll.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { InternalBalanceQuery } from "@balancer-pool-metadata/gql/src/balancer-internal-manager/__generated__/Mainnet";
+import { InternalBalanceQuery } from "@balancer-pool-metadata/gql/src/balancer-internal-manager/__generated__/Ethereum";
 import { AddressSchema } from "@balancer-pool-metadata/schema";
 import { Address, networkFor } from "@balancer-pool-metadata/shared";
 import { zodResolver } from "@hookform/resolvers/zod";

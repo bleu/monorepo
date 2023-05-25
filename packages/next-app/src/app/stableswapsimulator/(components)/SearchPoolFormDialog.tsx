3594f2d004d1af9af3d5fc0ac5bb2d045034e65d
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-05-25 12:54:58 -0300

    fix: remove push to import pool dialog

--- packages/next-app/src/app/stableswapsimulator/(components)/SearchPoolFormDialog.tsx
+++ packages/next-app/src/app/stableswapsimulator/(components)/SearchPoolFormDialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 import { Dialog } from "#/components/Dialog";
@@ -8,12 +7,10 @@ import SearchPoolForm, { PoolAttribute } from "#/components/SearchPoolForm";
 import { useStableSwap } from "#/contexts/StableSwapContext";
 
 export function SearchPoolFormDialog({ children }: PropsWithChildren) {
-  const { push } = useRouter();
   const { handleImportPoolParametersById } = useStableSwap();
 
   function onSubmit(data: PoolAttribute) {
     handleImportPoolParametersById(data);
-    push("/stableswapsimulator/analysis");
   }
   return (
     <Dialog

44dd7c1ce8a5933d4e12374621ca227b500ec5b2
luizakp <luiza.puccinelli06@gmail.com>
2023-05-29 14:53:33 -0300

    chore: add tabs to charts

--- packages/next-app/src/app/stableswapsimulator/(components)/GraphView.tsx
+++ packages/next-app/src/app/stableswapsimulator/(components)/GraphView.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 import { Spinner } from "#/components/Spinner";
+import { Tabs } from "#/components/Tabs";
+import { useStableSwap } from "#/contexts/StableSwapContext";
 
 const StableCurve = dynamic(() => import("./StableCurve"), {
   ssr: false,
@@ -8,5 +12,41 @@ const StableCurve = dynamic(() => import("./StableCurve"), {
 });
 
 export function GraphView() {
-  return <StableCurve />;
+  const { baselineData, indexAnalysisToken, setIndexCurrentTabToken } =
+    useStableSwap();
+  const tokensSymbol = baselineData.tokens.map((token) => token.symbol);
+  const tabTokens = tokensSymbol.filter(
+    (token, index) => index !== indexAnalysisToken
+  );
+
+  function handleTabClick(event: React.FormEvent<HTMLButtonElement>) {
+    const target = event.target as HTMLButtonElement;
+
+    setIndexCurrentTabToken(tokensSymbol.indexOf(target.innerText));
+  }
+
+  return (
+    <div className="w-full">
+      <Tabs>
+        <Tabs.ItemTriggerWrapper>
+          {tabTokens.map((symbol) => (
+            <Tabs.ItemTrigger
+              tabName={symbol}
+              key={symbol}
+              onClick={handleTabClick}
+            >
+              <span>{symbol}</span>
+            </Tabs.ItemTrigger>
+          ))}
+        </Tabs.ItemTriggerWrapper>
+        {tabTokens.map((symbol) => (
+          <div key={symbol}>
+            <Tabs.ItemContent tabName={symbol} bgColor="blue1">
+              <StableCurve />
+            </Tabs.ItemContent>
+          </div>
+        ))}
+      </Tabs>
+    </div>
+  );
 }

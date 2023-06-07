4c85c7f57baa7c2313c345773568912d09d52540
luizakp <luiza.puccinelli06@gmail.com>
2023-06-07 18:21:52 -0300

    fix: metadata pool search redirect

--- packages/next-app/src/components/WalletNotConnected.tsx
+++ packages/next-app/src/components/WalletNotConnected.tsx
@@ -1,16 +1,27 @@
+import { networkIdEnumMap } from "@balancer-pool-metadata/shared";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useNetwork } from "wagmi";
 
-import handleGoToPool from "#/app/metadata/(handles)/handleGoToPool";
 import ConnectWalletImage from "#/assets/connect-wallet.svg";
 
 import { Dialog } from "./Dialog";
-import { SearchPoolForm } from "./SearchPoolForm";
+import { PoolAttribute, SearchPoolForm } from "./SearchPoolForm";
 
 export default function WalletNotConnected({
   isInternalManager = false,
 }: {
   isInternalManager?: boolean;
 }) {
+  const { chain } = useNetwork();
+  const { push } = useRouter();
+
+  function handleGoToPool(formData: PoolAttribute) {
+    const networkId = formData.network ?? chain?.id.toString();
+    const networkName =
+      networkIdEnumMap[networkId as keyof typeof networkIdEnumMap];
+    push(`/metadata/${networkName}/pool/${formData.poolId}`);
+  }
   return (
     <div className="w-full rounded-3xl items-center py-16 px-12 md:py-20 flex flex-col">
       <div className="text-center text-amber9 text-3xl">

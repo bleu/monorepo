2585791d50504894dfc87bb4b7548053fc547e05
José Ribeiro <me@joseribeiro.dev>
2023-06-12 11:30:27 +0200

    run formatting

--- packages/next-app/tailwind.config.js
+++ packages/next-app/tailwind.config.js
@@ -28,50 +28,50 @@ module.exports = {
       keyframes: {
         hide: {
           from: { opacity: 1 },
-          to: { opacity: 0 }
+          to: { opacity: 0 },
         },
         overlayShow: {
           from: { opacity: 0 },
-          to: { opacity: 1 }
+          to: { opacity: 1 },
         },
         contentShow: {
           from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
-          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" }
+          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
         },
         slideDownAndFade: {
           from: { opacity: 0, transform: "translateY(-2px)" },
-          to: { opacity: 1, transform: "translateY(0)" }
+          to: { opacity: 1, transform: "translateY(0)" },
         },
         slideLeftAndFade: {
           from: { opacity: 0, transform: "translateX(2px)" },
-          to: { opacity: 1, transform: "translateX(0)" }
+          to: { opacity: 1, transform: "translateX(0)" },
         },
         slideUpAndFade: {
           from: { opacity: 0, transform: "translateY(2px)" },
-          to: { opacity: 1, transform: "translateY(0)" }
+          to: { opacity: 1, transform: "translateY(0)" },
         },
         slideRightAndFade: {
           from: { opacity: 0, transform: "translateX(2px)" },
-          to: { opacity: 1, transform: "translateX(0)" }
+          to: { opacity: 1, transform: "translateX(0)" },
         },
         slideIn: {
           from: {
-            transform: "translateX(calc(100% + var(--viewport-padding)))"
+            transform: "translateX(calc(100% + var(--viewport-padding)))",
           },
-          to: { transform: "translateX(0))" }
+          to: { transform: "translateX(0))" },
         },
         swipeOut: {
           from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
-          to: { transform: "translateX(calc(100% + var(--viewport-padding)))" }
+          to: { transform: "translateX(calc(100% + var(--viewport-padding)))" },
         },
         slideDown: {
           from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" }
+          to: { height: "var(--radix-accordion-content-height)" },
         },
         slideUp: {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 }
-        }
+          to: { height: 0 },
+        },
       },
       animation: {
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
@@ -87,9 +87,9 @@ module.exports = {
         slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         swipeOut: "swipeOut 100ms ease-out",
         slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
-        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)"
-      }
-    }
+        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+    },
   },
-  plugins: [require("tailwind-scrollbar")]
+  plugins: [require("tailwind-scrollbar")],
 };

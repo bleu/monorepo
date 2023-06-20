be742105d89eee8e6f2658b7de3639130f0ebfaf
luiza <63298341+luizakp@users.noreply.github.com>
2023-06-20 11:26:18 -0300

    chore: add gnosis icon to rainbowkit (#144)

--- packages/next-app/src/wagmi/client.ts
+++ packages/next-app/src/wagmi/client.ts
@@ -12,8 +12,14 @@ import { publicProvider } from "wagmi/providers/public";
 
 import { configureChains, createClient } from "#/wagmi";
 
+const Gnosis = {
+  ...gnosis,
+  iconUrl:
+    "https://cdn.sanity.io/images/r2mka0oi/production/bf37b9c7fb36c7d3c96d3d05b45c76d89072b777-1800x1800.png",
+};
+
 export const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, polygon, arbitrum, gnosis, optimism, goerli, sepolia],
+  [mainnet, polygon, arbitrum, Gnosis, optimism, goerli, sepolia],
   [publicProvider()]
 );
 

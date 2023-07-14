153752ed5fa4239249c58f498b7b88a10237fc63
luizakp <luiza.puccinelli06@gmail.com>
2023-07-14 16:51:33 -0300

    chore: add Approved step

--- packages/next-app/src/components/TransactionProgressBar.tsx
+++ packages/next-app/src/components/TransactionProgressBar.tsx
@@ -17,6 +17,11 @@ export const STAGE_CN_MAPPING: Record<TransactionStatus, Stage> = {
     transitionLine: "bg-slate10",
     writingOnChainStep: "border-slate10 text-slate12",
   },
+  [TransactionStatus.APPROVED]: {
+    pinningStep: "border-green10 text-green10",
+    transitionLine: "bg-green10",
+    writingOnChainStep: "border-amber10 text-amber10",
+  },
   [TransactionStatus.CONFIRMING]: {
     pinningStep: "border-green10 text-green10",
     transitionLine: "bg-green10",
@@ -33,6 +38,11 @@ export const STAGE_CN_MAPPING: Record<TransactionStatus, Stage> = {
     writingOnChainStep: "border-green10 text-green10",
   },
   [TransactionStatus.PINNING_ERROR]: {
+    pinningStep: "border-tomato10 text-tomato10",
+    transitionLine: "bg-slate10",
+    writingOnChainStep: "border-slate10 text-slate12",
+  },
+  [TransactionStatus.WRITE_ERROR]: {
     pinningStep: "border-tomato10 text-tomato10",
     transitionLine: "bg-slate10",
     writingOnChainStep: "border-slate10 text-slate12",

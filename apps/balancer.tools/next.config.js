c4a8d740b42dcf2258f15106557eae6f648cdb14
José Ribeiro <me@joseribeiro.dev>
2023-07-19 12:40:49 -0300

    fix pino-pretty issue

--- apps/balancer.tools/next.config.js
+++ apps/balancer.tools/next.config.js
@@ -20,6 +20,7 @@ const moduleExports = {
    */
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
   async redirects() {

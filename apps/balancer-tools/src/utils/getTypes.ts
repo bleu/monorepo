c283263da406331f2eedf2e5bd7da7bac982bc2d
luizakp <luiza.puccinelli06@gmail.com>
2023-07-23 17:21:25 -0300

    fix: eslint-disable @typescript-eslint/ban-types on getType.ts

--- apps/balancer-tools/src/utils/getTypes.ts
+++ apps/balancer-tools/src/utils/getTypes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 export type GetDeepProp<T extends object, K extends string> = K extends keyof T
   ? T[K]
   : { [P in keyof T]: GetDeepProp<Extract<T[P], object>, K> }[keyof T];

f57d4e7fedc3c1229f9d635c2cfa2ed511495fd5
luizakp <luiza.puccinelli06@gmail.com>
2023-07-25 12:07:46 -0300

    chore: add dialog when changing the pool type

--- apps/balancer-tools/src/app/poolsimulator/(components)/Menu.tsx
+++ apps/balancer-tools/src/app/poolsimulator/(components)/Menu.tsx
@@ -1,8 +1,10 @@
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Close, DialogClose } from "@radix-ui/react-dialog";
+import { ChevronDownIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import * as Separator from "@radix-ui/react-separator";
 import { usePathname } from "next/navigation";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
+import { Dialog } from "#/components/Dialog";
 import { Select, SelectItem } from "#/components/Select";
 import Sidebar from "#/components/Sidebar";
 import { Spinner } from "#/components/Spinner";
@@ -55,27 +57,71 @@ function AnalysisMenu() {
 }
 
 function IndexMenu() {
-  const { setPoolType } = usePoolSimulator();
-  const onChange = (value: PoolType) => {
-    setPoolType(value);
-  };
+  const { poolType } = usePoolSimulator();
 
   return (
     <SearchPoolFormWithDataForm>
-      <div className="flex flex-col gap-4 mt-4">
+      <div className="flex flex-col mt-4">
         <div className="flex flex-col">
-          <Label className="mb-2 block text-sm text-slate12">Pool type</Label>
-          <Select onValueChange={onChange} defaultValue="MetaStable">
-            {POOL_TYPES.map((type) => (
-              <SelectItem key={type} value={type}>
-                {POOL_TYPES_MAPPER[type]}
-              </SelectItem>
-            ))}
-          </Select>
+          <Dialog content={<SelectPoolType />}>
+            <div className="flex flex-col mb-4">
+              <Label className="mb-2 block text-sm text-slate12">
+                Pool type
+              </Label>
+              <button
+                type="button"
+                className="px-2w-full selection:color-white box-border flex h-[35px] w-full appearance-none items-center justify-between gap-2 rounded-[4px] bg-blue4 px-[10px] py-1 text-[15px] leading-none text-slate12 shadow-[0_0_0_1px] shadow-blue6 outline-none selection:bg-blue9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] disabled:bg-blue1"
+              >
+                <div>{poolType}</div>
+                <ChevronDownIcon />
+              </button>
+            </div>
+          </Dialog>
+          <PoolParamsForm />
         </div>
-        <PoolParamsForm />
       </div>
     </SearchPoolFormWithDataForm>
+  );
+}
+
+function SelectPoolType({}: { close?: () => void }) {
+  const { poolType, setPoolType } = usePoolSimulator();
+  const [selectedType, setSelectedType] = useState<PoolType>(poolType);
+
+  function onChange(value: PoolType) {
+    setSelectedType(value);
+  }
+
+  function onSubmit() {
+    setPoolType(selectedType);
+    Close;
+  }
+  return (
+    <div className="text-slate12 flex flex-col items-center gap-y-4">
+      <div className="text-center flex flex-col items-center">
+        <span>You are about to change the pool type.</span>
+        <span>This action will reset all the parameters.</span>
+      </div>
+      <Select defaultValue={poolType} onValueChange={onChange}>
+        {POOL_TYPES.map((type) => (
+          <SelectItem key={type} value={type}>
+            {POOL_TYPES_MAPPER[type]}
+          </SelectItem>
+        ))}
+      </Select>
+      <div className="flex gap-x-4 py-3">
+        <DialogClose>
+          <span className="bg-transparent text-slate9 border-slate9 hover:bg-slate2 hover:border-slate2 rounded-md py-3 px-5 text-center text-sm font-semibold border focus-visible:outline-blue7 focus-visible:outline-offset-2 disabled:opacity-40">
+            Cancel
+          </span>
+        </DialogClose>
+        <DialogClose onClick={onSubmit}>
+          <span className="bg-blue9 text-slate12 hover:bg-blue10 border-blue9 rounded-md py-3 px-5 text-center text-sm font-semibold border focus-visible:outline-blue7 focus-visible:outline-offset-2 disabled:opacity-40">
+            Confirm
+          </span>
+        </DialogClose>
+      </div>
+    </div>
   );
 }
 

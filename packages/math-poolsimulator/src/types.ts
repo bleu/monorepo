533753c477d6a47cc51dad3d0e01e7b545a4808c
luizakp <luiza.puccinelli06@gmail.com>
2023-07-31 15:15:00 -0300

    fix: math types

--- packages/math-poolsimulator/src/types.ts
+++ packages/math-poolsimulator/src/types.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { OldBigNumber } from "@balancer-labs/sor";
 import { GyroEPoolPairData, IGyroEMaths } from "./gyroE";
 import { IMetaStableMath, MetaStablePoolPairData } from "./metastable";
 
@@ -7,18 +8,24 @@ export type PoolParams = IMetaStableMath | IGyroEMaths;
 
 export interface IAMMFunctionality<TPoolPairData extends PoolPairData> {
   parsePoolPairData(tokenIn: string, tokenOut: string): TPoolPairData;
-  _exactTokenInForTokenOut(poolPairData: TPoolPairData, amountIn: any): any;
-  _tokenInForExactTokenOut(poolPairData: TPoolPairData, amountOut: any): any;
-  _spotPrice(poolPairData: TPoolPairData): any;
+  _exactTokenInForTokenOut(
+    poolPairData: TPoolPairData,
+    amountIn: OldBigNumber
+  ): OldBigNumber;
+  _tokenInForExactTokenOut(
+    poolPairData: TPoolPairData,
+    amountOut: OldBigNumber
+  ): OldBigNumber;
+  _spotPrice(poolPairData: TPoolPairData): OldBigNumber;
   _spotPriceAfterSwapExactTokenInForTokenOut(
     poolPairData: TPoolPairData,
-    amountIn: any
-  ): any;
+    amountIn: OldBigNumber
+  ): OldBigNumber;
   _derivativeSpotPriceAfterSwapExactTokenInForTokenOut(
     poolPairData: TPoolPairData,
-    amountIn: any
-  ): any;
+    amountIn: OldBigNumber
+  ): OldBigNumber;
   _firstGuessOfTokenInForExactSpotPriceAfterSwap(
     poolPairData: TPoolPairData
-  ): any;
+  ): OldBigNumber;
 }

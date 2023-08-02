69e08b47f699cc50cbc03fdff358af1b1765cc13
José Ribeiro <jose@bleu.studio>
2023-08-02 11:41:19 -0300

    add more tests to button, refactor icon classname logic (#176)
    * refactor button icon logic
    
    * add button tests
    
    * fix: use predefined classes on test -applies predefined class for a given color, variant and shade-
    
    ---------
    
    Co-authored-by: luizakp <luiza.puccinelli06@gmail.com>

--- apps/balancer-tools/src/__tests__/components/Button.test.tsx
+++ apps/balancer-tools/src/__tests__/components/Button.test.tsx
@@ -1,8 +1,8 @@
 import { DotIcon } from "@radix-ui/react-icons";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { Button } from "#/components";
-import { ButtonIcon } from "#/components/Button";
+import { ButtonIcon, PREDEFINED_CLASSES } from "#/components/Button";
 
 describe("Button component", () => {
   it("renders without crashing", () => {
@@ -15,14 +15,26 @@ describe("Button component", () => {
     expect(screen.getByText("Test Button")).toBeInTheDocument();
   });
 
-  it("displays ButtonIcon", () => {
-    const { container } = render(
+  it("positions the icon correctly", () => {
+    const { rerender } = render(
       <Button>
         <ButtonIcon icon={<DotIcon />} />
         Test Button
       </Button>
     );
-    expect(container.querySelector("svg")).toBeInTheDocument();
+    const firstIconParent = screen.getByRole("button").querySelector("svg")
+      ?.parentElement;
+    expect(firstIconParent).toHaveClass("mr-2");
+
+    rerender(
+      <Button>
+        Test Button
+        <ButtonIcon icon={<DotIcon />} />
+      </Button>
+    );
+    const secondIconParent = screen.getByRole("button").querySelector("svg")
+      ?.parentElement;
+    expect(secondIconParent).toHaveClass("ml-2");
   });
 
   it("applies predefined class for a given color, variant and shade", () => {
@@ -30,12 +42,12 @@ describe("Button component", () => {
       <Button color="blue" variant="solid" shade="light" />
     );
     expect(screen.getByRole("button")).toHaveClass(
-      "bg-blue9 text-slate12 hover:bg-blue10 border-blue9"
+      PREDEFINED_CLASSES.blue.solid.light
     );
 
     rerender(<Button color="amber" variant="outline" shade="dark" />);
     expect(screen.getByRole("button")).toHaveClass(
-      "bg-transparent text-amber10 border-amber3 hover:bg-amber2 hover:border-amber2"
+      PREDEFINED_CLASSES.amber.outline.dark
     );
   });
 
@@ -47,5 +59,20 @@ describe("Button component", () => {
   it("sets button to disabled state", () => {
     render(<Button disabled />);
     expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  it("renders with default props", () => {
+    const { getByRole } = render(<Button />);
+    expect(getByRole("button")).toHaveClass(
+      PREDEFINED_CLASSES.blue.solid.light,
+      "rounded-md py-3 px-5 text-center text-sm font-semibold border focus-visible:outline-blue7 focus-visible:outline-offset-2 disabled:opacity-40"
+    );
+  });
+
+  it("triggers onClick event", () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick} />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(handleClick).toHaveBeenCalled();
   });
 });

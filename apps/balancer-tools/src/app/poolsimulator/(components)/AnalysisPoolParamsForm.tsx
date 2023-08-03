fc09cbb3f3cca7ec21cad30058a1557aef395854
luizakp <luiza.puccinelli06@gmail.com>
2023-08-03 10:42:41 -0300

    remove: Form from Analysis pool form

--- apps/balancer-tools/src/app/poolsimulator/(components)/AnalysisPoolParamsForm.tsx
+++ apps/balancer-tools/src/app/poolsimulator/(components)/AnalysisPoolParamsForm.tsx
@@ -1,34 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
 
 import { Select, SelectItem } from "#/components/Select";
-import { Form, FormLabel } from "#/components/ui/form";
 import { usePoolSimulator } from "#/contexts/PoolSimulatorContext";
 
 export default function AnalysisPoolParamsForm() {
   const {
     initialData,
-    customData,
     setAnalysisTokenByIndex,
     analysisToken,
     setCurrentTabTokenByIndex,
   } = usePoolSimulator();
-
-  const form = useForm({
-    mode: "onChange",
-  });
-
-  const {
-    setValue,
-    getValues,
-    formState: { errors },
-  } = form;
-
-  const onSubmit = () => {
-    if (Object.keys(errors).length) return;
-  };
 
   useEffect(() => {
     setAnalysisTokenByIndex(0);
@@ -39,19 +22,13 @@ export default function AnalysisPoolParamsForm() {
     ({ symbol }) => symbol.toLowerCase() !== analysisToken.symbol.toLowerCase(),
   );
 
-  useEffect(() => {
-    // TODO: BAL 401
-    if (customData?.tokens == getValues().tokens) return;
-    if (customData?.tokens) setValue("tokens", customData?.tokens);
-  }, [customData?.tokens]);
-
   return (
-    <Form id="variant-data-form" {...form} onSubmit={onSubmit}>
+    <>
       <div className="flex flex-col gap-4">
         <div className="flex flex-col">
-          <FormLabel className="mb-2 block text-sm text-slate12">
+          <span className="mb-2 block text-sm text-slate12">
             Analysis Token
-          </FormLabel>
+          </span>
           <Select
             onValueChange={(i) => {
               if (indexCurrentTabToken === Number(i)) {
@@ -73,6 +50,6 @@ export default function AnalysisPoolParamsForm() {
           </Select>
         </div>
       </div>
-    </Form>
+    </>
   );
 }

daabab0c7e4aecb85bf60d09d453477ac58a4244
Rawallon Cardoso <rawallon@gmail.com>
2023-08-09 01:42:56 -0300

    Dev: Mocked return for DuneAPI

--- apps/balancer-tools/src/app/apr/rounds/[roundId]/route.ts
+++ apps/balancer-tools/src/app/apr/rounds/[roundId]/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { DuneAPI } from "#/lib/dune";
+import { voteGaugeByID } from "../../mock_apis";
 
 export async function GET(
   _: NextRequest,
   context: { params: { roundId: number } },
 ) {
   if (!context.params.roundId) return "Missing round_id";
-  const duneAPI = new DuneAPI();
-
-  const dune_request = await duneAPI.getPoolsByRoundId(context.params.roundId);
+  // const duneAPI = new DuneAPI();
+  // const dune_request = await duneAPI.getPoolsByRoundId(context.params.roundId)
+  const dune_request = voteGaugeByID
   return NextResponse.json(dune_request);
 }

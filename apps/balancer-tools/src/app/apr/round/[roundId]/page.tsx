9122d36bd44db7f1874f2442400348be98e0f02d
Rawallon Cardoso <rawallon@gmail.com>
2023-08-09 01:47:54 -0300

    Added data fetching for Round

--- apps/balancer-tools/src/app/apr/round/[roundId]/page.tsx
+++ apps/balancer-tools/src/app/apr/round/[roundId]/page.tsx
@@ -1,9 +1,46 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Loading from "#/app/metadata/[network]/pool/[poolId]/loading";
+import { Row } from "#/lib/dune";
 
 export default function Page() {
+  const [poolsData, setPoolsData] = useState<Row[]>([]);
+  const [hasError, setHasError] = useState("");
   const { roundId } = useParams();
+  useEffect(() => {
+    if (!poolsData.length) {
+      const fetchData = async () => {
+        const poolsDataRows = await (
+          await fetch(`/apr/rounds/${roundId}`)
+        ).json();
+        if (poolsDataRows instanceof Array) {
+          setPoolsData(poolsDataRows);
+        } else {
+          setHasError(poolsDataRows["error"]);
+        }
+      };
+      fetchData();
+    }
+  }, [roundId]);
+
+  if (!poolsData.length) {
+    return (
+      <div className="flex h-full w-full flex-col justify-center rounded-3xl">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <div className="flex h-full w-full flex-col justify-center rounded-3xl text-white">
+        {hasError}
+      </div>
+    );
+  }
 
   return (
     <div className="flex h-full w-full flex-col justify-center rounded-3xl">

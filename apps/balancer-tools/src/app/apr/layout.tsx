9ea5f6f35f034de9384e795615eb4336cff186fa
Rawallon Cardoso <rawallon@gmail.com>
2023-08-09 16:48:59 -0300

    Format

--- apps/balancer-tools/src/app/apr/layout.tsx
+++ apps/balancer-tools/src/app/apr/layout.tsx
@@ -9,21 +9,21 @@ import Sidebar from "./(components)/sidebar";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex h-full flex-col">
-      <Header
-        linkUrl={"/apr"}
-        title={"Pool Simulator"}
-        imageSrc={"/assets/balancer-symbol.svg"}
-        endButton={<></>}
-      />
-      <div className="flex flex-1 gap-x-8">
-        <div className="h-full w-72 lg:w-80 max-w-sm">
     <SelectedPoolRoundContextProvider>
+      <div className="flex h-full flex-col">
+        <Header
+          linkUrl={"/apr"}
+          title={"Pool Simulator"}
+          imageSrc={"/assets/balancer-symbol.svg"}
+          endButton={<></>}
+        />
+        <div className="flex flex-1 gap-x-8">
+          <div className="h-full w-72 lg:w-80 max-w-sm">
             <Sidebar />
+          </div>
+          <div className="h-full flex-1 py-5 pr-8 text-white">{children}</div>
         </div>
-        <div className="h-full flex-1 py-5 pr-8 text-white">{children}</div>
       </div>
-    </div>
     </SelectedPoolRoundContextProvider>
   );
 }

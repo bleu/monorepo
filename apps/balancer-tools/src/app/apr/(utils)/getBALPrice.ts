d07b7b1ae9955e099db50da6f025cb79eb3d7809
José Ribeiro <me@joseribeiro.dev>
2023-08-08 21:22:52 -0300

    fetch BAL price from coingecko

---
+++ apps/balancer-tools/src/app/apr/(utils)/getBALPrice.ts
@@ -0,0 +1,8 @@
+import { DefiLlamaAPI } from "#/lib/coingecko";
+
+export const getBALPrice = async (timestamp: number) => {
+  const BAL = 'ethereum:0xba100000625a3754423978a60c9317c58a424e3d';
+  const api = new DefiLlamaAPI();
+  const response = await api.getHistoricalPrice(timestamp, [BAL]);
+  return response.coins[BAL].price;
+}

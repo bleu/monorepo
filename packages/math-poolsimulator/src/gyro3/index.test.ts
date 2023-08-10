160a3b977d9162d71d28d8366d62f413dce98dcb
luizakp <luiza.puccinelli06@gmail.com>
2023-08-10 14:37:06 -0300

    chore: add different amounts by balancer percentage to Gyro tests

--- packages/math-poolsimulator/src/gyro3/index.test.ts
+++ packages/math-poolsimulator/src/gyro3/index.test.ts
@@ -1,62 +1,68 @@
 import { bnum, SubgraphPoolBase } from "@balancer-labs/sor";
-import { describe, expect, test } from "@jest/globals";
+import { formatFixed } from "@ethersproject/bignumber";
+import { describe, test } from "@jest/globals";
 
 import { numberToBigNumber } from "../conversions";
 import { AMM } from "../index";
+import { verifyApproximateEquality } from "../utils";
 import poolsFromFile from "./fixtures/data.json";
 import { ExtendedGyro3 } from "./index";
 
 describe("Tests new Gyro 3CLP math function based on package other functions", () => {
   const poolData = poolsFromFile["pool"];
+  const percentages = [25, 50, 75, 95];
 
   const tokenIn = "USDC";
   const tokenOut = "BUSD";
 
-  const amm = new AMM(
-    new ExtendedGyro3({
-      swapFee: poolData.swapFee,
-      totalShares: poolData.totalShares,
-      tokens: poolData.tokens,
-      tokensList: poolData.tokensList,
-      root3Alpha: poolData.root3Alpha,
-    }),
-  );
+  percentages.forEach((percentage) => {
+    describe(`Tests for ${percentage}% with ${tokenIn} as tokenIn`, () => {
+      const amm = new AMM(
+        new ExtendedGyro3({
+          swapFee: poolData.swapFee,
+          totalShares: poolData.totalShares,
+          tokens: poolData.tokens,
+          tokensList: poolData.tokensList,
+          root3Alpha: poolData.root3Alpha,
+        }),
+      );
 
-  const pool = ExtendedGyro3.fromPool(poolData as SubgraphPoolBase);
-  const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
-  const amount = 9104;
-  const amountOldBigNumber = bnum(amount);
-  const spotPriceExpected = pool._spotPriceAfterSwapExactTokenInForTokenOut(
-    poolPairData,
-    amountOldBigNumber,
-  );
-  const amountOut = amm.exactTokenInForTokenOut(amount, tokenIn, tokenOut);
+      const pool = ExtendedGyro3.fromPool(poolData as SubgraphPoolBase);
+      const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
 
-  poolPairData.balanceIn = poolPairData.balanceIn.add(
-    numberToBigNumber({ number: amount }),
-  );
+      const amount =
+        Number(formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut)) *
+        (percentage / 100);
+      const amountOldBigNumber = bnum(amount);
+      const spotPriceExpected = pool._spotPriceAfterSwapExactTokenInForTokenOut(
+        poolPairData,
+        amountOldBigNumber,
+      );
+      const amountOut = amm.exactTokenInForTokenOut(amount, tokenIn, tokenOut);
 
-  poolPairData.balanceOut = poolPairData.balanceOut.sub(
-    numberToBigNumber({ number: amountOut }),
-  );
+      poolPairData.balanceIn = poolPairData.balanceIn.add(
+        numberToBigNumber({ number: amount }),
+      );
 
-  test("_spotPrice", () => {
-    checkResult(
-      pool._spotPrice(poolPairData).toNumber(),
-      spotPriceExpected.toNumber(),
-    );
-  });
+      poolPairData.balanceOut = poolPairData.balanceOut.sub(
+        numberToBigNumber({ number: amountOut }),
+      );
 
-  test("_tokenInForExactSpotPriceAfterSwap", () => {
-    const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
-      spotPriceExpected.toNumber(),
-      tokenIn,
-      tokenOut,
-    );
-    checkResult(tokenInCalculated, amount);
+      test(`_spotPrice`, () => {
+        verifyApproximateEquality(
+          pool._spotPrice(poolPairData).toNumber(),
+          spotPriceExpected.toNumber(),
+        );
+      });
+
+      test(`_tokenInForExactSpotPriceAfterSwap`, () => {
+        const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
+          spotPriceExpected.toNumber(),
+          tokenIn,
+          tokenOut,
+        );
+        verifyApproximateEquality(tokenInCalculated, amount);
+      });
+    });
   });
 });
-
-function checkResult(calculated: number, result: number) {
-  expect(calculated / result).toBeCloseTo(1);
-}
--- packages/math-poolsimulator/src/gyroE/index.test.ts
+++ packages/math-poolsimulator/src/gyroE/index.test.ts
@@ -1,77 +1,86 @@
 import { bnum, SubgraphPoolBase } from "@balancer-labs/sor";
-import { describe, expect, test } from "@jest/globals";
+import { formatFixed } from "@ethersproject/bignumber";
+import { describe, test } from "@jest/globals";
 
 import { numberToBigNumber } from "../conversions";
 import { AMM } from "../index";
+import { verifyApproximateEquality } from "../utils";
 import poolsFromFile from "./fixtures/data.json";
 import { ExtendedGyroEV2 } from "./index";
 
 describe("Tests new Gyro ECLP math function based on package other functions", () => {
   const poolData = poolsFromFile["pool"];
+  const percentages = [25, 50, 75, 95];
 
   const tokenIn = "wstETH";
   const tokenOut = "swETH";
 
-  const amm = new AMM(
-    new ExtendedGyroEV2({
-      swapFee: poolData.swapFee,
-      totalShares: poolData.totalShares,
-      tokens: poolData.tokens,
-      tokensList: poolData.tokensList,
-      gyroEParams: {
-        alpha: poolData.alpha,
-        beta: poolData.beta,
-        c: poolData.c,
-        s: poolData.s,
-        lambda: poolData.lambda,
-      },
-      tokenRates: poolData.tokenRates,
-      derivedGyroEParams: {
-        tauAlphaX: poolData.tauAlphaX,
-        tauAlphaY: poolData.tauAlphaY,
-        tauBetaX: poolData.tauBetaX,
-        tauBetaY: poolData.tauBetaY,
-        u: poolData.u,
-        v: poolData.v,
-        w: poolData.w,
-        z: poolData.z,
-        dSq: poolData.dSq,
-      },
-    }),
-  );
-  const pool = ExtendedGyroEV2.fromPool(poolData as SubgraphPoolBase);
-  const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
-  const amount = 0.01;
-  const amountOldBigNumber = bnum(amount);
-  const spotPriceExpected = pool._spotPriceAfterSwapExactTokenInForTokenOut(
-    poolPairData,
-    amountOldBigNumber,
-  );
+  percentages.forEach((percentage) => {
+    describe(`Tests for ${percentage}% with ${tokenIn} as tokenIn`, () => {
+      const amm = new AMM(
+        new ExtendedGyroEV2({
+          swapFee: poolData.swapFee,
+          totalShares: poolData.totalShares,
+          tokens: poolData.tokens,
+          tokensList: poolData.tokensList,
+          gyroEParams: {
+            alpha: poolData.alpha,
+            beta: poolData.beta,
+            c: poolData.c,
+            s: poolData.s,
+            lambda: poolData.lambda,
+          },
+          tokenRates: poolData.tokenRates,
+          derivedGyroEParams: {
+            tauAlphaX: poolData.tauAlphaX,
+            tauAlphaY: poolData.tauAlphaY,
+            tauBetaX: poolData.tauBetaX,
+            tauBetaY: poolData.tauBetaY,
+            u: poolData.u,
+            v: poolData.v,
+            w: poolData.w,
+            z: poolData.z,
+            dSq: poolData.dSq,
+          },
+        }),
+      );
 
-  const amountOut = amm.exactTokenInForTokenOut(amount, tokenIn, tokenOut);
-  poolPairData.balanceIn = poolPairData.balanceIn.add(
-    numberToBigNumber({ number: amount }),
-  );
-  poolPairData.balanceOut = poolPairData.balanceOut.sub(
-    numberToBigNumber({ number: amountOut }),
-  );
+      const pool = ExtendedGyroEV2.fromPool(poolData as SubgraphPoolBase);
+      const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
 
-  test("_spotPrice", () => {
-    checkResult(
-      pool._spotPrice(poolPairData).toNumber(),
-      spotPriceExpected.toNumber(),
-    );
-  });
-  test("_tokenInForExactSpotPriceAfterSwap", () => {
-    const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
-      spotPriceExpected.toNumber(),
-      tokenIn,
-      tokenOut,
-    );
-    checkResult(tokenInCalculated, amount);
+      const amount =
+        Number(formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut)) *
+        (percentage / 100);
+      const amountOldBigNumber = bnum(amount);
+      const spotPriceExpected = pool._spotPriceAfterSwapExactTokenInForTokenOut(
+        poolPairData,
+        amountOldBigNumber,
+      );
+      const amountOut = amm.exactTokenInForTokenOut(amount, tokenIn, tokenOut);
+
+      poolPairData.balanceIn = poolPairData.balanceIn.add(
+        numberToBigNumber({ number: amount }),
+      );
+
+      poolPairData.balanceOut = poolPairData.balanceOut.sub(
+        numberToBigNumber({ number: amountOut }),
+      );
+
+      test(`_spotPrice`, () => {
+        verifyApproximateEquality(
+          pool._spotPrice(poolPairData).toNumber(),
+          spotPriceExpected.toNumber(),
+        );
+      });
+
+      test(`_tokenInForExactSpotPriceAfterSwap`, () => {
+        const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
+          spotPriceExpected.toNumber(),
+          tokenIn,
+          tokenOut,
+        );
+        verifyApproximateEquality(tokenInCalculated, amount);
+      });
+    });
   });
 });
-
-function checkResult(calculated: number, result: number) {
-  expect(calculated / result).toBeCloseTo(1);
-}

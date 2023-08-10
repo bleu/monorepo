3238f51b9c6a426bbe03745b5fcc42acf8d05d63
luizakp <luiza.puccinelli06@gmail.com>
2023-08-10 18:23:28 -0300

    fix: add swap with all the tokens in math test

--- packages/math-poolsimulator/src/gyro3/index.test.ts
+++ packages/math-poolsimulator/src/gyro3/index.test.ts
@@ -11,57 +11,74 @@ import { ExtendedGyro3 } from "./index";
 describe("Tests new Gyro 3CLP math function based on package other functions", () => {
   const poolData = poolsFromFile["pool"];
   const percentages = [25, 50, 75, 95];
+  const tokens = ["USDC", "BUSD", "USDT"];
 
-  const tokenIn = "USDC";
-  const tokenOut = "BUSD";
+  tokens.forEach((tokenIn) => {
+    tokens.forEach((tokenOut) => {
+      if (tokenIn === tokenOut) return; // Skip when both tokens are the same
 
-  percentages.forEach((percentage) => {
-    describe(`Tests for ${percentage}% with ${tokenIn} as tokenIn`, () => {
-      const amm = new AMM(
-        new ExtendedGyro3({
-          swapFee: poolData.swapFee,
-          totalShares: poolData.totalShares,
-          tokens: poolData.tokens,
-          tokensList: poolData.tokensList,
-          root3Alpha: poolData.root3Alpha,
-        }),
-      );
+      percentages.forEach((percentage) => {
+        describe(`Tests for ${percentage}% with ${tokenIn} as tokenIn and ${tokenOut} as tokenOut`, () => {
+          const amm = new AMM(
+            new ExtendedGyro3({
+              swapFee: poolData.swapFee,
+              totalShares: poolData.totalShares,
+              tokens: poolData.tokens,
+              tokensList: poolData.tokensList,
+              root3Alpha: poolData.root3Alpha,
+            }),
+          );
 
-      const pool = ExtendedGyro3.fromPool(poolData as SubgraphPoolBase);
-      const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
+          const pool = ExtendedGyro3.fromPool(poolData as SubgraphPoolBase);
+          const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
 
-      const amount =
-        Number(formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut)) *
-        (percentage / 100);
-      const amountOldBigNumber = bnum(amount);
-      const spotPriceExpected = pool._spotPriceAfterSwapExactTokenInForTokenOut(
-        poolPairData,
-        amountOldBigNumber,
-      );
-      const amountOut = amm.exactTokenInForTokenOut(amount, tokenIn, tokenOut);
+          const amount =
+            Number(
+              formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut),
+            ) *
+            (percentage / 100);
+          const amountOldBigNumber = bnum(amount);
+          const spotPriceExpected =
+            pool._spotPriceAfterSwapExactTokenInForTokenOut(
+              poolPairData,
+              amountOldBigNumber,
+            );
+          const amountOut = amm.exactTokenInForTokenOut(
+            amount,
+            tokenIn,
+            tokenOut,
+          );
 
-      poolPairData.balanceIn = poolPairData.balanceIn.add(
-        numberToBigNumber({ number: amount }),
-      );
+          poolPairData.balanceIn = poolPairData.balanceIn.add(
+            numberToBigNumber({
+              number: amount,
+              decimals: poolPairData.decimalsIn,
+            }),
+          );
 
-      poolPairData.balanceOut = poolPairData.balanceOut.sub(
-        numberToBigNumber({ number: amountOut }),
-      );
+          poolPairData.balanceOut = poolPairData.balanceOut.sub(
+            numberToBigNumber({
+              number: amountOut,
+              decimals: poolPairData.decimalsOut,
+            }),
+          );
 
-      test(`_spotPrice`, () => {
-        verifyApproximateEquality(
-          pool._spotPrice(poolPairData).toNumber(),
-          spotPriceExpected.toNumber(),
-        );
-      });
+          test(`_spotPrice`, () => {
+            verifyApproximateEquality(
+              pool._spotPrice(poolPairData).toNumber(),
+              spotPriceExpected.toNumber(),
+            );
+          });
 
-      test(`_tokenInForExactSpotPriceAfterSwap`, () => {
-        const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
-          spotPriceExpected.toNumber(),
-          tokenIn,
-          tokenOut,
-        );
-        verifyApproximateEquality(tokenInCalculated, amount);
+          test(`_tokenInForExactSpotPriceAfterSwap`, () => {
+            const tokenInCalculated = amm.tokenInForExactSpotPriceAfterSwap(
+              spotPriceExpected.toNumber(),
+              tokenIn,
+              tokenOut,
+            );
+            verifyApproximateEquality(tokenInCalculated, amount);
+          });
+        });
       });
     });
   });

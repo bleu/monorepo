0dcab7a73d847e6150579ae588c963ffc99ac1c8
Rawallon Cardoso <rawallon@gmail.com>
2023-08-10 22:15:54 -0300

    Implemented TVL endpoint

---
+++ apps/balancer-tools/src/app/apr/rounds/[roundId]/tvl/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { mockGetTVLByRoundId } from "#/app/apr/mock_apis";
+// import { DuneAPI } from "#/lib/dune";
+
+export async function GET(
+  _: NextRequest,
+  context: { params: { roundId: number } },
+) {
+  if (!context.params.roundId) return "Missing round_id";
+  // const duneAPI = new DuneAPI();
+  // const dune_request = await duneAPI.getTVLByRoundId(context.params.roundId)
+  const dune_request = mockGetTVLByRoundId;
+  return NextResponse.json(dune_request);
+}
--- apps/balancer-tools/src/lib/dune.ts
+++ apps/balancer-tools/src/lib/dune.ts
@@ -3,13 +3,20 @@ import "server-only";
 import { DuneClient, QueryParameter } from "@cowprotocol/ts-dune-client";
 import invariant from "tiny-invariant";
 
+import { Round } from "#/app/apr/(utils)/rounds";
+
 const duneApiKey = process.env.DUNE_API_KEY;
 
-export interface DunePoolData {
+interface DunePoolData {
   pct_votes: number;
   symbol: string;
   votes: number;
 }
+
+interface TVLData {
+  blockchain: string;
+  tvl: number;
+};
 
 interface ErrorReturn {
   error: string;
@@ -41,6 +48,27 @@ export class DuneAPI {
       .then(
         (executionResult) =>
           executionResult.result?.rows as unknown as DunePoolData[],
+      )
+      .catch((error: { message: string }) => ({ error: error.message }));
+  }
+
+  async getTVLByRoundId(
+    roundId: number = 0,
+  ): Promise<undefined | TVLData[] | ErrorReturn> {
+    invariant(roundId > 0, "An valid roundId must be passed");
+    const roundData = Round.getRoundByNumber(roundId);
+    // Ex for Round 1: 
+    // https://dune.com/queries/2842639?2.+Start+date_d83555=2022-04-13+00%3A00%3A00
+    const queryId = 2842639;
+    const parameters = [
+      QueryParameter.number("End date", roundData.endDate.toISOString().slice(0, 10)),
+    ];
+
+    return this.client
+      .refresh(queryId, parameters)
+      .then(
+        (executionResult) =>
+          executionResult.result?.rows as unknown as TVLData[],
       )
       .catch((error: { message: string }) => ({ error: error.message }));
   }

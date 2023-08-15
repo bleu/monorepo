4fc58aaece262491d0bc840355775b9bdb17f2c1
Rawallon Cardoso <rawallon@gmail.com>
2023-08-14 22:55:06 -0300

    Create formatDate.ts

---
+++ apps/balancer-tools/src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+/**
+ * Formats a date in the "MM/DD/YYYY" (American) format.
+ *
+ * @param {Date} date - The input date to be formatted.
+ * @returns {string} A string representing the formatted date.
+ */
+export function formatDateToAmerican(date: Date): string {
+    return date.toLocaleDateString('en-US', {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric'
+    });
+  }
+  

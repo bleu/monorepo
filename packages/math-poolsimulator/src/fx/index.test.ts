cc4a2afe8e7deef0aa284b63e041a347dd9618a9
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-08-15 17:35:36 -0300

    wip: add spot price test

--- packages/math-poolsimulator/src/fx/index.test.ts
+++ packages/math-poolsimulator/src/fx/index.test.ts
@@ -1,83 +1,60 @@
-import { bnum, FxMaths, SubgraphPoolBase } from "@balancer-labs/sor";
-import { formatFixed } from "@ethersproject/bignumber";
+import { bnum, FxMaths, FxPool, SubgraphPoolBase } from "@balancer-labs/sor";
+import { formatFixed, parseFixed } from "@ethersproject/bignumber";
 import { describe, test } from "@jest/globals";
 
 import { numberToBigNumber } from "../conversions";
-import { AMM } from "../index";
 import { verifyApproximateEquality } from "../utils";
 import poolsFromFile from "./fixtures/data.json";
-import { ExtendedFx } from "./index";
 
 describe("Tests new Fx math function based on package other functions", () => {
   const poolData = poolsFromFile["pool"];
-  const tokens = poolData.tokens.map((token) => token.symbol);
-  const percentages = [25, 50, 75, 95];
+  const tokens = poolData.tokens.map((token) => token.address);
+  const percentages = [25];
 
   tokens.forEach((tokenIn) => {
     const tokenOut = tokenIn === tokens[0] ? tokens[1] : tokens[0];
 
     percentages.forEach((percentage) => {
       describe(`Tests for ${percentage}% with ${tokenIn} as tokenIn`, () => {
-        const amm = new AMM(
-          new ExtendedFx({
-            swapFee: poolData.swapFee,
-            totalShares: poolData.totalShares,
-            tokens: poolData.tokens,
-            tokensList: poolData.tokensList,
-            alpha: poolData.alpha,
-            beta: poolData.beta,
-            delta: poolData.delta,
-            lambda: poolData.lambda,
-            epsilon: poolData.epsilon,
-          }),
-        );
-
-        const pool = ExtendedFx.fromPool(poolData as SubgraphPoolBase);
+        const pool = FxPool.fromPool(poolData as SubgraphPoolBase);
         const poolPairData = pool.parsePoolPairData(tokenIn, tokenOut);
 
         const amount =
           Number(
-            formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut),
+            formatFixed(poolPairData.balanceOut, poolPairData.decimalsOut)
           ) *
           (percentage / 100);
         const amountOldBigNumber = bnum(amount);
 
-        if (!pool._checkIfInIsOnLimit(poolPairData, amountOldBigNumber)) {
-          return;
-        }
-
-        const spotPriceExpected = FxMaths.spotPriceBeforeSwap(
-          numberToBigNumber({
-            number: amount,
-            decimals: poolPairData.decimalsOut,
-          }),
-          poolPairData,
-        );
+        const spotPriceExpected =
+          FxMaths._spotPriceAfterSwapExactTokenInForTokenOut(
+            poolPairData,
+            amountOldBigNumber
+          );
 
         test(`_spotPrice`, () => {
-          const amountOut = amm.exactTokenInForTokenOut(
-            amount,
-            tokenIn,
-            tokenOut,
+          const amountOut = pool._exactTokenInForTokenOut(
+            poolPairData,
+            bnum(amount)
           );
 
           poolPairData.balanceIn = poolPairData.balanceIn.add(
             numberToBigNumber({
               number: amount,
               decimals: poolPairData.decimalsIn,
-            }),
+            })
           );
 
           poolPairData.balanceOut = poolPairData.balanceOut.sub(
-            numberToBigNumber({
-              number: amountOut,
-              decimals: poolPairData.decimalsOut,
-            }),
+            parseFixed(amountOut.toString(), poolPairData.decimalsOut)
           );
 
           verifyApproximateEquality(
-            pool._spotPrice(poolPairData).toNumber(),
-            spotPriceExpected.toNumber(),
+            FxMaths._spotPriceAfterSwapExactTokenInForTokenOut(
+              poolPairData,
+              bnum(0)
+            ).toNumber(),
+            spotPriceExpected.toNumber()
           );
         });
       });

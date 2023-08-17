5d247680420c959a42b1fdb0bcd6d9455e9ee504
luizakp <luiza.puccinelli06@gmail.com>
2023-08-17 08:53:48 -0300

    chore: use only the same 5 colors on top pools chart

--- apps/balancer-tools/src/app/apr/round/(components)/TopPoolsChart.tsx
+++ apps/balancer-tools/src/app/apr/round/(components)/TopPoolsChart.tsx
@@ -1,73 +1,55 @@
 "use client";
 
+import {
+  amberDark,
+  blueDark,
+  crimsonDark,
+  greenDark,
+  violetDark,
+} from "@radix-ui/colors";
 import { PlotType } from "plotly.js";
 
 import Plot from "#/components/Plot";
 
 export default function TopPoolsChart({ roundId }: { roundId: string }) {
+  const colors = [
+    greenDark.green9,
+    violetDark.violet9,
+    crimsonDark.crimson8,
+    blueDark.blue9,
+    amberDark.amber9,
+  ];
+  const bars = [
+    { x: "0.00", y: "sNOTE-BPT" },
+    { x: "0.01", y: "B-50USDC-50WETH" },
+    { x: "0.35", y: "B-stETH-STABLE" },
+    { x: "0.50", y: "B-50VITA-50WETH" },
+    { x: "0.99", y: "80D2D-20USDC" },
+    { x: "1.10", y: "B-80GNO-20WETH" },
+    { x: "1.92", y: "50WETH-50AURA" },
+    { x: "3.48", y: "20WBTC-80BADGER" },
+    { x: "3.53", y: "50COW-50GNO" },
+    { x: "5.60", y: "50COW-50WETH" },
+    { x: "9.60", y: "VBPT" },
+    { x: "18.19", y: "20WETH-80WNCG" },
+    { x: "18.62", y: "B-sdBAL-STABLE" },
+    { x: "26.18", y: "50DFX-50WETH" },
+    { x: "39.03", y: "USDC-PAL" },
+  ];
+  const colorArray = Array.from(
+    { length: bars.length },
+    (_, index) => colors[index % colors.length],
+  );
+
   const data = {
     hovertemplate: "%{x:.2f}% APR<extra></extra>",
     marker: {
-      color: [
-        "rgba(227,176,10,1)",
-        "rgba(234,157,219,1)",
-        "rgba(26,51,112,1)",
-        "rgba(220,118,103,1)",
-        "rgba(138,42,4,1)",
-        "rgba(170,93,232,1)",
-        "rgba(248,224,240,1)",
-        "rgba(50,220,250,1)",
-        "rgba(46,68,220,1)",
-        "rgba(46,68,220,1)",
-        "rgba(252,101,62,1)",
-        "rgba(141,115,128,1)",
-        "rgba(202,164,107,1)",
-        "rgba(202,164,107,1)",
-        "rgba(179,162,239,1)",
-        "rgba(183,43,247,1)",
-        "rgba(140,38,142,1)",
-      ],
+      color: colorArray,
     },
     orientation: "h" as const,
     type: "bar" as PlotType,
-    x: [
-      "0.00",
-      "0.01",
-      "0.35",
-      "0.50",
-      "0.99",
-      "1.10",
-      "1.92",
-      "3.48",
-      "3.48",
-      "3.53",
-      "5.60",
-      "9.60",
-      "18.19",
-      "18.62",
-      "26.18",
-      "39.03",
-      "39.03",
-    ],
-    y: [
-      "sNOTE-BPT",
-      "B-50USDC-50WETH",
-      "B-stETH-STABLE",
-      "B-50VITA-50WETH",
-      "80D2D-20USDC",
-      "B-80GNO-20WETH",
-      "50WETH-50AURA",
-      "20WBTC-80BADGER",
-      "20WBTC-80BADGER",
-      "50COW-50GNO",
-      "50COW-50WETH",
-      "VBPT",
-      "20WETH-80WNCG",
-      "B-sdBAL-STABLE",
-      "50DFX-50WETH",
-      "USDC-PAL",
-      "USDC-PAL",
-    ],
+    x: bars.map((bar) => bar.x),
+    y: bars.map((bar) => bar.y),
   };
 
   return (

aa6ea797f1fee04e3a845ed3bd4292c12599399e
Rawallon Cardoso <rawallon@gmail.com>
2023-08-21 16:16:18 -0300

    Refactor KPI component to allow for component as content

--- apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
+++ apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
+
+import { Spinner } from "#/components/Spinner";
+
 import { KPI } from "./KPI";
 
 interface CardDetail {
   title: string;
-  content: string;
+  content: JSX.Element | string;
 }
 
 function OverviewCards({ cardsDetails }: { cardsDetails: CardDetail[] }) {
@@ -10,7 +14,12 @@ function OverviewCards({ cardsDetails }: { cardsDetails: CardDetail[] }) {
     <div className="flex justify-between border border-blue6 bg-blue3 rounded p-4">
       <div className="flex flex-row flex-wrap xl:flex-nowrap gap-6 justify-evenly w-full">
         {cardsDetails.map((cardDetail) => (
-          <KPI title={cardDetail.title} content={cardDetail.content} />
+          <Suspense key={cardDetail.title} fallback={<KPI title="" content={<Spinner size="sm" />} />}>
+            <KPI
+              title={cardDetail.title}
+              content={cardDetail.content}
+            />
+          </Suspense>
         ))}
       </div>
     </div>

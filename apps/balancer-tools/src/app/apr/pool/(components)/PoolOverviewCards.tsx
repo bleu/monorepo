e7067818fdf5ac1d4fb371302f9a8499ceaccae0
Rawallon Cardoso <rawallon@gmail.com>
2023-08-21 16:17:41 -0300

    Created  component for Pool Average APR/VTL

--- apps/balancer-tools/src/app/apr/pool/(components)/PoolOverviewCards.tsx
+++ apps/balancer-tools/src/app/apr/pool/(components)/PoolOverviewCards.tsx
@@ -4,7 +4,29 @@ import { formatNumber } from "#/utils/formatNumber";
 
 import OverviewCards from "../../(components)/OverviewCards";
 import { calculatePoolStats } from "../../(utils)/calculatePoolStats";
+import {
+  generatePromisesForHistoricalPoolData,
+  HistoricalDataEntry,
+} from "../../(utils)/getHistoricalDataForPool";
 import { Round } from "../../(utils)/rounds";
+
+async function AverageTVLCard({ poolId }: { poolId: string }) {
+  const data = await generatePromisesForHistoricalPoolData(poolId);
+  const totalTvl = data.reduce(
+    (sum: number, entry: HistoricalDataEntry) => sum + entry.tvl,
+    0,
+  );
+  return <div>{formatNumber(totalTvl / data.length)}</div>;
+}
+
+async function AverageAPRCard({ poolId }: { poolId: string }) {
+  const data = await generatePromisesForHistoricalPoolData(poolId);
+  const totalAPR = data.reduce(
+    (sum: number, entry: HistoricalDataEntry) => sum + entry.apr,
+    0,
+  );
+  return <div>{formatNumber(totalAPR / data.length).concat("%")}</div>;
+}
 
 export default async function PoolOverviewCards({
   roundId,

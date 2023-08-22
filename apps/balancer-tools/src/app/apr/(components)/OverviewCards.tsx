b5504ed9abe88485e56f983b53d59b0755c36909
Rawallon Cardoso <rawallon@gmail.com>
2023-08-21 22:43:54 -0300

    First implementation for routes

--- apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
+++ apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
@@ -14,11 +14,11 @@ function OverviewCards({ cardsDetails }: { cardsDetails: CardDetail[] }) {
     <div className="flex justify-between border border-blue6 bg-blue3 rounded p-4">
       <div className="flex flex-row flex-wrap xl:flex-nowrap gap-6 justify-evenly w-full">
         {cardsDetails.map((cardDetail) => (
-          <Suspense key={cardDetail.title} fallback={<KPI title="" content={<Spinner size="sm" />} />}>
-            <KPI
-              title={cardDetail.title}
-              content={cardDetail.content}
-            />
+          <Suspense
+            key={cardDetail.title}
+            fallback={<KPI title="" content={<Spinner size="sm" />} />}
+          >
+            <KPI title={cardDetail.title} content={cardDetail.content} />
           </Suspense>
         ))}
       </div>

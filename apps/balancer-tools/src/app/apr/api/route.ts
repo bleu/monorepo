cd69be51bb07857b874c9eee6180ae2a4aa98ef4
José Ribeiro <me@joseribeiro.dev>
2023-08-23 11:55:35 -0300

    add refactor ideas

--- apps/balancer-tools/src/app/apr/api/route.ts
+++ apps/balancer-tools/src/app/apr/api/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
+import fetch from "node-fetch";
 
 import votingGauges from "#/data/voting-gauges.json";
 
 import { calculatePoolStats } from "../(utils)/calculatePoolStats";
 import { Round } from "../(utils)/rounds";
+
+type Order = "asc" | "desc";
 
 export interface PoolStatsData {
   apr: number;
@@ -23,154 +26,196 @@ export interface PoolStatsResults {
   average: PoolStatsData;
 }
 
-function sortingPoolStats(
-  PoolStatsResults: { [key: string]: PoolStatsData },
-  sortProperty: keyof PoolStatsData,
-  orderArg: string = "desc",
-  offset: number = 0,
-  limit: number = Infinity,
-) {
-  const sortedEntries = Object.entries(PoolStatsResults)
-    .sort((a, b) => {
-      if (!a[1] || !b[1]) return 0;
-      const valueA = a[1][sortProperty];
-      const valueB = b[1][sortProperty];
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
-      // Handle Numbers
-      if (typeof valueA === "number" && typeof valueB === "number") {
-        // Handle null values
-        if (valueB === null || (isNaN(valueA) && isNaN(valueB))) return 0;
-        if (isNaN(valueA)) return 1;
-        if (isNaN(valueB)) return -1;
+const memoryCache: { [key: string]: any } = {};
 
-        return orderArg === "asc" ? valueA - valueB : valueB - valueA;
+const fetchDataForRoundId = async (roundId: string) => {
+  const validGaugesList = votingGauges
+    .filter((gauge) => !gauge.isKilled)
+    .map((gauge) => gauge.pool.id);
+
+  const gaugesData = await Promise.allSettled(
+    validGaugesList.map(async (poolId) => {
+      const res = await fetch(
+        `${BASE_URL}?roundid=${roundId}&poolid=${poolId}`
+      );
+      return await res.json();
+    })
+  );
+
+  const resolvedGaugesData = gaugesData
+    .filter((result) => result.status === "fulfilled")
+    .map((result) => result?.value);
+
+  const parsedResult = validGaugesList.reduce((acc, poolId, index) => {
+    acc[poolId] = resolvedGaugesData[index];
+    return acc;
+  }, {});
+
+  return parsedResult;
+};
+
+const fetchDataForPoolId = async (poolId: string) => {
+  const gauge = votingGauges.filter((gauge) => gauge.pool.id === poolId)[0];
+  // Multiplying by 1000 because unix timestamp is in seconds
+  const gaugeAddedDate = new Date(gauge["addedTimestamp"] * 1000);
+  let roundGaugeAdded = Round.getRoundByDate(gaugeAddedDate);
+  if (Round.getRoundByNumber(1).endDate > gaugeAddedDate) {
+    // There are pools that were added before the first round, so we need to handle that
+    // Ex: 0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014
+    roundGaugeAdded = Round.getRoundByNumber(1);
+  }
+
+  const results: PoolStatsData[] = await Promise.all(
+    Array.from(
+      {
+        length:
+          parseInt(Round.currentRound().value) -
+          parseInt(roundGaugeAdded.value),
+      },
+      async (_, index) => {
+        const res = await fetch(
+          `${BASE_URL}?roundid=${String(
+            index + parseInt(roundGaugeAdded.value)
+          )}&poolid=${poolId}`
+        );
+        return await res.json();
       }
+    )
+  );
+
+  const averagedValues = results.reduce(
+    (acc, result) => {
+      return {
+        apr: acc.apr + result.apr,
+        balPriceUSD: acc.balPriceUSD + result.balPriceUSD,
+        tvl: acc.tvl + result.tvl,
+        votingShare: acc.votingShare + result.votingShare,
+      };
+    },
+    { apr: 0, balPriceUSD: 0, tvl: 0, votingShare: 0 }
+  );
+  const numResults = results.length;
+  const averageResult = {
+    apr: averagedValues.apr / numResults,
+    balPriceUSD: averagedValues.balPriceUSD / numResults,
+    tvl: averagedValues.tvl / numResults,
+    votingShare: averagedValues.votingShare / numResults,
+  };
+  const perRound = results.reduce(
+    (acc, obj, index) => {
+      acc[index + 1 + parseInt(roundGaugeAdded.value)] = obj;
+      return acc;
+    },
+    {} as { [key: number]: PoolStatsData }
+  );
+
+  return { perRound, average: averageResult };
+};
+
+const getDataFromCacheOrCompute = async (
+  cacheKey: string,
+  computeFn: () => Promise<any>
+) => {
+  if (!memoryCache[cacheKey]) {
+    memoryCache[cacheKey] = await computeFn();
+  }
+  return memoryCache[cacheKey];
+};
+
+const handlePoolIdOnly = async (poolId: string): Promise<PoolStatsResults> => {
+  const cacheKey = `pool_${poolId}`;
+  return await getDataFromCacheOrCompute(cacheKey, async () => {
+    return fetchDataForPoolId(poolId);
+  });
+};
+
+const handleRoundIdOnly = async (
+  roundId: string
+): Promise<{ [key: string]: PoolStatsData }> => {
+  const cacheKey = `round_${roundId}`;
+  return await getDataFromCacheOrCompute(cacheKey, async () => {
+    return fetchDataForRoundId(roundId);
+  });
+};
+
+const handleBothPoolAndRoundId = async (
+  poolId: string,
+  roundId: string
+): Promise<PoolStatsData> => {
+  const cacheKey = `pool_${poolId}_round_${roundId}`;
+  return await getDataFromCacheOrCompute(cacheKey, async () => {
+    return calculatePoolStats({ roundId, poolId });
+  });
+};
+
+export async function GET(request: NextRequest) {
+  const {
+    searchParams: { get },
+  } = request.nextUrl;
+
+  const poolId = get("poolid");
+  const roundId = get("roundid");
+  const sortArg = (get("sort") as keyof PoolStatsData) || "apr";
+  const orderArg = (get("order") as Order) || "desc";
+  const limitArg = parseInt(get("limit") ?? "0") || Infinity;
+  const offsetArg = parseInt(get("offset") ?? "0");
+
+  let responseData;
+
+  if (poolId && roundId) {
+    responseData = await handleBothPoolAndRoundId(roundId, poolId);
+  } else if (poolId) {
+    responseData = await handlePoolIdOnly(poolId);
+  } else if (roundId) {
+    responseData = await handleRoundIdOnly(roundId);
+  } else {
+    return NextResponse.json({ error: "no roundId or poolId provided" });
+  }
+
+  return NextResponse.json(
+    sortAndLimit(responseData, sortArg, orderArg, offsetArg, limitArg)
+  );
+}
+
+function compareNumbers(a: number, b: number, order: Order): number {
+  // Handle NaN values
+  if (isNaN(a) && isNaN(b)) return 0;
+  if (isNaN(a)) return 1;
+  if (isNaN(b)) return -1;
+
+  return order === "asc" ? a - b : b - a;
+}
+
+function compareStrings(a: string, b: string, order: Order): number {
+  return order === "asc" ? a.localeCompare(b) : b.localeCompare(a);
+}
+
+function sortAndLimit(
+  PoolStatsResults: { [key: string]: PoolStatsData },
+  sortProperty: keyof PoolStatsData = "apr",
+  orderArg: Order = "desc",
+  offset: number = 0,
+  limit: number = Infinity
+): { [key: string]: PoolStatsData } {
+  const sortedEntries = Object.entries(PoolStatsResults)
+    .sort(([_, aValue], [__, bValue]) => {
+      const valueA = aValue[sortProperty];
+      const valueB = bValue[sortProperty];
 
       // Handle null values
       if (valueA === null && valueB === null) return 0;
       if (valueA === null) return 1;
       if (valueB === null) return -1;
 
-      // Handle Strings
-      return orderArg === "asc"
-        ? valueA.toString().localeCompare(valueB.toString())
-        : valueB.toString().localeCompare(valueA.toString());
+      if (typeof valueA === "number" && typeof valueB === "number") {
+        return compareNumbers(valueA, valueB, orderArg);
+      } else {
+        return compareStrings(valueA.toString(), valueB.toString(), orderArg);
+      }
     })
     .slice(offset, offset + limit);
+
   return Object.fromEntries(sortedEntries);
-}
-
-export async function GET(request: NextRequest) {
-  const poolId = request.nextUrl.searchParams.get("poolid");
-  const roundId = request.nextUrl.searchParams.get("roundid");
-  const sortArg: keyof PoolStatsData = request.nextUrl.searchParams.get("sort");
-  const orderArg = request.nextUrl.searchParams.get("order") || undefined;
-  const limitArg =
-    parseInt(request.nextUrl.searchParams.get("limit") ?? "0") || Infinity;
-  const offsetArg = parseInt(request.nextUrl.searchParams.get("offset") ?? "0");
-
-  // If it has no poolId or roundId, return an error
-  if (!roundId && !poolId) {
-    return NextResponse.json({ error: "no roundId or poolId provided" });
-  }
-
-  // If it has a poolId but no roundId, return the average of all rounds and per round
-  if (poolId) {
-    const gauge = votingGauges.filter((gauge) => gauge.id === poolId)[0];
-    // Multiplying by 1000 because unix timestamp is in seconds
-    const gaugeAddedDate = new Date(gauge["addedTimestamp"] * 1000);
-    const roundGaugeAdded = Round.getRoundByDate(gaugeAddedDate);
-
-    const results: PoolStatsData[] = await Promise.all(
-      Array.from(
-        {
-          length:
-            parseInt(Round.currentRound().value) -
-            parseInt(roundGaugeAdded.value),
-        },
-        (_, index) =>
-          calculatePoolStats({
-            poolId: poolId,
-            roundId: String(index + parseInt(roundGaugeAdded.value)),
-          }),
-      ),
-    );
-
-    const averagedValues = results.reduce(
-      (acc, result) => {
-        return {
-          apr: acc.apr + result.apr,
-          balPriceUSD: acc.balPriceUSD + result.balPriceUSD,
-          tvl: acc.tvl + result.tvl,
-          votingShare: acc.votingShare + result.votingShare,
-        };
-      },
-      { apr: 0, balPriceUSD: 0, tvl: 0, votingShare: 0 },
-    );
-    const numResults = results.length;
-    const averageResult = {
-      apr: averagedValues.apr / numResults,
-      balPriceUSD: averagedValues.balPriceUSD / numResults,
-      tvl: averagedValues.tvl / numResults,
-      votingShare: averagedValues.votingShare / numResults,
-    };
-    const perRound = results.reduce(
-      (acc, obj, index) => {
-        acc[index + 1 + parseInt(roundGaugeAdded.value)] = obj;
-        return acc;
-      },
-      {} as { [key: number]: PoolStatsData },
-    );
-
-    return NextResponse.json({ perRound, average: averageResult });
-  }
-
-  // If it has a roundId but no poolId, return all pools for that round
-  if (roundId && !poolId) {
-    const validGaugesList = votingGauges
-      .filter((gauge) => !gauge.gauge.isKilled)
-      .map((gauge) => gauge.id);
-
-    const gaguesData = await Promise.allSettled(
-      validGaugesList.map((poolId) => calculatePoolStats({ poolId, roundId })),
-    );
-
-    // This should handle case where a pool returns nothing for that round
-    const resolvedGaugesData = gaguesData
-      .filter((result) => result.status === "fulfilled")
-      .map((result) => {
-        if (result.status === "fulfilled") {
-          // This if is here because TS can't infer only fufilled results are in the array
-          return result.value;
-        }
-      });
-
-    const parsedResult = validGaugesList.reduce((acc, poolId, index) => {
-      acc[poolId] = resolvedGaugesData[index];
-      return acc;
-    }, {});
-
-    if (sortArg) {
-      return NextResponse.json(
-        sortingPoolStats(parsedResult, sortArg, orderArg, offsetArg, limitArg),
-      );
-    }
-    if (limitArg) {
-      const limitedData = Object.keys(parsedResult)
-        .slice(offsetArg, offsetArg + limitArg)
-        .reduce((acc, key) => {
-          acc[key] = parsedResult[key];
-          return acc;
-        }, {});
-      return NextResponse.json(limitedData);
-    }
-
-    return NextResponse.json(parsedResult);
-  }
-
-  // If it has a poolId and a roundId, return the stats for that pool and round
-  if (roundId && poolId) {
-    const poolStats = await calculatePoolStats({ roundId, poolId });
-    return NextResponse.json(poolStats);
-  }
 }

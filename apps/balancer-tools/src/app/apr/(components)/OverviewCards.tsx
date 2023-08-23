149aed8b60ca689a858738236e17e85239690acb
Rawallon Cardoso <rawallon@gmail.com>
2023-08-22 21:22:23 -0300

    Altered suspense Boundary on KPI component

--- apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
+++ apps/balancer-tools/src/app/apr/(components)/OverviewCards.tsx
@@ -1,7 +1,3 @@
-import { Suspense } from "react";
-
-import { Spinner } from "#/components/Spinner";
-
 import { KPI } from "./KPI";
 
 interface CardDetail {
@@ -13,12 +9,7 @@ function OverviewCards({ cardsDetails }: { cardsDetails: CardDetail[] }) {
   return (
     <div className="border border-blue6 bg-blue3 rounded p-4 gap-6 w-full grid auto-cols-fr	grid-flow-col">
       {cardsDetails.map((cardDetail) => (
-        <Suspense
-          key={cardDetail.title}
-          fallback={<KPI title="" content={<Spinner size="sm" />} />}
-        >
-          <KPI title={cardDetail.title} content={cardDetail.content} />
-        </Suspense>
+        <KPI title={cardDetail.title} content={cardDetail.content} />
       ))}
     </div>
   );

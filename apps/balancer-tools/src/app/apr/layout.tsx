52ddd28ab627aa2f78abae541e946c0112e014c0
Rawallon Cardoso <rawallon@gmail.com>
2023-08-25 18:09:21 -0300

    Refactor: Moved sidebar pool selector to header

--- apps/balancer-tools/src/app/apr/layout.tsx
+++ apps/balancer-tools/src/app/apr/layout.tsx
@@ -1,35 +1,18 @@
 "use client";
 
-import clsx from "clsx";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import * as React from "react";
-import { useForm } from "react-hook-form";
 import invariant from "tiny-invariant";
 
 import { Header } from "#/components/Header";
-import { PoolAttribute, SearchPoolForm } from "#/components/SearchPoolForm";
-import { Select, SelectItem } from "#/components/Select";
 
-import { Round } from "./(utils)/rounds";
-
-const ALL_ROUNDS = Round.getAllRounds();
-const LAST_ROUND_ID = ALL_ROUNDS[0].value;
+import HeaderEndButton from "./(components)/HeaderEndButton";
 
 export default function Layout({ children }: React.PropsWithChildren) {
-  const router = useRouter();
-
   const { roundId, poolId, network } = useParams();
-  const form = useForm<PoolAttribute>();
-  const { setValue } = form;
-
   invariant(!Array.isArray(roundId), "roundId cannot be a list");
   invariant(!Array.isArray(poolId), "poolId cannot be a list");
-
-  React.useEffect(() => {
-    if (!poolId && (!roundId || roundId === "current")) {
-      router.push(`/apr/round/${LAST_ROUND_ID}`);
-    }
-  }, [roundId]);
+  invariant(!Array.isArray(network), "network cannot be a list");
 
   return (
     <div className="flex h-full flex-col">
@@ -37,56 +20,15 @@ export default function Layout({ children }: React.PropsWithChildren) {
         linkUrl={"/apr"}
         title={"Pool Simulator"}
         imageSrc={"/assets/balancer-symbol.svg"}
-        endButton={<></>}
+        endButton={
+          <HeaderEndButton
+            network={network}
+            poolId={poolId}
+            roundId={roundId}
+          />
+        }
       />
-      <div className="flex flex-1 gap-x-8">
-        <div className="h-full w-72 lg:w-80 max-w-sm">
-          <SearchPoolForm
-            showPools
-            form={form}
-            defaultValuePool={poolId}
-            onSubmit={(value) => {
-              router.push(`/apr/pool/${value.network}/${value.poolId}`);
-              setValue("poolId", value.poolId);
-            }}
-          >
-            <div className="relative">
-              <div className="space-x-2">
-                <label className="mb-2 text-sm text-slate12">Round</label>
-                <label
-                  className={clsx("px-1 float-right text-sm rounded-full", {
-                    "text-slate12 bg-blue-600 hover:bg-blue-700":
-                      roundId === ALL_ROUNDS[0].value,
-                    "text-slate12 bg-gray-600 hover:bg-gray-700":
-                      roundId !== ALL_ROUNDS[0].value,
-                  })}
-                >
-                  {roundId === ALL_ROUNDS[0].value ? "Current" : "Ended"}
-                </label>
-              </div>
-
-              <Select
-                value={roundId ?? ""}
-                onValueChange={(value) => {
-                  router.push(
-                    !poolId
-                      ? `/apr/round/${value}`
-                      : `/apr/pool/${network}/${poolId}/round/${value}`,
-                  );
-                }}
-                className="w-full"
-              >
-                {ALL_ROUNDS.map((round) => (
-                  <SelectItem key={round.value} value={round.value}>
-                    {round.label}
-                  </SelectItem>
-                ))}
-              </Select>
-            </div>
-          </SearchPoolForm>
-        </div>
-        <div className="h-full flex-1 py-5 pr-8 text-white">{children}</div>
-      </div>
+      <div className="flex flex-1 gap-x-8 px-4 pt-4 text-white">{children}</div>
     </div>
   );
 }

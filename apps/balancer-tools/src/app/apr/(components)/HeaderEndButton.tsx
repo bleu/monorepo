c32c9aa3a02cf2b9a03ac4faa57dbd57be2a2c62
Rawallon Cardoso <rawallon@gmail.com>
2023-08-25 18:09:21 -0300

    Refactor: Moved sidebar pool selector to header

---
+++ apps/balancer-tools/src/app/apr/(components)/HeaderEndButton.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { networkIdFor } from "@bleu-balancer-tools/utils";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
+import * as React from "react";
+
+import { Dialog } from "#/components/Dialog";
+import { SearchPoolForm } from "#/components/SearchPoolForm";
+import { Select, SelectItem } from "#/components/Select";
+
+import { Round } from "../(utils)/rounds";
+
+const ALL_ROUNDS = Round.getAllRounds();
+const LAST_ROUND_ID = ALL_ROUNDS[0].value;
+
+export default function HeaderEndButton({
+  roundId,
+  poolId,
+  network,
+}: {
+  roundId?: string | undefined;
+  poolId?: string | undefined;
+  network?: string | undefined;
+}) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!poolId && (!roundId || roundId === "current")) {
+      router.push(`/apr/round/${LAST_ROUND_ID}`);
+    }
+  }, [roundId]);
+
+  const handlePoolClick = ({
+    network,
+    poolId,
+  }: {
+    network: string;
+    poolId: string;
+  }) => {
+    router.push(`/apr/pool/${network}/${poolId}`);
+  };
+
+  return (
+    <div className="flex gap-6">
+      <Dialog
+        title="Import pool parameters"
+        content={
+          <SearchPoolForm
+            defaultValueNetwork={networkIdFor(network)}
+            onSubmit={handlePoolClick}
+          />
+        }
+      >
+        <div className="flex items-center space-x-2 text-sm font-normal text-slate12 bg-blue3 border border-blue6 p-2 rounded-[4px] cursor-pointer">
+          <MagnifyingGlassIcon width="20" height="20" strokeWidth={1} />
+          <span className="font-medium">Search for especific pool</span>
+        </div>
+      </Dialog>
+
+      <Select
+        placeholder="Select a round"
+        value={roundId}
+        onValueChange={(value) => {
+          router.push(
+            !poolId
+              ? `/apr/round/${value}`
+              : `/apr/pool/${network}/${poolId}/round/${value}`,
+          );
+        }}
+        className=""
+      >
+        {ALL_ROUNDS.map((round) => (
+          <SelectItem key={round.value} value={round.value}>
+            {round.label}
+          </SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
+}

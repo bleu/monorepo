924bec8fd6c33c628665f25c20f289ea58a064e7
Rawallon Cardoso <rawallon@gmail.com>
2023-08-28 16:19:12 -0300

    Implemented Breadcrumb

--- apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
+++ apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
@@ -2,12 +2,17 @@ import { Suspense } from "react";
 
 import HistoricalAPRChart from "#/app/apr/pool/(components)/HistoricalAPRChart";
 import PoolOverviewCards from "#/app/apr/pool/(components)/PoolOverviewCards";
+import Breadcrumb from "#/app/apr/round/(components)/Breadcrumb";
 import { Spinner } from "#/components/Spinner";
 
-export default async function Page({ params }: { params: { poolId: string } }) {
-  const { poolId } = params;
+export default async function Page({
+  params: { roundId, network, poolId },
+}: {
+  params: { roundId: string; poolId: string; network: string };
+}) {
   return (
     <div className="flex flex-1 h-full w-full flex-col justify-start rounded-3xl text-white gap-y-3">
+      <Breadcrumb network={network} poolId={poolId} roundId={roundId} />
       <Suspense fallback={<Spinner />}>
         <PoolOverviewCards poolId={poolId} />
       </Suspense>
--- apps/balancer-tools/src/app/apr/round/[roundId]/page.tsx
+++ apps/balancer-tools/src/app/apr/round/[roundId]/page.tsx
@@ -2,17 +2,19 @@ import { Suspense } from "react";
 
 import { Spinner } from "#/components/Spinner";
 
+import Breadcrumb from "../(components)/Breadcrumb";
 import PoolTableWrapper from "../(components)/PoolTableWrapper";
 import RoundOverviewCards from "../(components)/RoundOverviewCards";
 import TopPoolsChart from "../(components)/TopPoolsChart";
 
 export default function Page({
-  params: { roundId },
+  params: { roundId, network, poolId },
 }: {
-  params: { roundId: string };
+  params: { roundId: string; poolId: string; network: string };
 }) {
   return (
     <div className="flex flex-1 flex-col gap-y-3">
+      <Breadcrumb network={network} poolId={poolId} roundId={roundId} />
       <Suspense fallback={<Spinner />}>
         <RoundOverviewCards roundId={roundId} />
       </Suspense>

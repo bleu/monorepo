77fd3a86d2035fa8bf7fd014ccdc96fe4f0a4be9
Rawallon Cardoso <rawallon@gmail.com>
2023-08-29 13:52:38 -0300

    Added toast on copy link

--- apps/balancer-tools/src/app/apr/round/(components)/Breadcrumb.tsx
+++ apps/balancer-tools/src/app/apr/round/(components)/Breadcrumb.tsx
@@ -7,9 +7,10 @@ import {
 } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import invariant from "tiny-invariant";
 
+import { Toast } from "#/components/Toast";
 import { Pool } from "#/lib/balancer/gauges";
 
 import { Round } from "../../(utils)/rounds";
@@ -30,12 +31,10 @@ function BreadcrumbItem({
             href={link}
             className="flex ml-1 text-sm font-medium text-white hover:text-blue-600 md:ml-2"
           >
-            {content}
             {children}
           </a>
         ) : (
           <div className="flex ml-1 text-sm font-medium text-white md:ml-2">
-            {content}
             {children}
           </div>
         )}
@@ -67,12 +66,15 @@ export default function Breadcrumb() {
   invariant(!Array.isArray(roundId), "roundId cannot be a list");
   invariant(!Array.isArray(network), "network should not be an array");
 
+  const [isNotifierOpen, setIsNotifierOpen] = useState<boolean>(false);
+
   let selectedPool;
   if (poolId) {
     selectedPool = new Pool(poolId);
   }
 
   return (
+    <>
     <nav
       className="border border-blue6 bg-blue3 text-white justify-between px-4 py-3 rounded-lg sm:flex sm:px-5"
       aria-label="Breadcrumb"
@@ -119,6 +121,7 @@ export default function Breadcrumb() {
           <button
             onClick={() => {
               navigator.clipboard.writeText(window.location.href);
+              setIsNotifierOpen(true);
             }}
             className="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-white bg-blue6 border border-blue9 rounded-lg hover:bg-blue7"
           >
@@ -128,5 +131,23 @@ export default function Breadcrumb() {
         </div>
       )}
     </nav>
+          <Toast
+          content={<ToastContent />}
+          isOpen={isNotifierOpen}
+          setIsOpen={setIsNotifierOpen}
+          duration={5000}
+        />
+        </>
+  );
+}
+
+
+function ToastContent() {
+  return (
+    <div className="flex h-14 flex-row items-center justify-between px-4 py-8">
+      <div className="flex flex-col justify-between space-y-1">
+        <h1 className="text-xl font-medium text-slate12">Link copied</h1>
+      </div>
+    </div>
   );
 }

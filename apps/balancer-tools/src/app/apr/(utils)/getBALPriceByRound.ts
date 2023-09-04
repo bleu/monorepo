3642135300de8a5c36c90bcf9f12c9bad21ad664
Rawallon Cardoso <rawallon@gmail.com>
2023-09-04 11:34:12 -0300

    Implemented logic that fetches the price of a token by round

--- apps/balancer-tools/src/app/apr/(utils)/getBALPriceByRound.ts
+++ apps/balancer-tools/src/app/apr/(utils)/getBALPriceByRound.ts
@@ -1,16 +1,22 @@
+import { networkFor } from "@bleu-balancer-tools/utils";
+
 import { DefiLlamaAPI } from "#/lib/coingecko";
 
 import { Round } from "./rounds";
 
 export const getBALPriceByRound = async (round: Round) => {
-  const BAL = "ethereum:0xba100000625a3754423978a60c9317c58a424e3d";
+  return getTokenPriceByRound(round, '0xba100000625a3754423978a60c9317c58a424e3d', 1);
+};
+
+export const getTokenPriceByRound = async (round: Round, tokenAddress: string, tokenNetwork: number) => {
+  const token = `${networkFor(tokenNetwork)}:${tokenAddress}`;
   const relevantDateForPrice = Math.min(Date.now(), round.endDate.getTime());
   const api = new DefiLlamaAPI();
 
   const response = await api.getHistoricalPrice(
     new Date(relevantDateForPrice),
-    [BAL],
+    [token],
   );
 
-  return response.coins[BAL]?.price;
+  return response.coins[token]?.price;
 };

d8a43416a48e510130d520f8fe6cfc3094d43346
luizakp <luiza.puccinelli06@gmail.com>
2023-09-05 15:50:32 -0300

    chore: move HistoricalAPRChart and remove onClick redirect

--- apps/balancer-tools/src/app/apr/pool/(components)/HistoricalCharts/HistoricalAPR/HistoricalAPRPlot.tsx
+++ apps/balancer-tools/src/app/apr/pool/(components)/HistoricalCharts/HistoricalAPR/HistoricalAPRPlot.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
-import { PlotMouseEvent } from "plotly.js";
-
 import Plot from "#/components/Plot";
 
 export default function HistoricalAPRPlot({ data }: { data: Plotly.Data[] }) {
-  const router = useRouter();
-  const currentPathname = usePathname();
-  function onClickHandler(event: PlotMouseEvent) {
-    const newPath = currentPathname.replace(/\/round\/\d+$/, "");
-    router.push(`${newPath}/round/${event.points[0].x}`);
-  }
-
   return (
-    <div className="flex justify-between border border-blue6 bg-blue3 rounded p-4 cursor-pointer">
+    <div className="flex justify-between bg-blue3 rounded p-4 cursor-pointer z-50">
       <Plot
-        onClick={onClickHandler}
         title={`Historical APR`}
         data={data}
         layout={{

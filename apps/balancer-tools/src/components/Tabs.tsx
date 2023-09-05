75ab07fd8bbad2c4efca5d6894171c0564eec638
luizakp <luiza.puccinelli06@gmail.com>
2023-09-05 15:48:57 -0300

    chore: bg tab trigger

--- apps/balancer-tools/src/components/Tabs.tsx
+++ apps/balancer-tools/src/components/Tabs.tsx
@@ -61,7 +61,7 @@ export function Tabs<T extends string | undefined>({
         value={value}
         defaultValue={defaultValue}
       >
-        <TabsPrimitive.List className="flex shrink-0 flex-col border-b border-blue1 bg-blue3">
+        <TabsPrimitive.List className="flex shrink-0 flex-col bg-blue3">
           {children}
         </TabsPrimitive.List>
       </TabsPrimitive.Root>
@@ -78,9 +78,11 @@ function TabItemTrigger({
   tabName,
   color,
   onClick,
+  triggerBgColor = "bg-blue3",
 }: React.PropsWithChildren<{
   tabName: string;
   color?: string;
+  triggerBgColor?: string;
   onClick?: (event: React.FormEvent<HTMLButtonElement>) => void;
 }>) {
   const { setValue } = useTabContext();
@@ -88,6 +90,7 @@ function TabItemTrigger({
     <TabsPrimitive.Trigger
       className={cn(
         "px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] hover:text-white hover:bg-blue4 data-[state=active]:text-white outline-none cursor-defaul} data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0]",
+        triggerBgColor,
       )}
       value={tabName}
       onClick={(e) => {

a8c11d1e18ccff0cc5dca1c9e18e7f53d521e048
Rawallon Cardoso <rawallon@gmail.com>
2023-09-06 16:42:35 -0300

    Update HistoricalAPRPlot.tsx

--- apps/balancer-tools/src/app/apr/pool/(components)/HistoricalCharts/HistoricalAPR/HistoricalAPRPlot.tsx
+++ apps/balancer-tools/src/app/apr/pool/(components)/HistoricalCharts/HistoricalAPR/HistoricalAPRPlot.tsx
@@ -4,23 +4,23 @@ import Plot from "#/components/Plot";
 
 export default function HistoricalAPRPlot({ data }: { data: Plotly.Data[] }) {
   return (
-      <Plot
-        title={`Historical APR`}
-        data={data}
-        config={{ displayModeBar: false }}
-        layout={{
-          margin: { t: 30, r: 20, l: 20, b: 30 },
-          autosize: true,
-          legend: { orientation: "h", y: -0.2, xanchor: "center", x: 0.5 },
-          xaxis: {
-            dtick: 1,
-            title: "Round Number",
-          },
-          yaxis: {
-            fixedrange: true,
-            title: "APR %",
-          },
-        }}
-      />
+    <Plot
+      title={`Historical APR`}
+      data={data}
+      config={{ displayModeBar: false }}
+      layout={{
+        margin: { t: 30, r: 20, l: 20, b: 30 },
+        autosize: true,
+        legend: { orientation: "h", y: -0.2, xanchor: "center", x: 0.5 },
+        xaxis: {
+          dtick: 1,
+          title: "Round Number",
+        },
+        yaxis: {
+          fixedrange: true,
+          title: "APR %",
+        },
+      }}
+    />
   );
 }

3da645de55a94915eb68aa2344402975715adb2a
Rawallon Cardoso <rawallon@gmail.com>
2023-09-11 12:24:31 -0300

    Refactor ChartSkelton
    Co-Authored-By: luiza <63298341+luizakp@users.noreply.github.com>

--- apps/balancer-tools/src/app/apr/(components)/(skeleton)/ChartSkelton.tsx
+++ apps/balancer-tools/src/app/apr/(components)/(skeleton)/ChartSkelton.tsx
@@ -1,21 +1,25 @@
+const Bar = ({ height }: { height: `h-${string}` }) => (
+  <div className={`w-full bg-blue6 rounded-t-lg ${height}`}></div>
+);
+
+function BarSet() {
+  return (
+    <>
+      <Bar height={"h-64"} />
+      <Bar height={"h-80"} />
+      <Bar height={"h-72"} />
+      <Bar height={"h-80"} />
+    </>
+  );
+}
+
 export default function ChartSkelton() {
   return (
-    <div
-      role="status"
-      className="w-full p-4 border border-blue6 bg-blue3 rounded animate-pulse md:p-6"
-    >
+    <div className="w-full p-4 border border-blue6 bg-blue3 rounded animate-pulse md:p-6">
       <div className="h-2.5 bg-blue6 rounded-full w-32 mb-2.5 pb-4"></div>
       <div className="flex items-baseline mt-4 space-x-6">
-        <div className="w-full h-64 bg-blue6 rounded-t-lg"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-80"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-72"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-80"></div>
-        <div className="w-full h-64 bg-blue6 rounded-t-lg"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-80"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-72"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-80"></div>
-        <div className="w-full h-64 bg-blue6 rounded-t-lg"></div>
-        <div className="w-full bg-blue6 rounded-t-lg h-80"></div>
+        <BarSet />
+        <BarSet />
       </div>
       <span className="sr-only">Loading...</span>
     </div>

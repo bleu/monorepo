135422c4ce5c0912f84600f97117931ac1d8a5d5
Rawallon Cardoso <rawallon@gmail.com>
2023-09-11 16:25:13 -0300

    Refactor getBALPriceByRound logic to return average for a given Round

--- apps/balancer-tools/src/app/apr/(utils)/getBALPriceByRound.ts
+++ apps/balancer-tools/src/app/apr/(utils)/getBALPriceByRound.ts
@@ -4,13 +4,39 @@ import { DefiLlamaAPI } from "#/lib/coingecko";
 
 import { Round } from "./rounds";
 
-export const getBALPriceByRound = async (round: Round) => {
-  return getTokenPriceByRound(
-    round,
-    "0xba100000625a3754423978a60c9317c58a424e3d",
-    1,
-  );
-};
+const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+
+export const getBALPriceByRound = async (round: Round) =>
+  Promise.all(
+    Array.from(
+      {
+        length:
+          Math.floor(
+            (round.endDate.getTime() -
+              round.startDate.getTime()) /
+              MILLISECONDS_IN_DAY,
+          ) + 1,
+      },
+      (_, i) =>
+        getTokenPriceByDate(
+          new Date(
+            round.startDate.getTime() +
+              i * MILLISECONDS_IN_DAY,
+          ),
+          "0xba100000625a3754423978a60c9317c58a424e3d",
+          1,
+        ),
+    ),
+  )
+    .then((prices) => {
+      const total = prices.reduce((sum, price) => sum + price, 0);
+      return total / prices.length;
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      throw error;
+    });
 
 export const getTokenPriceByDate = async (
   date: Date,

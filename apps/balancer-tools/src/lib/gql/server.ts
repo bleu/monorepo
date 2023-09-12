829fb60d19facf112f22fce42919db514a2ddcc7
luizakp <luiza.puccinelli06@gmail.com>
2023-09-12 16:27:12 -0300

    fix format

--- apps/balancer-tools/src/lib/gql/server.ts
+++ apps/balancer-tools/src/lib/gql/server.ts
@@ -13,7 +13,7 @@ import { GraphQLClient } from "graphql-request";
 
 export function impersonateWhetherDAO(
   chainId: string,
-  address: Address | undefined
+  address: Address | undefined,
 ) {
   const network = networkFor(chainId);
 
@@ -48,7 +48,7 @@ export const poolsMetadata = {
   client: clientFor(Subgraph.BalancerPoolsMetadata),
   gql: (chainId: number | string) =>
     poolMetadataSdks[networkFor(chainId)](
-      poolsMetadata.client(String(chainId))
+      poolsMetadata.client(String(chainId)),
     ),
 };
 

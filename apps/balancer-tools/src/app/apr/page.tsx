dcd3072a22ece3ae763a95c2936967791ce93608
Rawallon Cardoso <rawallon@gmail.com>
2023-09-18 12:06:15 -0300

    WIP: Changed Page for APR

--- apps/balancer-tools/src/app/apr/page.tsx
+++ apps/balancer-tools/src/app/apr/page.tsx
@@ -1,5 +1,50 @@
-import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import Breadcrumb from "./round/(components)/Breadcrumb";
+import KpisSkeleton from "./(components)/(skeleton)/KpisSkeleton";
+import RoundOverviewCards from "./round/(components)/RoundOverviewCards";
+import ChartSkelton from "./(components)/(skeleton)/ChartSkelton";
+import TopPoolsChartWrapper from "./round/(components)/TopPoolsChartWrapper";
+import TableSkeleton from "./(components)/(skeleton)/TableSkeleton";
+import PoolTableWrapper from "./round/(components)/PoolTableWrapper";
+import getFilteredRoundApiUrl from "./(utils)/getFilteredApiUrl";
+import { parseParamToDate } from "./api/route";
 
-export default function Page() {
-  redirect("/apr/round/current");
+export interface SearchParams {
+  minTVL?: string;
+  maxTVL?: string;
+  minAPR?: string;
+  maxAPR?: string;
+  tokens?: string;
+  type?: string;
+  network?: string;
+}
+
+export default function Page({
+  searchParams: { startAt, endAt },
+}: {
+  params: { roundId: string };
+  searchParams: SearchParams;
+}) {
+  const startAtDate = parseParamToDate(startAt)
+  const endAtDate = parseParamToDate(endAt)
+  
+  return (
+    <div className="flex flex-1 flex-col gap-y-3">
+       <Breadcrumb />
+      <Suspense fallback={<KpisSkeleton />}>
+        <RoundOverviewCards startAt={startAtDate} endAt={endAtDate}/>
+      </Suspense>
+      
+      <Suspense fallback={<ChartSkelton />}>
+        <TopPoolsChartWrapper startAt={startAtDate} endAt={endAtDate}/>
+      </Suspense>
+      {/*
+      <Suspense fallback={<TableSkeleton colNumbers={10} />}>
+        <PoolTableWrapper
+          roundId={roundId}
+          url={getFilteredRoundApiUrl(searchParams, roundId)}
+        />
+      </Suspense> */}
+    </div>
+  );
 }

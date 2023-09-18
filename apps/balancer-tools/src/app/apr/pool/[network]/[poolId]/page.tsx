b382ffd346138f249faa28c0d7450db7fca8f3a7
Rawallon Cardoso <rawallon@gmail.com>
2023-09-18 17:34:31 -0300

    Fix args on components for pool page

--- apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
+++ apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
@@ -1,32 +1,38 @@
 import { Suspense } from "react";
 
-import ChartSkelton from "#/app/apr/(components)/(skeleton)/ChartSkelton";
 import KpisSkeleton from "#/app/apr/(components)/(skeleton)/KpisSkeleton";
-import TableSkeleton from "#/app/apr/(components)/(skeleton)/TableSkeleton";
-import PoolOverviewCards from "#/app/apr/pool/(components)/PoolOverviewCards";
+import { parseParamToDate } from "#/app/apr/api/route";
 import Breadcrumb from "#/app/apr/round/(components)/Breadcrumb";
 
-import HistoricalCharts from "../../(components)/HistoricalCharts";
-import PoolTokens from "../../(components)/PoolTokens";
+import PoolOverviewCards from "../../(components)/PoolOverviewCards";
 import { YieldWarning } from "../../(components)/YieldWarning";
+import ChartSkelton from "#/app/apr/(components)/(skeleton)/ChartSkelton";
+import HistoricalCharts from "../../(components)/HistoricalCharts";
+import TableSkeleton from "#/app/apr/(components)/(skeleton)/TableSkeleton";
+import PoolTokens from "../../(components)/PoolTokens";
 
 export default async function Page({
-  params: { poolId },
+  params: {poolId},
+  searchParams: { startAt, endAt },
 }: {
-  params: { poolId: string };
+  searchParams: { startAt: string; endAt: string };
+  params: {poolId: string}
 }) {
+  const startAtDate = parseParamToDate(startAt);
+  const endAtDate = parseParamToDate(endAt);
+
   return (
     <div className="flex flex-1 h-full w-full flex-col justify-start rounded-3xl text-white gap-y-3">
       <Breadcrumb />
       <Suspense fallback={<KpisSkeleton />}>
-        <PoolOverviewCards poolId={poolId} />
+        <PoolOverviewCards startAt={startAtDate} endAt={endAtDate} poolId={poolId} />
       </Suspense>
       <YieldWarning />
       <Suspense fallback={<ChartSkelton />}>
-        <HistoricalCharts poolId={poolId} />
+        <HistoricalCharts startAt={startAtDate} endAt={endAtDate} />
       </Suspense>
       <Suspense fallback={<TableSkeleton colNumbers={2} />}>
-        <PoolTokens poolId={poolId} />
+        <PoolTokens startAt={startAtDate} endAt={endAtDate} poolId={poolId} />
       </Suspense>
     </div>
   );

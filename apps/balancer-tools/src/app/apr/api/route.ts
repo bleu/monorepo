313a56ee0f0bc5fe92ad33e4e9562638a71c88c9
Rawallon Cardoso <rawallon@gmail.com>
2023-09-18 12:05:49 -0300

    Exported Date helper functions from Route

--- apps/balancer-tools/src/app/apr/api/route.ts
+++ apps/balancer-tools/src/app/apr/api/route.ts
@@ -326,7 +326,7 @@ function validateSearchParams(
   }
 }
 
-function parseParamToDate(dateStr: string) {
+export function parseParamToDate(dateStr: string) {
   const parts = dateStr.split("-");
   if (parts.length !== 3) {
     throw new Error("Invalid date format. Use 'dd-mm-yyyy'.");
@@ -352,7 +352,7 @@ function parseParamToDate(dateStr: string) {
   return date;
 }
 
-function formatDateToMMDDYYYY(date: Date) {
+export function formatDateToMMDDYYYY(date: Date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0"); // Add 1 to month since it's 0-indexed
   const day = String(date.getDate()).padStart(2, "0");

75f8050c4712d110c85c31fe2a7119dac7373dbb
Rawallon Cardoso <rawallon@gmail.com>
2023-09-19 13:23:44 -0300

    Refactor computeAverages

--- apps/balancer-tools/src/app/apr/api/(utils)/computeAverages.ts
+++ apps/balancer-tools/src/app/apr/api/(utils)/computeAverages.ts
@@ -1,16 +1,42 @@
-import { calculatePoolData } from "../../(utils)/calculatePoolStats";
-import { PoolStatsWithoutVotingShareAndCollectedFees } from "../route";
+import { PoolStats, PoolStatsWithoutVotingShareAndCollectedFees, tokenAPR } from "../route";
 
-export const computeAverages = (formattedPoolData: {
-  [key: string]: calculatePoolData[];
-}): PoolStatsWithoutVotingShareAndCollectedFees => {
-  const averages: PoolStatsWithoutVotingShareAndCollectedFees = {
+export const computeAverages = (
+  formattedPoolData: { [key: string]: PoolStats[] }
+): PoolStatsWithoutVotingShareAndCollectedFees => {
+  const averages: PoolStatsWithoutVotingShareAndCollectedFees = initializeAverages();
+
+  const uniqueEntries: {
+    [key: string]: { idx: number; occurences: number };
+  } = {};
+
+  let totalDataCount = 0;
+
+  for (const key in formattedPoolData) {
+    if (Object.hasOwnProperty.call(formattedPoolData, key)) {
+      const dataArr = formattedPoolData[key];
+      dataArr.forEach((data) => {
+        accumulateData(averages, data);
+        accumulateTokens(averages.apr.breakdown.tokens, data.apr.breakdown.tokens, uniqueEntries);
+        accumulateOtherMetrics(averages, data);
+        totalDataCount++;
+      });
+    }
+  }
+
+  if (totalDataCount > 0) {
+    calculateAverages(averages, totalDataCount, uniqueEntries);
+  }
+
+  return averages;
+};
+
+function initializeAverages(): PoolStatsWithoutVotingShareAndCollectedFees {
+  return {
     apr: {
       total: 0,
       breakdown: {
         veBAL: 0,
         swapFee: 0,
-        //TODO: on #BAL-795 get tokenAPR from total
         tokens: {
           total: 0,
           breakdown: [],
@@ -21,58 +47,54 @@ export const computeAverages = (formattedPoolData: {
     tvl: 0,
     volume: 0,
   };
+}
 
-  let totalDataCount = 0;
-  const uniqueEntries: { [key: string]: { idx: number; occorencies: number } } =
-    {};
+function accumulateData(target: PoolStatsWithoutVotingShareAndCollectedFees, source: PoolStatsWithoutVotingShareAndCollectedFees): void {
+  target.apr.total += source.apr.total;
+  target.apr.breakdown.veBAL += source.apr.breakdown.veBAL || 0;
+  target.apr.breakdown.swapFee += source.apr.breakdown.swapFee;
+  target.apr.breakdown.tokens.total += source.apr.breakdown.tokens.total;
+}
 
-  for (const key in formattedPoolData) {
-    if (Object.hasOwnProperty.call(formattedPoolData, key)) {
-      const dataArr = formattedPoolData[key];
-      dataArr.forEach((data) => {
-        averages.apr.total += data.apr.total;
-        averages.apr.breakdown.veBAL += data.apr.breakdown.veBAL || 0;
-        averages.apr.breakdown.swapFee += data.apr.breakdown.swapFee;
-        averages.apr.breakdown.tokens.total +=
-          data.apr.breakdown.tokens.total || 0;
-
-        data.apr.breakdown.tokens.breakdown.map((tokenData) => {
-          if (!uniqueEntries[tokenData.symbol]) {
-            uniqueEntries[tokenData.symbol] = {
-              idx: averages.apr.breakdown.tokens.breakdown.length,
-              occorencies: 0,
-            };
-            averages.apr.breakdown.tokens.breakdown.push(tokenData);
-          } else {
-            uniqueEntries[tokenData.symbol].occorencies++;
-            averages.apr.breakdown.tokens.breakdown[
-              uniqueEntries[tokenData.symbol].idx
-            ].yield += tokenData.yield;
-          }
-        });
-
-        averages.balPriceUSD += data.balPriceUSD;
-        averages.tvl += data.tvl;
-        averages.volume += data.volume;
-        totalDataCount++;
-      });
-    }
-  }
-
-  if (totalDataCount > 0) {
-    averages.apr.total /= totalDataCount;
-    averages.apr.breakdown.veBAL /= totalDataCount;
-    averages.apr.breakdown.swapFee /= totalDataCount;
-    averages.balPriceUSD /= totalDataCount;
-    averages.tvl /= totalDataCount;
-    averages.volume /= totalDataCount;
-    averages.apr.breakdown.tokens.breakdown.map((tokenData) => {
-      return {
-        ...tokenData,
-        yield: tokenData.yield / uniqueEntries[tokenData.symbol].occorencies,
+function accumulateTokens(
+  targetTokens: { total: number; breakdown: tokenAPR[] },
+  sourceTokens: { total: number; breakdown: tokenAPR[] },
+  uniqueEntries: { [key: string]: { idx: number; occurences: number } }
+): void {
+  sourceTokens.breakdown.forEach((tokenData) => {
+    if (!uniqueEntries[tokenData.symbol]) {
+      uniqueEntries[tokenData.symbol] = {
+        idx: targetTokens.breakdown.length,
+        occurences: 0,
       };
-    });
-  }
+      targetTokens.breakdown.push(tokenData);
+    } else {
+      uniqueEntries[tokenData.symbol].occurences++;
+      const existingTokenData = targetTokens.breakdown[uniqueEntries[tokenData.symbol].idx];
+      existingTokenData.yield += tokenData.yield;
+    }
+  });
+}
 
-  return averages;
-};
+function accumulateOtherMetrics(target: PoolStatsWithoutVotingShareAndCollectedFees, source: PoolStats): void {
+  target.balPriceUSD += source.balPriceUSD;
+  target.tvl += source.tvl;
+  target.volume += source.volume;
+}
+
+function calculateAverages(
+  averages: PoolStatsWithoutVotingShareAndCollectedFees,
+  totalDataCount: number,
+  uniqueEntries: { [key: string]: { idx: number; occurences: number } }
+): void {
+  averages.apr.total /= totalDataCount;
+  averages.apr.breakdown.veBAL /= totalDataCount;
+  averages.apr.breakdown.swapFee /= totalDataCount;
+  averages.balPriceUSD /= totalDataCount;
+  averages.tvl /= totalDataCount;
+  averages.volume /= totalDataCount;
+
+  averages.apr.breakdown.tokens.breakdown.forEach((tokenData) => {
+    tokenData.yield /= uniqueEntries[tokenData.symbol].occurences;
+  });
+}

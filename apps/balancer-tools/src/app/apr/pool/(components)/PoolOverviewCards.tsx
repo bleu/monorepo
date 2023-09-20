49126ff51f02c3262e9de45b42732a405fab2cf8
Rawallon Cardoso <rawallon@gmail.com>
2023-09-19 22:59:06 -0300

    Removed unused PoolOverviewCards logic

--- apps/balancer-tools/src/app/apr/pool/(components)/PoolOverviewCards.tsx
+++ apps/balancer-tools/src/app/apr/pool/(components)/PoolOverviewCards.tsx
@@ -6,20 +6,6 @@ import OverviewCards, {
 import { formatAPR, formatTVL } from "../../(utils)/formatPoolStats";
 import { BASE_URL, PoolStatsResults } from "../../api/route";
 
-async function AverageTVLCard({ poolId }: { poolId: string }) {
-  const data: PoolStatsResults = await fetcher(
-    `${BASE_URL}/apr/api/?poolId=${poolId}`,
-  );
-  return <div>{formatTVL(data.average.tvl)}</div>;
-}
-
-async function AverageAPRCard({ poolId }: { poolId: string }) {
-  const data: PoolStatsResults = await fetcher(
-    `${BASE_URL}/apr/api/?poolId=${poolId}`,
-  );
-  return <div>{formatAPR(data.average.apr.total)}</div>;
-}
-
 export default async function PoolOverviewCards({
   startAt,
   endAt,
@@ -34,31 +20,20 @@ export default async function PoolOverviewCards({
     content: JSX.Element | string;
     tooltip?: string;
   }[] = [];
-  if (startAt || endAt) {
-    const results: PoolStatsResults = await fetcher(
-      `${BASE_URL}/apr/api/?poolId=${poolId}&startAt${startAt}&endAt=${endAt}`,
-    );
+  const results: PoolStatsResults = await fetcher(
+    `${BASE_URL}/apr/api/?poolId=${poolId}&startAt${startAt}&endAt=${endAt}`,
+  );
 
-    cardsDetails.push(
-      ...[
-        { title: "TVL", content: formatTVL(results.average.tvl) },
-        {
-          title: "veBAL APR",
-          content: formatAPR(results.average.apr.breakdown.veBAL),
-        },
-        ...getDatesDetails(startAt, endAt),
-      ],
-    );
-  } else {
-    cardsDetails.push(
-      ...[
-        { title: "Avg. TVL", content: <AverageTVLCard poolId={poolId} /> },
-        {
-          title: "Avg. veBAL APR",
-          content: <AverageAPRCard poolId={poolId} />,
-        },
-      ],
-    );
-  }
+  cardsDetails.push(
+    ...[
+      { title: "Avg. TVL", content: formatTVL(results.average.tvl) },
+      {
+        title: "Avg. veBAL APR",
+        content: formatAPR(results.average.apr.breakdown.veBAL),
+      },
+      ...getDatesDetails(startAt, endAt),
+    ],
+  );
+
   return <OverviewCards cardsDetails={cardsDetails} />;
 }

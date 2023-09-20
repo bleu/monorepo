9144857ea43cc78b1844678b1eadbff46606db51
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-09-20 18:56:42 -0300

    wip: add slider component

--- apps/balancer-tools/src/app/poolsimulator/(components)/PoolParamsForm.tsx
+++ apps/balancer-tools/src/app/poolsimulator/(components)/PoolParamsForm.tsx
@@ -3,7 +3,7 @@ import { forwardRef, useEffect, useImperativeHandle } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
 import Button from "#/components/Button";
-import { Input } from "#/components/Input";
+import { Slider } from "#/components/Slider";
 import { useTabContext } from "#/components/Tabs";
 import { Form, FormField } from "#/components/ui/form";
 import { AnalysisData } from "#/contexts/PoolSimulatorContext";
@@ -38,19 +38,29 @@ const docsMapper = {
   [PoolTypeEnum.Fx]: "https://docs.xave.co/product-overview-1/fxpools",
 };
 
+const dynamicGetMinMaxAndStep = {
+  getMin: (n?: number) => (n ? n / 100 : 0.01),
+  getMax: (n?: number) => (n ? n * 100 : 0.01),
+  getStep: (n?: number) => (n ? n / 100 : 0.01),
+} as const;
+
+const SwapFeeInput = {
+  name: "swapFee",
+  label: "Swap Fee",
+  unit: "%",
+  transformFromDataToForm: (n?: number) =>
+    typeof n === "number" ? n * 100 : undefined,
+  transformFromFormToData: (n?: number) =>
+    typeof n === "number" ? n / 100 : undefined,
+  tooltip: "Percentage fee charged on swaps",
+  getMin: (_n?: number) => 0,
+  getMax: (_n?: number) => 10,
+  getStep: (_n?: number) => 0.001,
+} as const;
+
 const inputMapper = {
   [PoolTypeEnum.MetaStable]: [
-    {
-      name: "swapFee",
-      label: "Swap Fee",
-
-      unit: "%",
-      transformFromDataToForm: (n?: number) =>
-        typeof n === "number" ? n * 100 : undefined,
-      transformFromFormToData: (n?: number) =>
-        typeof n === "number" ? n / 100 : undefined,
-      tooltip: "Percentage fee charged on swaps",
-    },
+    SwapFeeInput,
     {
       name: "ampFactor",
       label: "Amplification Factor",
@@ -59,19 +69,11 @@ const inputMapper = {
       transformFromFormToData: (n?: number) => n,
       tooltip:
         "Balance the constant-sum and constant-product AMMs. The higher the value, the closer the AMM behavior is to constant-sum",
+      ...dynamicGetMinMaxAndStep,
     },
   ],
   [PoolTypeEnum.GyroE]: [
-    {
-      name: "swapFee",
-      label: "Swap Fee",
-      unit: "%",
-      transformFromDataToForm: (n?: number) =>
-        typeof n === "number" ? n * 100 : undefined,
-      transformFromFormToData: (n?: number) =>
-        typeof n === "number" ? n / 100 : undefined,
-      tooltip: "Percentage fee charged on swaps",
-    },
+    SwapFeeInput,
     {
       name: "alpha",
       label: "Alpha",
@@ -79,6 +81,7 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => n,
       transformFromFormToData: (n?: number) => n,
       tooltip: "Lower price bound considering token 2 / token 1",
+      ...dynamicGetMinMaxAndStep,
     },
     {
       name: "beta" as const,
@@ -87,6 +90,7 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => n,
       transformFromFormToData: (n?: number) => n,
       tooltip: "Upper price bound considering token 2 / token 1",
+      ...dynamicGetMinMaxAndStep,
     },
     {
       name: "lambda",
@@ -95,6 +99,9 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => n,
       transformFromFormToData: (n?: number) => n,
       tooltip: "Stretching factor. 1 implies a circle",
+      getMin: (_n?: number) => 0.01,
+      getMax: (_n?: number) => 1,
+      getStep: (_n?: number) => 0.01,
     },
     {
       name: "c",
@@ -103,6 +110,7 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => n,
       transformFromFormToData: (n?: number) => n,
       tooltip: "First coordinate of rotation point",
+      ...dynamicGetMinMaxAndStep,
     },
     {
       name: "s",
@@ -111,19 +119,11 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => n,
       transformFromFormToData: (n?: number) => n,
       tooltip: "Second coordinate of rotation point",
+      ...dynamicGetMinMaxAndStep,
     },
   ],
   [PoolTypeEnum.Gyro2]: [
-    {
-      name: "swapFee",
-      label: "Swap Fee",
-      unit: "%",
-      transformFromDataToForm: (n?: number) =>
-        typeof n === "number" ? n * 100 : undefined,
-      transformFromFormToData: (n?: number) =>
-        typeof n === "number" ? n / 100 : undefined,
-      tooltip: "Percentage fee charged on swaps",
-    },
+    SwapFeeInput,
     {
       name: "sqrtAlpha" as const,
       label: "Alpha",
@@ -131,6 +131,7 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n ** 2 : undefined),
       transformFromFormToData: (n?: number) => (n ? n ** (1 / 2) : undefined),
       tooltip: "Lower price bound considering token 2 / token 1",
+      ...dynamicGetMinMaxAndStep,
     },
     {
       name: "sqrtBeta" as const,
@@ -139,21 +140,11 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n ** 2 : undefined),
       transformFromFormToData: (n?: number) => (n ? n ** (1 / 2) : undefined),
       tooltip: "Upper price bound considering token 2 / token 1",
+      ...dynamicGetMinMaxAndStep,
     },
   ],
   [PoolTypeEnum.Gyro3]: [
-    {
-      name: "swapFee",
-      label: "Swap Fee",
-
-      unit: "%",
-      transformFromDataToForm: (n?: number) => {
-        return typeof n === "number" ? n * 100 : undefined;
-      },
-      transformFromFormToData: (n?: number) =>
-        typeof n === "number" ? n / 100 : undefined,
-      tooltip: "Percentage fee charged on swaps",
-    },
+    SwapFeeInput,
     {
       name: "root3Alpha" as const,
       label: "Alpha",
@@ -161,20 +152,11 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n ** 3 : undefined),
       transformFromFormToData: (n?: number) => (n ? n ** (1 / 3) : undefined),
       tooltip: "Lower bound of the price range",
+      ...dynamicGetMinMaxAndStep,
     },
   ],
   [PoolTypeEnum.Fx]: [
-    {
-      name: "swapFee",
-      label: "Swap Fee",
-      unit: "%",
-      transformFromDataToForm: (n?: number) => {
-        return typeof n === "number" ? n * 100 : undefined;
-      },
-      transformFromFormToData: (n?: number) =>
-        typeof n === "number" ? n / 100 : undefined,
-      tooltip: "Percentage fee charged on swaps",
-    },
+    SwapFeeInput,
     {
       name: "alpha",
       label: "Alpha",
@@ -182,6 +164,9 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n * 100 : undefined),
       transformFromFormToData: (n?: number) => (n ? n / 100 : undefined),
       tooltip: "Maximum and minimum allocation of each reserve",
+      getMin: (_n?: number) => 55,
+      getMax: (_n?: number) => 95,
+      getStep: (_n?: number) => 1,
     },
     {
       name: "beta" as const,
@@ -190,6 +175,9 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n * 100 : undefined),
       transformFromFormToData: (n?: number) => (n ? n / 100 : undefined),
       tooltip: "Depth of liquidity pool without price slippage",
+      getMin: (_n?: number) => 0,
+      getMax: (_n?: number) => 100,
+      getStep: (_n?: number) => 1,
     },
     {
       name: "delta",
@@ -198,6 +186,9 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n * 100 : undefined),
       transformFromFormToData: (n?: number) => (n ? n / 100 : undefined),
       tooltip: "Rate of price change",
+      getMin: (_n?: number) => 0,
+      getMax: (_n?: number) => 100,
+      getStep: (_n?: number) => 1,
     },
     {
       name: "epsilon",
@@ -206,6 +197,9 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n * 100 : undefined),
       transformFromFormToData: (n?: number) => (n ? n / 100 : undefined),
       tooltip: "Fixed fee",
+      getMin: (_n?: number) => 0,
+      getMax: (_n?: number) => 10,
+      getStep: (_n?: number) => 0.001,
     },
     {
       name: "lambda",
@@ -214,19 +208,22 @@ const inputMapper = {
       transformFromDataToForm: (n?: number) => (n ? n * 100 : undefined),
       transformFromFormToData: (n?: number) => (n ? n / 100 : undefined),
       tooltip: "Dynamic fee",
+      getMin: (_n?: number) => 0,
+      getMax: (_n?: number) => 50,
+      getStep: (_n?: number) => 1,
     },
   ],
 } as const;
 
 const createPayload = (
   poolType: keyof typeof inputMapper,
-  fieldData: FieldValues,
+  fieldData: FieldValues
 ): AnalysisData => ({
   poolParams: Object.fromEntries(
     inputMapper[poolType].map((input) => [
       input.name,
       input.transformFromFormToData(fieldData[input.name]),
-    ]),
+    ])
   ),
   tokens: fieldData.tokens,
   poolType: poolType,
@@ -301,34 +298,35 @@ export const PoolParamsForm = forwardRef<unknown, PoolParamsFormProps>(
         id="initial-data-form"
       >
         <div className="flex flex-col gap-4">
-          {inputMapper[poolType].map((input) => (
-            <div className="relative">
+          {inputMapper[poolType].map((input) => {
+            const value = input.transformFromDataToForm(
+              data.poolParams?.[input.name]
+            );
+            return (
               <FormField
                 name={input.name}
-                render={({ field }) => (
-                  <Input
-                    {...field}
+                render={() => (
+                  <Slider
+                    // {...field}
+                    name={input.name}
                     label={input.label}
-                    type="number"
                     validation={{
                       required: true,
                       valueAsNumber: true,
                       value: data.poolParams?.[input.name],
                     }}
-                    defaultValue={input.transformFromDataToForm(
-                      data.poolParams?.[input.name],
-                    )}
+                    defaultValue={value}
                     placeholder={`Enter ${input.label}`}
                     tooltipText={input.tooltip}
                     tooltipLink={docsMapper[poolType]}
+                    min={input.getMin(value)}
+                    max={input.getMax(value)}
+                    step={input.getStep(value)}
                   />
                 )}
               />
-              <span className="absolute top-8 right-2 flex items-center text-slate10">
-                {input.unit}
-              </span>
-            </div>
-          ))}
+            );
+          })}
           <div className="flex flex-col">
             <label className="mb-2 block text-sm text-slate12">Tokens</label>
             {errors?.tokens?.message && (
@@ -349,5 +347,5 @@ export const PoolParamsForm = forwardRef<unknown, PoolParamsFormProps>(
         </div>
       </Form>
     );
-  },
+  }
 );

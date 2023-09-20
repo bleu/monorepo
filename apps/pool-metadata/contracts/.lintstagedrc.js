e223a44ab541beeb85cd95cea307e3b99063eae0
José Ribeiro <me@joseribeiro.dev>
2023-09-20 15:09:44 -0300

    remove eslint from apps/pool-metadata/contracts

--- apps/pool-metadata/contracts/.lintstagedrc.js
+++ apps/pool-metadata/contracts/.lintstagedrc.js
@@ -3,5 +3,4 @@
 module.exports = {
   "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit --skipLibCheck",
   "*.{md,html,mjml,json,graphql,yml,css,scss}": "prettier --write",
-  "*.{js,jsx,ts,tsx}": "eslint -c ../../../.eslintrc.js --fix --no-ignore --max-warnings 0",
 };

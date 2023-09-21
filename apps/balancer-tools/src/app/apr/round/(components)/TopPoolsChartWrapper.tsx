065ff5c434b0511a9b15447dd244f4e712b6bc83
José Ribeiro <jose@bleu.studio>
2023-09-21 10:28:59 -0300

    ensure chart data is also filtered, fix bug with latest day's volume (#310)
    * ensure APR chart data uses UI filters
    
    * ensure volume gets calculated correctly in APR
    
    closes BAL-864

--- apps/balancer-tools/src/app/apr/round/(components)/TopPoolsChartWrapper.tsx
+++ apps/balancer-tools/src/app/apr/round/(components)/TopPoolsChartWrapper.tsx
@@ -1,17 +1,16 @@
 import { fetcher } from "#/utils/fetcher";
 
-import getFilteredRoundApiUrl from "../../(utils)/getFilteredApiUrl";
 import { PoolStatsResults } from "../../api/route";
 import TopPoolsChart from "./TopPoolsChart";
 
 export default async function TopPoolsChartWrapper({
   roundId,
+  url,
 }: {
   roundId: string;
+  url: string;
 }) {
-  const topAprApi = await fetcher<PoolStatsResults>(
-    getFilteredRoundApiUrl({}, roundId),
-  );
+  const topAprApi = await fetcher<PoolStatsResults>(url);
 
   return <TopPoolsChart roundId={roundId} ApiResult={topAprApi} />;
 }

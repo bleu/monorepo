a75949e5727da961936c04c965c566016b3fc8b4
Rawallon Cardoso <rawallon@gmail.com>
2023-09-22 10:41:33 -0300

    Refactor getBALPriceForDateRange to recieve timestamp

--- apps/balancer-tools/src/app/apr/(utils)/getBALPriceForDateRange.ts
+++ apps/balancer-tools/src/app/apr/(utils)/getBALPriceForDateRange.ts
@@ -3,16 +3,10 @@ import { networkFor } from "@bleu-balancer-tools/utils";
 import { withCache } from "#/lib/cache";
 import { DefiLlamaAPI } from "#/lib/coingecko";
 
-const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+import { calculateDaysBetween } from "../api/(utils)/date";
+
 const BAL_TOKEN_ADDRESS = "0xba100000625a3754423978a60c9317c58a424e3d";
 const BAL_TOKEN_NETWORK = 1;
-
-/**
- * Calculates the number of days between two dates, inclusive of both start and end dates.
- */
-const calculateDaysBetween = (startDate: Date, endDate: Date) =>
-  Math.floor((endDate.getTime() - startDate.getTime()) / MILLISECONDS_IN_DAY) +
-  1;
 
 /**
  * Calculates the average of an array of numbers.
@@ -21,37 +15,43 @@ const calculateAverage = (arr: number[]) =>
   arr.reduce((sum, val) => sum + val, 0) / arr.length;
 
 export const getBALPriceForDateRange = withCache(
-  async function getBALPriceByRoundFn(startAt: Date, endAt: Date) {
-    const days = calculateDaysBetween(startAt, endAt);
-    const pricePromises = Array.from({ length: days }, (_, i) =>
-      getTokenPriceByDate(
-        new Date(startAt.getTime() + i * MILLISECONDS_IN_DAY),
+  async function getBALPriceByRoundFn(
+    startAtTimestamp: number,
+    endAtTimestamp: number,
+  ) {
+    const numberOfDays = calculateDaysBetween(
+      startAtTimestamp * 1000,
+      endAtTimestamp * 1000,
+    );
+    const pricePromises = Array.from({ length: numberOfDays }, (_) => {
+      return getTokenPriceByDate(
+        endAtTimestamp * 1000,
         BAL_TOKEN_ADDRESS,
         BAL_TOKEN_NETWORK,
-      ),
-    );
+      );
+    });
     try {
       const prices = await Promise.all(pricePromises);
       return calculateAverage(prices);
     } catch (error) {
       // TODO: BAL-782 - Add sentry here
       // eslint-disable-next-line no-console
-      console.error(`Error fetching BAL price between ${startAt} and ${endAt}`);
+      console.error(
+        `Error fetching BAL price between ${startAtTimestamp} and ${endAtTimestamp}`,
+      );
       throw error;
     }
   },
 );
 
 export const getTokenPriceByDate = withCache(async function getTokenPriceByDate(
-  date: Date,
+  dateTimestamp: number,
   tokenAddress: string,
   tokenNetwork: number,
 ) {
   const token = `${networkFor(tokenNetwork).toLowerCase()}:${tokenAddress}`;
-  const relevantDateForPrice = Math.min(Date.now(), date.getTime());
-  const api = new DefiLlamaAPI();
-
-  const response = await api.getHistoricalPrice(
+  const relevantDateForPrice = Math.min(Date.now(), dateTimestamp);
+  const response = await new DefiLlamaAPI().getHistoricalPrice(
     new Date(relevantDateForPrice),
     [token],
   );
--- apps/balancer-tools/src/app/apr/api/(utils)/date.ts
+++ apps/balancer-tools/src/app/apr/api/(utils)/date.ts
@@ -1,3 +1,4 @@
+const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
 export function formatDateToMMDDYYYY(date: Date): string {
   const year = date.getUTCFullYear();
   const month = String(date.getUTCMonth() + 1).padStart(2, "0");
@@ -6,16 +7,24 @@ export function formatDateToMMDDYYYY(date: Date): string {
 }
 
 export const generateDateRange = (startDate: Date, endDate: Date) => {
-  const dayMilliseconds = 24 * 60 * 60 * 1000;
   const dateRange = [];
 
   for (
     let currentDate = startDate;
     currentDate <= endDate;
-    currentDate = new Date(currentDate.getTime() + dayMilliseconds)
+    currentDate = new Date(currentDate.getTime() + MILLISECONDS_IN_DAY)
   ) {
     dateRange.push(currentDate);
   }
 
   return dateRange;
 };
+
+
+/**
+ * Calculates the number of days between two dates, inclusive of both start and end dates.
+ */
+export const calculateDaysBetween = (startDate: number, endDate: number) =>
+  Math.floor((endDate - startDate) / MILLISECONDS_IN_DAY) + 1;
+
+

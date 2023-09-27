a25d12a97765e19c36a7754e078cf467a2a9a974
luizakp <luiza.puccinelli06@gmail.com>
2023-09-27 15:32:06 -0300

    filter gauges with more 10K of TVL currently

--- apps/balancer-tools/pre-build.js
+++ apps/balancer-tools/pre-build.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-env node */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
@@ -26,7 +27,7 @@ async function gql(endpoint, query, variables = {}, headers = {}) {
   return response.json();
 }
 
-const ENDPOINT = "https://api-v3.balancer.fi/graphql";
+const ENDPOINT_V3 = "https://api-v3.balancer.fi/graphql";
 
 const VOTING_GAUGES_QUERY = `
 query VeBalGetVotingList {
@@ -52,10 +53,88 @@ query VeBalGetVotingList {
 }
 `;
 
+const BASE_ENDPOINT_V2 =
+  "https://api.thegraph.com/subgraphs/name/balancer-labs";
+const EXISTING_NETWORKS = {
+  Ethereum: `${BASE_ENDPOINT_V2}/balancer-v2`,
+  Goerli: `${BASE_ENDPOINT_V2}/balancer-goerli-v2`,
+  Polygon: `${BASE_ENDPOINT_V2}/balancer-polygon-v2`,
+  PolygonZKEVM:
+    "https://api.studio.thegraph.com/query/24660/balancer-polygon-zk-v2/version/latest",
+  Arbitrum: `${BASE_ENDPOINT_V2}/balancer-arbitrum-v2`,
+  Gnosis: `${BASE_ENDPOINT_V2}/balancer-gnosis-chain-v2`,
+  Optimism: `${BASE_ENDPOINT_V2}/balancer-optimism-v2`,
+  Base: "https://api.studio.thegraph.com/query/24660/balancer-base-v2/version/latest",
+  Avalanche: `${BASE_ENDPOINT_V2}/balancer-avalanche-v2`,
+};
+
+const POOLS_WITHOUT_GAUGE_QUERY = `
+query Pools($skip: Int!) {
+  pools(
+    where: { totalLiquidity_gt: 10000 }
+    orderBy: totalLiquidity
+    orderDirection: desc
+    skip: $skip
+  ) {
+    id
+    address
+    symbol
+    poolType
+    createTime
+    totalLiquidity
+    tokens {
+      address
+      symbol
+      weight
+    }
+  }
+}
+`;
+
 async function main() {
-  const response = await gql(ENDPOINT, VOTING_GAUGES_QUERY);
+  const response = await gql(ENDPOINT_V3, VOTING_GAUGES_QUERY);
   const votingGauges = response.data.veBalGetVotingList;
-  fs.writeFileSync("src/data/voting-gauges.json", JSON.stringify(votingGauges));
+  const poolIds = [];
+
+  for (const networkName in EXISTING_NETWORKS) {
+    const networkEndpoint = EXISTING_NETWORKS[networkName];
+
+    let skipValue = 0;
+    let allPools = [];
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const response = await gql(networkEndpoint, POOLS_WITHOUT_GAUGE_QUERY, {
+        skip: skipValue,
+      });
+      const pools = response.data.pools;
+      if (pools.length === 0) {
+        break;
+      }
+      for (const pool of pools) {
+        poolIds.push(pool.id);
+      }
+      allPools = allPools.concat(pools);
+      skipValue += 1000;
+    }
+
+    const fileName = `pools-${networkName.toLowerCase()}.json`;
+    fs.writeFileSync(`src/data/${fileName}`, JSON.stringify(allPools));
+    console.log(
+      `File created for ${networkName} with ${allPools.length} pools: ${fileName}`,
+    );
+  }
+
+  const votingGaugesFiltered = votingGauges.filter((pool) =>
+    poolIds.includes(pool.id),
+  );
+  console.log(
+    `File created pools with gauges with ${votingGaugesFiltered.length} pools`,
+  );
+  fs.writeFileSync(
+    "src/data/voting-gauges.json",
+    JSON.stringify(votingGaugesFiltered),
+  );
 }
 
 main();

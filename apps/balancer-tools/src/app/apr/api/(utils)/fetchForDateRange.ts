81e1ff1cdd0f681a227a781fcd87614450be3b64
RawallonDeucredito <rawallon@gmail.com>
2023-09-29 00:19:41 -0300

    Implemented p-limiter on fetchForDateRange endpoint

--- apps/balancer-tools/src/app/apr/api/(utils)/fetchForDateRange.ts
+++ apps/balancer-tools/src/app/apr/api/(utils)/fetchForDateRange.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 import * as Sentry from "@sentry/nextjs";
+// @ts-ignore: TS2307
+import pLimit from 'p-limit';
 
 import POOLS_WITHOUT_GAUGES from "#/data/pools-without-gauge.json";
 import { fetcher } from "#/utils/fetcher";
@@ -8,6 +10,9 @@ import { fetcher } from "#/utils/fetcher";
 import { BASE_URL } from "../../(utils)/types";
 import { PoolStatsData, PoolStatsResults } from "../route";
 import { formatDateToMMDDYYYY } from "./date";
+
+// Create a p-limit instance with a concurrency limit of 1
+const limit = pLimit(55);
 
 export async function fetchDataForDateRange(
   startDate: Date,
@@ -22,10 +27,15 @@ export async function fetchDataForDateRange(
     existingPoolForDate.map(async (pool) => {
       let gaugesData;
       try {
-        gaugesData = await fetcher<PoolStatsResults>(
-          `${BASE_URL}/apr/api?startAt=${formatDateToMMDDYYYY(
-            startDate,
-          )}&endAt=${formatDateToMMDDYYYY(endDate)}&poolId=${pool.id}`,
+        // Use limit to control concurrency here
+        gaugesData = await limit(() =>
+        {
+          console.log('>> Returned for pool', pool.id)
+          return fetcher<PoolStatsResults>(
+            `${BASE_URL}/apr/api?startAt=${formatDateToMMDDYYYY(
+              startDate,
+            )}&endAt=${formatDateToMMDDYYYY(endDate)}&poolId=${pool.id}`,
+          )}
         );
       } catch (error) {
         console.log(error);
@@ -40,9 +50,9 @@ export async function fetchDataForDateRange(
       if (gaugesData) {
         Object.entries(gaugesData.perDay).forEach(([dayStr, poolData]) => {
           if (perDayData[dayStr]) {
-            perDayData[dayStr].push(poolData[0]);
+            perDayData[dayStr].push((poolData as PoolStatsData[])[0]);
           } else {
-            perDayData[dayStr] = [poolData[0]];
+            perDayData[dayStr] = [(poolData as PoolStatsData[])[0]];
           }
         });
       }

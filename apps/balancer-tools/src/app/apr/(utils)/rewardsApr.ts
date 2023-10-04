c06d1c3ea43490b286ae86f2cc8f319c73cc5285
luizakp <luiza.puccinelli06@gmail.com>
2023-10-04 18:25:16 -0300

    chore: calculate reward APR

--- apps/balancer-tools/src/app/apr/(utils)/rewardsApr.ts
+++ apps/balancer-tools/src/app/apr/(utils)/rewardsApr.ts
@@ -1,20 +1,85 @@
 import { PoolRewardsQuery } from "@bleu-balancer-tools/gql/src/balancer-rewards/__generated__/Ethereum";
+import { NetworkChainId } from "@bleu-balancer-tools/utils";
 
 import { rewards } from "#/lib/gql/server";
 import { ArrElement, GetDeepProp } from "#/utils/getTypes";
+
+import { fetchPoolAveragesForDateRange } from "./calculatePoolStats";
 
 type RewardInfo = ArrElement<
   GetDeepProp<PoolRewardsQuery, "rewardTokenDeposits">
 >;
 
-export async function getTokensRewards() {
-  const response = await rewards.gql(String(8453)).PoolRewards({
-    poolId:
-      "0xe40cbccba664c7b1a953827c062f5070b78de86800020000000000000000001b",
+export async function getRewardsApr({
+  startAt,
+  endAt,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  poolId,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  network,
+}: {
+  startAt: number;
+  endAt: number;
+  poolId: string;
+  network: string;
+}) {
+  const tempPoolId =
+    "0x17e7d59bb209a3215ccc25fffef7161498b7c10d000200000000000000000020";
+  const tempNetwork = String(NetworkChainId.BASE);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [tvl, volume, symbol, tokenBalance, bptPrice] =
+    await fetchPoolAveragesForDateRange(
+      tempPoolId,
+      tempNetwork,
+      startAt,
+      endAt,
+    );
+
+  const tokensRewards = await getTokensRewards({
+    startAt,
+    endAt,
+    poolId: tempPoolId,
+    network: tempNetwork,
   });
 
-  const startAt = 1694044800;
-  const endAt = 1694131200;
+  if (tokensRewards.length === 0) {
+    return [];
+  }
+
+  const rewards = tokensRewards.map((tokenReward) => {
+    const tokenAmount = tokenReward.amount;
+    // reference for 10_000 https://github.com/balancer/balancer-sdk/blob/f4879f06289c6f5f9766ead1835f4f4b096ed7dd/balancer-js/src/modules/pools/apr/apr.ts#L367
+    const tokenApr = ((tokenAmount * bptPrice) / tvl) * 10_000 * 7;
+    return {
+      token: tokenReward.token,
+      amount: tokenAmount,
+      bptPrice,
+      liquidity: tvl,
+      apr: tokenApr,
+    };
+  });
+  return rewards;
+}
+
+export async function getTokensRewards({
+  startAt,
+  endAt,
+  poolId,
+  network,
+}: {
+  startAt: number;
+  endAt: number;
+  poolId: string;
+  network: string;
+}) {
+  const response = await rewards.gql(network).PoolRewards({
+    poolId,
+  });
+
+  if (!response.rewardTokenDeposits) {
+    return [];
+  }
 
   const intersectedRewards = response.rewardTokenDeposits.filter((reward) =>
     isTimePeriodsIntersecting(
@@ -37,7 +102,7 @@ export async function getTokensRewards() {
     amount: calculateAmount(groupedReward, startAt, endAt),
   }));
 
-  console.log(finalRewards);
+  return finalRewards;
 }
 
 function isTimePeriodsIntersecting(

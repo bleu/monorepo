a3fa6b42b8d5f23191f29f28f4a30389c35a7a38
Rawallon Cardoso <rawallon@gmail.com>
2023-10-05 11:19:46 -0300

    Add pools to vunerability affected pool

--- apps/balancer-tools/src/app/apr/(utils)/vunerabilityAffectedPool.ts
+++ apps/balancer-tools/src/app/apr/(utils)/vunerabilityAffectedPool.ts
@@ -13,7 +13,19 @@ export const vunerabilityAffecteRateProviders: {
     address: "0xa5fe91dde37d8bf2dacacc0168b115d28ed03f84",
   },
   {
+    token: "bb-a-DAI",
+    address: "0x6667c6fa9f2b3fc1cc8d85320b62703d938e4385",
+  },
+  {
     token: "bb-a-USDT",
     address: "0xb59be8f3c85a9dd6e2899103b6fbf6ea405b99a4",
+  },
+  {
+    token: "bb-a-WETH",
+    address: "0xbb6881874825e60e1160416d6c426eae65f2459e",
+  },
+  {
+    token: "bb-a-WETH",
+    address: "0x43894de14462b421372bcfe445fa51b1b4a0ff3d",
   },
 ];

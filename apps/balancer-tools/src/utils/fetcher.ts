84a9b92c3d4299fe7dd1f093d3ccf5d70235dba1
luizakp <luiza.puccinelli06@gmail.com>
2023-10-16 14:32:50 -0300

    add GraphQL error message to fetcher

--- apps/balancer-tools/src/utils/fetcher.ts
+++ apps/balancer-tools/src/utils/fetcher.ts
@@ -3,13 +3,20 @@ export async function fetcher<JSON = unknown>(
   init?: RequestInit,
 ): Promise<JSON> {
   const response = await fetch(input, init);
+  const responseJson = await response.json();
 
   if (!response.ok) {
-    const errorBody = await response.json();
     const errorMessage =
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      errorBody.details?.map((e: any) => e.message).join(", ") ||
+      responseJson.details?.map((e: any) => e.message).join(", ") ||
       `Network response was not ok when fetching ${input}`;
+    throw new Error(errorMessage);
+  }
+  if (responseJson.error || responseJson.errors) {
+    const errorMessage = responseJson.errors
+      ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        responseJson.errors.map((e: any) => e.message).join(", ")
+      : responseJson.error.message || "GraphQL error occurred";
     throw new Error(errorMessage);
   }
 

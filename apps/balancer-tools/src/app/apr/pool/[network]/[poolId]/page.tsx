437d26692fedbc90a408963c223f64ca990aec5b
luizakp <luiza.puccinelli06@gmail.com>
2023-10-23 16:22:11 -0300

    chore: change default range

--- apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
+++ apps/balancer-tools/src/app/apr/pool/[network]/[poolId]/page.tsx
@@ -15,7 +15,6 @@ import PoolOverviewCards from "../../(components)/PoolOverviewCards";
 import { RewardWarning } from "../../(components)/RewardsWarning";
 
 export const revalidate = 60 * 30;
-
 export default async function Page({
   params: { poolId, network },
   searchParams,
@@ -25,14 +24,16 @@ export default async function Page({
 }) {
   const parsedParams = QueryParamsPagesSchema.safeParse(searchParams);
   if (!parsedParams.success) {
-    const currentDateFormated = new Date();
+    const oneDayAgoFormated = new Date(
+      new Date().getTime() - SECONDS_IN_DAY * 1000,
+    );
     const threeDaysAgoDateFormated = new Date(
       new Date().getTime() - 3 * SECONDS_IN_DAY * 1000,
     );
     return redirect(
       generatePoolPageLink(
         threeDaysAgoDateFormated,
-        currentDateFormated,
+        oneDayAgoFormated,
         searchParams,
       ),
     );

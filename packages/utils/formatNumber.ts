f192a12a6972bdc46e397b48be85d7f73adb8117
luizakp <luiza.puccinelli06@gmail.com>
2023-10-29 19:18:23 -0300

    move format number to utils

---
+++ packages/utils/formatNumber.ts
@@ -0,0 +1,28 @@
+type Notation = "compact" | "engineering" | "scientific" | "standard";
+
+export const formatNumber = (
+  number: number | string | bigint,
+  decimals = 1,
+  style = "decimal",
+  notation: Notation = "compact",
+  lessThanThresholdToReplace = 0.001,
+) => {
+  if (Math.abs(Number(number)) < lessThanThresholdToReplace) {
+    return `< ${lessThanThresholdToReplace.toLocaleString("en-US")}`;
+  }
+
+  return Number(number).toLocaleString("en-US", {
+    notation,
+    maximumFractionDigits: decimals,
+    style,
+  });
+};
+
+export function numberToPercent(value?: number) {
+  if (!value) return undefined;
+  return value * 100;
+}
+
+export function percentToNumber(value: number) {
+  return value / 100;
+}

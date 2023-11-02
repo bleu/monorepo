a2a5e0fc5b7a1a9c312e37a58b8e49e849bbad8a
luizakp <luiza.puccinelli06@gmail.com>
2023-11-02 15:19:36 -0300

    create more order status and add context

--- apps/cow-tools/src/app/milkman/utils/type.ts
+++ apps/cow-tools/src/app/milkman/utils/type.ts
@@ -1,4 +1,7 @@
 export enum TransactionStatus {
+  DRAFT_SELECT_TOKENS = "Draft: Select tokens",
+  DRAFT_SELECT_PRICE_CHECKER = "Draft: Select price checker",
+  DRAFT_RESUME = "Draft: Resume",
   TO_BE_EXECUTED = "To be executed",
   MILKMAN_CREATED = "Milkman created",
   ORDER_PLACED = "Order placed",
---
+++ apps/cow-tools/src/contexts/OrderContext.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+import { TransactionStatus } from "#/app/milkman/utils/type";
+
+type OrderContextType = {
+  transactionStatus: TransactionStatus;
+  setTransactionStatus: (status: TransactionStatus) => void;
+};
+
+export const OrderContext = createContext({} as OrderContextType);
+
+export function OrderProvider({ children }: PropsWithChildren) {
+  const [transactionStatus, setTransactionStatus] = useState<TransactionStatus>(
+    TransactionStatus.DRAFT_SELECT_TOKENS,
+  );
+
+  return (
+    <OrderContext.Provider
+      value={{
+        transactionStatus,
+        setTransactionStatus,
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
+  );
+}
+
+export function useOrder() {
+  const context = useContext(OrderContext);
+  return context;
+}

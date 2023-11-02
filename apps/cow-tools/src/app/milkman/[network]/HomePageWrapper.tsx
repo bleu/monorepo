c8cb99922c92f5532f0d3ed923572412bbc24a2f
Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
2023-11-02 14:17:30 -0300

    [MilkmApp] Trigger first real order: hardcoded data (#457)
    * wip: hardoceded order with safe-sdk
    
    * wip public clients
    
    * remove wagmi to ether conversion
    
    * add safe-app-sdk package
    
    * wip: add hardcoded test
    
    * fix: read token decimals
    
    * refactor: reduce uni and weth hardcoded amounts
    
    * fix: remove wrong changes on apr app
    
    * refactor: milkman abi as const
    
    Co-authored-by: José Ribeiro <jose@bleu.studio>
    
    * refactor: remove ethers
    
    * refactor: remove unused public clients
    
    * refactor: solve PR comments
    
    * Update milkman address comment
    
    Co-authored-by: José Ribeiro <jose@bleu.studio>
    
    ---------
    
    Co-authored-by: luizakp <luiza.puccinelli06@gmail.com>
    Co-authored-by: José Ribeiro <jose@bleu.studio>

--- apps/cow-tools/src/app/milkman/[network]/HomePageWrapper.tsx
+++ apps/cow-tools/src/app/milkman/[network]/HomePageWrapper.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { Network } from "@bleu-balancer-tools/utils";
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk";
 import { PlusIcon } from "@radix-ui/react-icons";
-import { useAccount, useNetwork } from "wagmi";
+import { Address, useAccount, useNetwork } from "wagmi";
 
 import { Button } from "#/components";
 import { Spinner } from "#/components/Spinner";
 import WalletNotConnected from "#/components/WalletNotConnected";
 import { getNetwork } from "#/contexts/networks";
 import { AllSwapsQuery } from "#/gql/generated";
+import { getERC20ApproveRawTx } from "#/transactions/erc20Approve";
+import {
+  getRequestSwapExactTokensForTokensRawTx,
+  MILKMAN_ADDRESS,
+} from "#/transactions/milkmanOrder";
 
 import { OrderTable } from "../components/OrdersTable";
 
@@ -23,8 +29,8 @@ export function HomePageWrapper({
 }) {
   const { chain } = useNetwork();
   const { isConnected, isReconnecting, isConnecting } = useAccount();
-  const { address } = useAccount();
 
+  const { safe, sdk } = useSafeAppsSDK();
   if (!isConnected && !isReconnecting && !isConnecting) {
     return <WalletNotConnected />;
   }
@@ -54,7 +60,7 @@ export function HomePageWrapper({
           <div className="flex flex-col gap-1">
             <h1 className="text-3xl text-slate12">My Milkman transactions</h1>
             {chain?.name}
-            <span>{address}</span>
+            <span>{safe.safeAddress}</span>
           </div>
           <div className="flex gap-4">
             <Button
@@ -63,6 +69,33 @@ export function HomePageWrapper({
             >
               <PlusIcon />
               New order
+            </Button>
+            <Button
+              className="flex items-center gap-1 py-3 px-6"
+              title="Send hardcoded tx"
+              onClick={async () => {
+                const tokenIn = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; //WETH
+                const tokenOut = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"; //UNI
+                const decimals = 18;
+                const amount = BigInt(0.004 * 10 ** decimals);
+                const minOut = BigInt(1 * 10 ** decimals);
+                const txs = [
+                  getERC20ApproveRawTx(tokenIn, MILKMAN_ADDRESS, amount),
+                  getRequestSwapExactTokensForTokensRawTx(
+                    tokenIn,
+                    tokenOut,
+                    safe.safeAddress as Address,
+                    amount,
+                    minOut,
+                  ),
+                ];
+                await sdk.txs.send({
+                  txs,
+                });
+              }}
+            >
+              <PlusIcon />
+              Send HardCoded Tx
             </Button>
           </div>
         </div>

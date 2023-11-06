d003124032c8a7a4c8e3c718f69447268f138b73
mendesfabio <ms.fabiomendes@gmail.com>
2023-11-06 15:13:17 -0300

    add status to swaps

--- src/Milkman.ts
+++ src/Milkman.ts
@@ -11,6 +11,7 @@ ponder.on("Milkman:SwapRequested", async ({ event, context }) => {
     data: {
       chainId: 5, // only tracking goerli currently, waiting ponder to expose chainId in event object
       user: user.id,
+      status: "REQUESTED",
       tokenIn: tokenIn.id,
       tokenOut: tokenOut.id,
       tokenAmountIn: event.params.amountIn,

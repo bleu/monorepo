ffbe289d39db1b8adbfbdf629ffd1edc7c402ef4
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-11-09 10:44:54 -0300

    chore: add schemas

--- apps/cow-tools/src/lib/schema.ts
+++ apps/cow-tools/src/lib/schema.ts
@@ -1,25 +1,49 @@
 import { isAddress } from "viem";
 import { z } from "zod";
 
-const baseAddressValidation = z
+const basicAddressSchema = z
   .string()
   .min(1)
   .refine((value) => isAddress(value), {
     message: "Provided address is invalid",
   });
 
-export const baseOrderSchema = z
+const baseTokenAddress = z.object({
+  address: basicAddressSchema,
+  decimals: z.number().positive(),
+  symbol: z.string(),
+});
+
+export const orderOverviewSchema = z
   .object({
-    tokenSellAddress: baseAddressValidation,
-    tokenSellAmount: z.number().positive(),
-    tokenBuyAddress: baseAddressValidation,
-    receiverAddress: baseAddressValidation,
+    tokenSell: baseTokenAddress,
+    tokenSellAmount: z.coerce.number().positive(),
+    tokenBuy: baseTokenAddress,
+    receiverAddress: basicAddressSchema,
+    validFrom: z.coerce.string().optional(),
   })
   .refine(
     (data) => {
-      return data.tokenSellAddress != data.tokenBuyAddress;
+      return data.tokenSell.address != data.tokenBuy.address;
     },
     {
-      message: "Tokens sell and buy addresses must be different",
+      path: ["tokenBuy"],
+      message: "Tokens sell and buy must be different",
     }
   );
+
+const basicPriceCheckerSchema = z.object({
+  priceChecker: z.coerce.string(),
+  priceCheckerAddress: basicAddressSchema,
+});
+
+const basicDynamicSlippageSchema = basicPriceCheckerSchema.extend({
+  allowedSlippageInBps: z.coerce.number().positive(),
+});
+
+export const fixedMinOutSchema = basicPriceCheckerSchema.extend({
+  minOut: z.coerce.number().positive(),
+});
+
+export const uniV2Schema = basicDynamicSlippageSchema;
+export const sushiSwapSchema = basicDynamicSlippageSchema;

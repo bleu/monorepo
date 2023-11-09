6d2ae5327b0298241c3deeaa85807d2d810b174c
luiza <63298341+luizakp@users.noreply.github.com>
2023-11-09 17:41:55 -0300

    Add cow swap token list on token select (#472)
    * chore: add safe loader page
    
    * fix: add key to map component
    
    * chore: add cow token list
    
    * chore: add cow token list source

--- apps/cow-tools/src/components/RootLayout.tsx
+++ apps/cow-tools/src/components/RootLayout.tsx
@@ -3,6 +3,7 @@
 import SafeProvider from "@gnosis.pm/safe-apps-react-sdk";
 import { darkTheme, RainbowKitProvider, Theme } from "@rainbow-me/rainbowkit";
 import merge from "lodash.merge";
+import Link from "next/link";
 import React from "react";
 import { WagmiConfig } from "wagmi";
 
@@ -18,12 +19,32 @@ export function RootLayout({ children }: React.PropsWithChildren) {
         modalSize="compact"
         theme={CustomTheme}
       >
-        <SafeProvider loader={<>Waiting for Safe...</>}>
+        <SafeProvider loader={<SafeLoader />}>
           <Fathom />
           <div className="h-full w-full bg-blue1">{children}</div>
         </SafeProvider>
       </RainbowKitProvider>
     </WagmiConfig>
+  );
+}
+
+function SafeLoader() {
+  return (
+    <div className="flex h-full w-full flex-col  justify-center items-center rounded-3xl px-12 py-16 md:py-20  text-slate12">
+      <div className="text-center text-3xl">This is a Safe (wallet) App</div>
+      <p className="text-xl">
+        To access please use this
+        <Link
+          target="_blank"
+          href="https://app.safe.global/share/safe-app?appUrl=https%3A%2F%2Fcow-tools-bleu-fi.vercel.app&chain=gor"
+          className="text-amber9"
+        >
+          {" "}
+          link{" "}
+        </Link>
+        with your safe account connected
+      </p>
+    </div>
   );
 }
 

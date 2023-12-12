2f6c7bb34a84f21c07dc90596a566f2b220b1fb8
luizakp <luiza.puccinelli06@gmail.com>
2023-12-12 17:27:34 -0300

    try to debug endpoint

--- apps/balancer-tools/src/app/apr/api/seed/route.ts
+++ apps/balancer-tools/src/app/apr/api/seed/route.ts
@@ -1,12 +1,25 @@
-/* eslint-disable no-console */
-
+import { runDailyETLs } from "@bleu-fi/balancer-apr/src/dailySeed";
 import { NextRequest } from "next/server";
 
 export const maxDuration = 300;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: NextRequest) {
-  console.log(process.env.DATABASE_URL);
+  // const authHeader = request.headers.get("authorization");
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //   });
+  // }
+
+  // Start the ETL process asynchronously
+  runDailyETLs()
+    .then(() => {
+      console.log("Seed finished");
+    })
+    .catch((error) => {
+      console.error("Seed encountered an error:", error);
+    });
 
   return Response.json({ message: "Seed is being done" });
 }

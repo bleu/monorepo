7aac25098c4a4ca97bff9579d2e4bf12db18dcff
luizakp <luiza.puccinelli06@gmail.com>
2023-12-12 16:21:37 -0300

    try to execute seed from vercel without cron

--- apps/balancer-tools/src/app/apr/api/seed/route.ts
+++ apps/balancer-tools/src/app/apr/api/seed/route.ts
@@ -7,12 +7,12 @@ export const maxDuration = 300;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
+  // const authHeader = request.headers.get("authorization");
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //   });
+  // }
 
   // Start the ETL process asynchronously
   runDailyETLs()

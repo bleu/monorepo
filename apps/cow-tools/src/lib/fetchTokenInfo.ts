e1f21f346c5472daed1eb75882fd6f8478ced602
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-12-15 12:15:09 -0300

    chore: run lint

--- apps/cow-tools/src/lib/fetchTokenInfo.ts
+++ apps/cow-tools/src/lib/fetchTokenInfo.ts
@@ -1,7 +1,7 @@
 import { Address } from "@bleu-fi/utils";
+import { erc20ABI } from "@wagmi/core";
 
 import { ChainId, publicClientsFromIds } from "#/utils/chainsPublicClients";
-import { erc20ABI } from "@wagmi/core";
 
 export function fetchTokenInfo<T>(
   tokenAddress: Address,

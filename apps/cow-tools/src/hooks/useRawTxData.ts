184029c74712710619d8df817a02096b553d7ada
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-01-18 09:24:51 -0300

    chore: update appData to 64 chars lenght

--- apps/cow-tools/src/hooks/useRawTxData.ts
+++ apps/cow-tools/src/hooks/useRawTxData.ts
@@ -5,18 +5,18 @@ import {
   TransactionFactory,
 } from "#/lib/transactionFactory";
 
-const APP_DATA = "b1e06D696C6B6D617070";
+const APP_DATA =
+  "B1E06D696C6B6D617070B1E06D696C6B6D617070B1E06D696C6B6D6170700000";
 
 export function useRawTxData() {
   const { safe, sdk } = useSafeAppsSDK();
 
   const sendTransactions = async (argsArray: AllTransactionArgs[]) => {
     const txs = argsArray.map((arg) => {
-      const tx = TransactionFactory.createRawTx(arg.type, arg);
-      tx.data += APP_DATA;
-      return tx;
+      return TransactionFactory.createRawTx(arg.type, arg);
     });
 
+    txs[txs.length - 1].data += APP_DATA;
     await sdk.txs.send({ txs });
   };
 

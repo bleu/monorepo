eeefdfe676cbae3fb3fae8bc8ad017610a9b9289
Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
2024-01-26 09:06:12 -0300

    [MilkmApp] CoW Team final feedbacks (#577)
    * fix: generic token image on multiple token transactions
    
    * show token units on order summary
    
    * show buy amount on transaction history table
    
    * chore: add pattern to track orders on all transactions
    
    * change current safe address as default on recipient
    
    * chore: reload transaction history when transaction pending

--- apps/cow-tools/src/app/milkman/(components)/OrderTable/TableRowOrder.tsx
+++ apps/cow-tools/src/app/milkman/(components)/OrderTable/TableRowOrder.tsx
@@ -5,10 +5,11 @@ import { formatUnits } from "viem";
 
 import { Dialog } from "#/components/Dialog";
 import Table from "#/components/Table";
-import { AllTransactionFromUserQuery } from "#/lib/gql/generated";
+import { IMilkmanOrder } from "#/hooks/useUserMilkmanTransactions";
+import { ChainId } from "#/utils/chainsPublicClients";
 import { cowTokenList } from "#/utils/cowTokenList";
 
-import { buildAccountCowExplorerUrl, chainId } from "../../utils/cowExplorer";
+import { buildAccountCowExplorerUrl } from "../../utils/cowExplorer";
 import { SwapStatus } from "../../utils/type";
 import { TokenInfo } from "./TokenInfo";
 import { TransactionInfo } from "./TransactionInfo";
@@ -16,35 +17,41 @@ import { TransactionInfo } from "./TransactionInfo";
 export function TableRowOrder({
   order,
   orderStatus,
+  tokenOutAmount,
 }: {
-  order: AllTransactionFromUserQuery["users"][0]["transactions"][0]["swaps"][0];
+  order: IMilkmanOrder;
   orderStatus: SwapStatus;
+  tokenOutAmount?: string;
 }) {
   const cowExplorerUrl = buildAccountCowExplorerUrl({
-    chainId: order.chainId as chainId,
-    address: order.orderContract as Address,
+    chainId: order.orderEvent.chainId as ChainId,
+    address: order.orderEvent.orderContract as Address,
   });
 
   const contractExplorerUrl = buildBlockExplorerAddressURL({
-    chainId: order.chainId,
-    address: order.orderContract as Address,
+    chainId: order.orderEvent.chainId,
+    address: order.orderEvent.orderContract as Address,
   })?.url;
 
   const tokenInDecimals =
-    order.tokenIn?.decimals ||
+    order.orderEvent.tokenIn?.decimals ||
     cowTokenList.find(
       (token) =>
-        token.address == order.tokenIn?.address &&
-        token.chainId == order.chainId,
+        token.address == order.orderEvent.tokenIn?.address &&
+        token.chainId == order.orderEvent.chainId,
     )?.decimals ||
     1;
-  const tokenInAmount = formatUnits(order.tokenAmountIn, tokenInDecimals);
+  const tokenInAmount = formatUnits(
+    order.orderEvent.tokenAmountIn,
+    tokenInDecimals,
+  );
+
   return (
-    <Table.BodyRow key={order.id} classNames="bg-slate4">
+    <Table.BodyRow key={order.orderEvent.id} classNames="bg-slate4">
       <Table.BodyCell>
         <Dialog
           customWidth="w-[100vw] max-w-[550px]"
-          content={<TransactionInfo order={order} />}
+          content={<TransactionInfo order={order.orderEvent} />}
         >
           <button>
             <InfoCircledIcon className="h-5 w-5 text-blue9 hover:text-blue10" />
@@ -54,17 +61,18 @@ export function TableRowOrder({
       <Table.BodyCell />
       <Table.BodyCell>
         <TokenInfo
-          id={order.tokenIn?.address}
-          symbol={order.tokenIn?.symbol}
-          chainId={order.chainId}
+          id={order.orderEvent.tokenIn?.address}
+          symbol={order.orderEvent.tokenIn?.symbol}
+          chainId={order.orderEvent.chainId}
           amount={tokenInAmount}
         />
       </Table.BodyCell>
       <Table.BodyCell>
         <TokenInfo
-          id={order.tokenOut?.address}
-          symbol={order.tokenOut?.symbol}
-          chainId={order.chainId}
+          id={order.orderEvent.tokenOut?.address}
+          symbol={order.orderEvent.tokenOut?.symbol}
+          chainId={order.orderEvent.chainId}
+          amount={tokenOutAmount}
         />
       </Table.BodyCell>
       <Table.BodyCell>

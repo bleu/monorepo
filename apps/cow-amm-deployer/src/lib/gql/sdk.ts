db62c8a84a1fedd7640931b6ddb12fdcb5302082
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-02-22 15:44:11 -0300

    chore: remove milkmapp related files

--- apps/cow-amm-deployer/src/lib/gql/sdk.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-import { getSdk } from "#/lib/gql/generated";
-
-export const ENDPOINT = "https://milkman-api.up.railway.app/";
-
-const client = new GraphQLClient(ENDPOINT);
-
-export const milkmanSubgraph = getSdk(client);

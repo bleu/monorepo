ee441e805fd11df4a0b070365f749ea3ec00c485
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-02-27 13:25:46 -0300

    add cow amm handler

---
+++ src/utils.ts
@@ -0,0 +1,43 @@
+import { erc20Abi } from "../abis/erc20";
+import { contextType } from "./types";
+
+export function callERC20Contract<T>(
+  address: `0x${string}`,
+  functionName: "symbol" | "decimals" | "name",
+  context: contextType
+): Promise<T> {
+  return context.client.readContract({
+    abi: erc20Abi,
+    address,
+    functionName,
+  }) as Promise<T>;
+}
+
+export function getErc20Data(address: `0x${string}`, context: contextType) {
+  return Promise.all([
+    callERC20Contract<string>(address, "symbol", context).catch(() => ""),
+    callERC20Contract<number>(address, "decimals", context).catch(() => 0),
+    callERC20Contract<string>(address, "name", context).catch(() => ""),
+  ]);
+}
+
+export async function getToken(address: `0x${string}`, context: contextType) {
+  const tokenId = `${address}-${context.network.chainId}`;
+  let token = await context.db.Token.findUnique({
+    id: tokenId,
+  });
+  if (!token) {
+    const [symbol, decimals, name] = await getErc20Data(address, context);
+    token = await context.db.Token.create({
+      id: `${address}-${context.network.chainId}`,
+      data: {
+        address,
+        chainId: context.network.chainId,
+        symbol,
+        decimals,
+        name,
+      },
+    });
+  }
+  return token;
+}

2cb22e0f37bf9efc5cf872c20b169caba4f60cc4
José Ribeiro <me@joseribeiro.dev>
2024-03-04 10:33:27 -0300

    remove sentry from cow projects

--- apps/cow-amm-deployer/next.config.js
+++ apps/cow-amm-deployer/next.config.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const moduleExports = {
@@ -19,10 +18,6 @@ const moduleExports = {
       },
     ];
   },
-  experimental: {
-    // Add the "@sentry/profiling-node" to serverComponentsExternalPackages.
-    serverComponentsExternalPackages: ["@sentry/profiling-node"],
-  },
   transpilePackages: ["@bleu-fi/gql"],
   reactStrictMode: true,
   swcMinify: true,
@@ -33,13 +28,8 @@ const moduleExports = {
    */
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding", {
-      "@sentry/profiling-node": "commonjs @sentry/profiling-node",
-    });
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
-  },
-  sentry: {
-    hideSourceMaps: true,
   },
   images: {
     domains: [
@@ -102,34 +92,4 @@ const moduleExports = {
   },
 };
 
-module.exports = withSentryConfig(
-  moduleExports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "bleu",
-    project: "cowswap-tools",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-);
+module.exports = moduleExports;

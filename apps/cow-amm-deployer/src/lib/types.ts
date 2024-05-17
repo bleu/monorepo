7f9252f75f85f82916511e189f3d2af6de89da41
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-05-17 18:37:31 -0300

    add price information of new oracles

--- apps/cow-amm-deployer/src/lib/types.ts
+++ apps/cow-amm-deployer/src/lib/types.ts
@@ -23,6 +23,7 @@ export interface IToken {
 export interface PriceOracleData {
   balancerPoolId?: `0x${string}`;
   uniswapV2PairAddress?: Address;
+  sushiSwapPairAddress?: Address;
 }
 export interface ICowAmm {
   token0: TokenBalance & {
@@ -38,4 +39,5 @@ export interface ICowAmm {
   priceOracle: PRICE_ORACLES;
   priceOracleData: PriceOracleData;
   hash: `0x${string}`;
+  priceOracleAddress: Address;
 }

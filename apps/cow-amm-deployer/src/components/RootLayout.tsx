920cfd4ef8bdc90313e1b5377fb5ee4e9a02a3cf
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-05-29 12:13:17 -0300

    Use cow token list on token select

--- apps/cow-amm-deployer/src/components/RootLayout.tsx
+++ apps/cow-amm-deployer/src/components/RootLayout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "@bleu/ui";
 import SafeProvider from "@gnosis.pm/safe-apps-react-sdk";
 import React from "react";
 
+import { TokenSelectContextProvider } from "#/contexts/tokenSelect";
+
 import Fathom from "./Fathom";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -11,17 +13,19 @@ import { Header } from "./Header";
 export function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <SafeProvider>
-      <Fathom />
-      <div className="size-full">
-        <div className="flex flex-col h-screen">
-          <Header linkUrl={"/"} imageSrc={"/assets/cow-amm-deployer.svg"} />
-          <div className="flex flex-1 overflow-auto p-4 sm:flex-row sm:gap-x-8 text-foreground">
-            {children}
+      <TokenSelectContextProvider>
+        <Fathom />
+        <div className="size-full">
+          <div className="flex flex-col h-screen">
+            <Header linkUrl={"/"} imageSrc={"/assets/cow-amm-deployer.svg"} />
+            <div className="flex flex-1 overflow-auto p-4 sm:flex-row sm:gap-x-8 text-foreground">
+              {children}
+            </div>
+            <Footer twitterLink="https://twitter.com/cowswap" />
+            <Toaster />
           </div>
-          <Footer twitterLink="https://twitter.com/cowswap" />
-          <Toaster />
         </div>
-      </div>
+      </TokenSelectContextProvider>
     </SafeProvider>
   );
 }
---
+++ apps/cow-amm-deployer/src/contexts/tokenSelect.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+
+import { cowTokenList } from "#/lib/cowTokenList";
+import { IToken } from "#/lib/fetchAmmData";
+import { ChainId } from "#/utils/chainsPublicClients";
+
+interface ITokenWithChainId extends IToken {
+  chainId: ChainId;
+}
+
+interface ITokenSelectContext {
+  getTokenList: (chainId: ChainId) => IToken[];
+  addImportedToken: (token: IToken, chainId: ChainId) => void;
+}
+
+export const TokenSelectContext = React.createContext<ITokenSelectContext>(
+  {} as ITokenSelectContext
+);
+
+export const TokenSelectContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [importedTokenList, setImportedTokenList] = React.useState<
+    ITokenWithChainId[]
+  >(
+    (JSON.parse(
+      // @ts-ignore
+      localStorage.getItem("importedTokens")
+    ) as ITokenWithChainId[] | null) ?? []
+  );
+
+  function getTokenList(chainId: ChainId) {
+    return [
+      ...(cowTokenList.filter(
+        (token) => token.chainId === chainId
+      ) as IToken[]),
+      ...importedTokenList,
+    ];
+  }
+
+  function addImportedToken(token: IToken, chainId: ChainId) {
+    const newImportedTokenList = [...importedTokenList, { ...token, chainId }];
+    setImportedTokenList(newImportedTokenList);
+    localStorage.setItem(
+      "importedTokens",
+      JSON.stringify(newImportedTokenList)
+    );
+  }
+
+  return (
+    <TokenSelectContext.Provider
+      value={{
+        getTokenList,
+        addImportedToken,
+      }}
+    >
+      {children}
+    </TokenSelectContext.Provider>
+  );
+};
+
+export const useTokenSelect = () => React.useContext(TokenSelectContext);

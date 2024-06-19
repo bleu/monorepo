0183922f642bad14c4a4dd355914ae69a801c6a4
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-06-19 09:06:39 -0300

    remove codegen of composable cow

--- apps/cow-amm-deployer/src/lib/gqlComposableCow/sdk.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-import { getSdk } from "#/lib/gqlComposableCow/generated";
-
-export const ENDPOINT = "https://composable-cow-api.up.railway.app";
-
-const client = new GraphQLClient(ENDPOINT);
-
-export const composableCowApi = getSdk(client);

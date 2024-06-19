9f4c5fdca19f20770b59a8e5a4ba9b3ea1700f85
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-06-19 09:06:51 -0300

    remove autoconnect

--- apps/cow-amm-deployer/src/app/page.tsx
+++ apps/cow-amm-deployer/src/app/page.tsx
@@ -3,12 +3,10 @@
 import { useAccount } from "wagmi";
 
 import { HomePageWrapper } from "#/components/HomePageWrapper";
-import { useAutoConnect } from "#/hooks/tx-manager/useAutoConnect";
 
 export default function Page() {
   const { address: safeAddress, chainId } = useAccount();
 
-  useAutoConnect();
   const userId = `${safeAddress}-${chainId}`;
 
   return <HomePageWrapper userId={userId} />;
